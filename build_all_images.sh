bf9d27dbe54dfeb53f228e55285bbda92ba37d83
Brian Chan <brian.chan@liferay.com>
2018-09-07 14:10:02 -0700

    Update images

--- build_all_images.sh
+++ build_all_images.sh
@@ -6,7 +6,7 @@ function main {
 		releases.liferay.com/portal/7.0.6-ga7/liferay-ce-portal-tomcat-7.0-ga7-20180507111753223.zip
 		releases.liferay.com/portal/7.1.0-ga1/liferay-ce-portal-tomcat-7.1.0-ga1-20180703012531655.zip
 		releases.liferay.com/portal/snapshot-7.0.x/201808161346/liferay-portal-tomcat-7.0.x.zip
-		releases.liferay.com/portal/snapshot-7.1.x/201809042204/liferay-portal-tomcat-7.1.x.7z
+		releases.liferay.com/portal/snapshot-7.1.x/201809071128/liferay-portal-tomcat-7.1.x.7z
 		files.liferay.com/private/ee/portal/6.2.10.21/liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
 		files.liferay.com/private/ee/portal/7.0.10.8/liferay-dxp-digital-enterprise-tomcat-7.0-sp8-20180717152749345.zip
 		files.liferay.com/private/ee/portal/7.1.10/liferay-dxp-tomcat-7.1.10-ga1-20180703090613030.zip
