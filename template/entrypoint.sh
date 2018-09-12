b8a0098241884c3116a4ca7bd155ecf582ccbbb3
Brian Chan <brian.chan@liferay.com>
2018-09-11 20:48:39 -0700

    Forgot to escape

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -22,7 +22,7 @@ function main {
 
 		cp -r /etc/liferay/mount/files ${LIFERAY_HOME}
 	else
-		echo "[LIFERAY] The directory /etc/liferay/mount/files does not exist. Create the directory ${pwd}/xyz123/files on the host operating system to create the directory /etc/liferay/mount/files on the container. Files in /etc/liferay/mount/files will be copied to ${LIFERAY_HOME} before ${LIFERAY_PRODUCT_NAME} starts."
+		echo "[LIFERAY] The directory /etc/liferay/mount/files does not exist. Create the directory \${pwd}/xyz123/files on the host operating system to create the directory /etc/liferay/mount/files on the container. Files in /etc/liferay/mount/files will be copied to ${LIFERAY_HOME} before ${LIFERAY_PRODUCT_NAME} starts."
 	fi
 
 	echo ""
@@ -41,7 +41,7 @@ function main {
 			${SCRIPT_NAME}
 		done
 	else
-		echo "[LIFERAY] The directory /etc/liferay/mount/scripts does not exist. Create the directory ${pwd}/xyz123/scripts on the host operating system to create the directory /etc/liferay/mount/scripts on the container. Files in /etc/liferay/mount/scripts will be executed, in alphabetical order, before ${LIFERAY_PRODUCT_NAME} starts."
+		echo "[LIFERAY] The directory /etc/liferay/mount/scripts does not exist. Create the directory \${pwd}/xyz123/scripts on the host operating system to create the directory /etc/liferay/mount/scripts on the container. Files in /etc/liferay/mount/scripts will be executed, in alphabetical order, before ${LIFERAY_PRODUCT_NAME} starts."
 	fi
 
 	echo ""
@@ -54,9 +54,9 @@ function main {
 
 		ln -s /etc/liferay/mount/deploy /opt/liferay/deploy
 
-		echo "The directory /etc/liferay/mount/deploy is ready. Copy files to ${pwd}/xyz123/deploy on the host operating system, which is bridged to /etc/liferay/mount/deploy on the container, to deploy new modules to ${LIFERAY_PRODUCT_NAME} at runtime."
+		echo "The directory /etc/liferay/mount/deploy is ready. Copy files to \${pwd}/xyz123/deploy on the host operating system, which is bridged to /etc/liferay/mount/deploy on the container, to deploy new modules to ${LIFERAY_PRODUCT_NAME} at runtime."
 	else
-		echo "[LIFERAY] The directory /etc/liferay/mount/deploy does not exist. Create the directory ${pwd}/xyz123/deploy on the host operating system to create the directory /etc/liferay/mount/deploy on the container. Copy files to ${pwd}/xyz123/deploy to deploy modules to ${LIFERAY_PRODUCT_NAME} at runtime."
+		echo "[LIFERAY] The directory /etc/liferay/mount/deploy does not exist. Create the directory \${pwd}/xyz123/deploy on the host operating system to create the directory /etc/liferay/mount/deploy on the container. Copy files to \${pwd}/xyz123/deploy to deploy modules to ${LIFERAY_PRODUCT_NAME} at runtime."
 	fi
 
 	echo ""
