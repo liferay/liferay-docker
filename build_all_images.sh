e548d8ec2b18755fe62274a30bf34068f20cc1f9
Brian Chan <brian.chan@liferay.com>
2018-11-12 19:07:23 -0800

    Update 7.1.1 ga2 bundle

--- build_all_images.sh
+++ build_all_images.sh
@@ -6,7 +6,7 @@ function main {
 		releases.liferay.com/portal/6.2.5-ga6/liferay-portal-tomcat-6.2-ce-ga6-20160112152609836.zip
 		files.liferay.com/private/ee/portal/6.2.10.21/liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
 		releases.liferay.com/portal/7.0.6-ga7/liferay-ce-portal-tomcat-7.0-ga7-20180507111753223.zip
-		releases.liferay.com/portal/7.1.1-ga2/liferay-ce-portal-tomcat-7.1.1-ga2-20181102214521000.7z
+		releases.liferay.com/portal/7.1.1-ga2/liferay-ce-portal-tomcat-7.1.1-ga2-20181112144637000.7z
 		#releases.liferay.com/portal/snapshot-7.0.x/201808161346/liferay-portal-tomcat-7.0.x.zip
 		#releases.liferay.com/portal/snapshot-7.1.x/201809171924/liferay-portal-tomcat-7.1.x.7z
 		files.liferay.com/private/ee/portal/7.0.10.8/liferay-dxp-digital-enterprise-tomcat-7.0-sp8-20180717152749345.zip
