393b5b0e61008a287b2d7a7ea0ce93bc909a0051
Brian Chan <brian.chan@liferay.com>
2018-11-21 20:07:26 -0800

    [#9] Always declare it

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -66,7 +66,7 @@ function main {
 	echo "[LIFERAY] Starting ${LIFERAY_PRODUCT_NAME}. To stop the container with CTRL-C, run this container with the option \"-it\"."
 	echo ""
 
-	if [ -n ${ENABLE_DEBUG} ] && [ "${ENABLE_DEBUG}" == "true" ]
+	if [ "${LIFERAY_JPDA_ENABLED}" == "true" ]
 	then
 		${LIFERAY_HOME}/tomcat/bin/catalina.sh jpda run
 	else
