7e90d22d69b95c5f3119d80f54c018d2362346c7
Brian Chan <brian.chan@liferay.com>
2019-01-21 19:47:39 -0800

    7.1.2 ga3

--- build_all_images.sh
+++ build_all_images.sh
@@ -2,13 +2,13 @@
 
 function main {
 	local release_file_urls=(
-		#releases.liferay.com/commerce/1.0.2/liferay-commerce-1.0.2-201812111420.7z
+		#releases.liferay.com/commerce/1.0.2/liferay-commerce-1.0.2-201812200941.7z
 		releases.liferay.com/portal/6.1.2-ga3/liferay-portal-tomcat-6.1.2-ce-ga3-20130816114619181.zip
 		files.liferay.com/private/ee/portal/6.1.30.5/liferay-portal-tomcat-6.1-ee-ga3-sp5-20160201142343123.zip
 		releases.liferay.com/portal/6.2.5-ga6/liferay-portal-tomcat-6.2-ce-ga6-20160112152609836.zip
 		files.liferay.com/private/ee/portal/6.2.10.21/liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
 		releases.liferay.com/portal/7.0.6-ga7/liferay-ce-portal-tomcat-7.0-ga7-20180507111753223.zip
-		releases.liferay.com/portal/7.1.1-ga2/liferay-ce-portal-tomcat-7.1.1-ga2-20181112144637000.7z
+		releases.liferay.com/portal/7.1.2-ga3/liferay-ce-portal-tomcat-7.1.2-ga3-20190107144105508.7z
 		#releases.liferay.com/portal/snapshot-7.0.x/201808161346/liferay-portal-tomcat-7.0.x.zip
 		#releases.liferay.com/portal/snapshot-7.1.x/201809171924/liferay-portal-tomcat-7.1.x.7z
 		files.liferay.com/private/ee/portal/7.0.10.8/liferay-dxp-digital-enterprise-tomcat-7.0-sp8-20180717152749345.zip
