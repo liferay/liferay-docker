57a3603f3ae89897f5bfaba65954a60f19e20740
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-08-05 10:18:07 +0200

    Enable downloading from the release servers

--- build_image.sh
+++ build_image.sh
@@ -94,7 +94,7 @@ function main {
 
 	local release_file_url=${1}
 
-	if [[ ${release_file_url} != http://mirrors.*.liferay.com* ]]
+	if [[ ${release_file_url} != http://mirrors.*.liferay.com* ]] && [[ ${release_file_url} != http://release* ]]
 	then
 		release_file_url=http://mirrors.lax.liferay.com/${release_file_url}
 	fi
