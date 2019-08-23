2c107220ff7156762576e07cd03820585b35ac20
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-08-23 15:25:32 +0200

    Separating scripts to provide better support for using this as a parent image

--- template/scripts/liferay_entrypoint.sh
+++ template/scripts/liferay_entrypoint.sh
@@ -10,82 +10,11 @@ function main {
 		LIFERAY_MOUNT_DIR=/mnt/liferay
 	fi
 
-	if [ ! -d ${LIFERAY_MOUNT_DIR} ]
-	then
-		echo "[LIFERAY] Run this container with the option \"-v \$(pwd)/xyz123:/mnt/liferay\" to bridge \$(pwd)/xyz123 in the host operating system to ${LIFERAY_MOUNT_DIR} on the container."
-		echo ""
-	fi
+	export LIFERAY_MOUNT_DIR
 
-	if [ -d ${LIFERAY_MOUNT_DIR}/files ]
-	then
-		if [ $(ls -A ${LIFERAY_MOUNT_DIR}/files) ]
-		then
-			echo "[LIFERAY] Copying files from ${LIFERAY_MOUNT_DIR}/files:"
-			echo ""
+	setup_liferay_dirs.sh
 
-			tree --noreport ${LIFERAY_MOUNT_DIR}/files
-
-			echo ""
-			echo "[LIFERAY] ... into ${LIFERAY_HOME}."
-
-			cp -r ${LIFERAY_MOUNT_DIR}/files/* ${LIFERAY_HOME}
-
-			echo ""
-		fi
-	else
-		echo "[LIFERAY] The directory /mnt/liferay/files does not exist. Create the directory \$(pwd)/xyz123/files on the host operating system to create the directory ${LIFERAY_MOUNT_DIR}/files on the container. Files in ${LIFERAY_MOUNT_DIR}/files will be copied to ${LIFERAY_HOME} before ${LIFERAY_PRODUCT_NAME} starts."
-		echo ""
-	fi
-
-	if [ -d ${LIFERAY_MOUNT_DIR}/scripts ]
-	then
-		if [ $(ls -A ${LIFERAY_MOUNT_DIR}/scripts) ]
-		then
-			echo "[LIFERAY] Executing scripts in ${LIFERAY_MOUNT_DIR}/scripts:"
-
-			for SCRIPT_NAME in ${LIFERAY_MOUNT_DIR}/scripts/*
-			do
-				echo ""
-				echo "[LIFERAY] Executing ${SCRIPT_NAME}."
-
-				chmod a+x ${SCRIPT_NAME}
-
-				${SCRIPT_NAME}
-			done
-
-			echo ""
-		fi
-	else
-		echo "[LIFERAY] The directory /mnt/liferay/scripts does not exist. Create the directory \$(pwd)/xyz123/scripts on the host operating system to create the directory ${LIFERAY_MOUNT_DIR}/scripts on the container. Files in ${LIFERAY_MOUNT_DIR}/scripts will be executed, in alphabetical order, before ${LIFERAY_PRODUCT_NAME} starts."
-		echo ""
-	fi
-
-	if [ -d ${LIFERAY_MOUNT_DIR}/deploy ]
-	then
-		if [ $(ls -A /opt/liferay/deploy) ]
-		then
-			cp /opt/liferay/deploy/* ${LIFERAY_MOUNT_DIR}/deploy
-		fi
-
-		rm -fr /opt/liferay/deploy
-
-		ln -s ${LIFERAY_MOUNT_DIR}/deploy /opt/liferay/deploy
-
-		echo "[LIFERAY] The directory /mnt/liferay/deploy is ready. Copy files to \$(pwd)/xyz123/deploy on the host operating system to deploy modules to ${LIFERAY_PRODUCT_NAME} at runtime."
-	else
-		echo "[LIFERAY] The directory /mnt/liferay/deploy does not exist. Create the directory \$(pwd)/xyz123/deploy on the host operating system to create the directory ${LIFERAY_MOUNT_DIR}/deploy on the container. Copy files to \$(pwd)/xyz123/deploy to deploy modules to ${LIFERAY_PRODUCT_NAME} at runtime."
-	fi
-
-	echo ""
-	echo "[LIFERAY] Starting ${LIFERAY_PRODUCT_NAME}. To stop the container with CTRL-C, run this container with the option \"-it\"."
-	echo ""
-
-	if [ "${LIFERAY_JPDA_ENABLED}" == "true" ]
-	then
-		${LIFERAY_HOME}/tomcat/bin/catalina.sh jpda run
-	else
-		${LIFERAY_HOME}/tomcat/bin/catalina.sh run
-	fi
+	start_liferay.sh
 }
 
 main
---
+++ template/scripts/start_liferay.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function main {
+	echo ""
+	echo "[LIFERAY] Starting ${LIFERAY_PRODUCT_NAME}. To stop the container with CTRL-C, run this container with the option \"-it\"."
+	echo ""
+
+	if [ "${LIFERAY_JPDA_ENABLED}" == "true" ]
+	then
+		${LIFERAY_HOME}/tomcat/bin/catalina.sh jpda run
+	else
+		${LIFERAY_HOME}/tomcat/bin/catalina.sh run
+	fi
+}
+
+main
