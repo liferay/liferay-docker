7d906312d3eb74c349b359a773b67b3a8f0e6a3c
Brian Chan <brian.chan@liferay.com>
2019-09-10 13:27:55 -0700

    Commerce 2.0.5

--- build_all_images.sh
+++ build_all_images.sh
@@ -2,7 +2,7 @@
 
 function main {
 	local release_file_urls=(
-		#releases.liferay.com/commerce/2.0.4/liferay-commerce-2.0.4-201908161006.7z
+		#releases.liferay.com/commerce/2.0.5/liferay-commerce-2.0.5-201909101308.7z
 		releases.liferay.com/portal/6.1.2-ga3/liferay-portal-tomcat-6.1.2-ce-ga3-20130816114619181.zip
 		files.liferay.com/private/ee/portal/6.1.30.5/liferay-portal-tomcat-6.1-ee-ga3-sp5-20160201142343123.zip
 		releases.liferay.com/portal/6.2.5-ga6/liferay-portal-tomcat-6.2-ce-ga6-20160112152609836.zip
