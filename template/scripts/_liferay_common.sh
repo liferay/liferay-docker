c37d4b6e982d8a850f873bfe829d7a17350e295e
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-11-21 10:46:26 +0100

    We need to allow scripts to set environment variables. Exec permission is not needed when a script is sourced, this is why it's simplified

--- template/scripts/_liferay_common.sh
+++ template/scripts/_liferay_common.sh
@@ -10,12 +10,7 @@ function execute_scripts {
 			echo ""
 			echo "[LIFERAY] Executing ${SCRIPT_NAME}."
 
-			if [ ! -x ${1}/${SCRIPT_NAME} ]
-			then
-				chmod a+x ${1}/${SCRIPT_NAME}
-			fi
-
-			${1}/${SCRIPT_NAME}
+			source ${1}/${SCRIPT_NAME}
 		done
 
 		echo ""
