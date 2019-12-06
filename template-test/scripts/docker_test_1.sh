47f7d638b6f9dd6e8b8ea0e5ec8800bd1ccfd8bc
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-12-06 15:00:38 +0100

    Adding the files to be mounted into the docker image

---
+++ template-test/scripts/docker_test_1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "TEST1" > /opt/liferay/tomcat/webapps/ROOT/docker_generated_test.jsp
---
+++ template-test/scripts/docker_test_2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "TEST2" > /opt/liferay/tomcat/webapps/ROOT/docker_generated_test.jsp
