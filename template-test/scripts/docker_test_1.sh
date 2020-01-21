2b6450d9da26bcee635c3dc4ade0641b48a5e97c
Brian Chan <brian.chan@liferay.com>
2020-01-21 13:19:03 -0800

    SF

--- template-test/scripts/docker_test_1.sh
+++ template-test/scripts/docker_test_1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 echo "TEST1" > /opt/liferay/tomcat/webapps/ROOT/docker_generated_test.jsp
--- template-test/scripts/docker_test_2.sh
+++ template-test/scripts/docker_test_2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 echo "TEST2" > /opt/liferay/tomcat/webapps/ROOT/docker_generated_test.jsp
