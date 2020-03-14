a4ddc6568a63cc12c904e92566023103660e054f
Brian Chan <brian.chan@liferay.com>
2020-03-13 21:16:31 -0700

    Rounder

--- test_image.sh
+++ test_image.sh
@@ -103,7 +103,7 @@ function test_docker_image_scripts_2 {
 function test_health_status {
 	echo -en "Waiting for health status"
 
-	for counter in {1..50}
+	for counter in {1..60}
 	do
 		echo -en "."
 
