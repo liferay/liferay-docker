ab6fe7c0d1686171565ec841288f26d847d263d3
Zsolt Balogh <zsolt.balogh@liferay.com>
2020-09-03 18:45:32 +0000

    LPS-120393 License file was renamed

--- template/scripts/configure_liferay.sh
+++ template/scripts/configure_liferay.sh
@@ -5,7 +5,8 @@ source /usr/local/bin/_liferay_common.sh
 function main {
 	if [ "${LIFERAY_DISABLE_TRIAL_LICENSE}" == "true" ]
 	then
-		rm -f /opt/liferay/deploy/trial-license-*.xml
+		rm -f /opt/liferay/deploy/trial-dxp-license-*.xml
+		rm -f /opt/liferay/data/license/trial-commerce-enterprise-license-*.li
 	fi
 
 	if [ ! -d ${LIFERAY_MOUNT_DIR} ]
