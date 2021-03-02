e9d4d804cbce566e5fbea73732298837b4675332
Brian Chan <brian.chan@liferay.com>
2021-03-02 09:50:04 -0300

    LPS-128539 SF

--- build_bundle_image.sh
+++ build_bundle_image.sh
@@ -239,7 +239,7 @@ function update_patching_tool {
 		patching_tool_minor_version=${patching_tool_minor_version##*patching-tool version: }
 		patching_tool_minor_version=${patching_tool_minor_version%.*}
 
-		if ( ! echo ${patching_tool_minor_version} | grep -e '[0-9]*[.][0-9]*' >/dev/null )
+		if (! echo ${patching_tool_minor_version} | grep -e '[0-9]*[.][0-9]*' >/dev/null)
 		then
 			echo "Patching Tool update is skipped as it's not a 2.0+ version or the bundle did not include a properly configured Patching Tool."
 
