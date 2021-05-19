6a8cd633d3acef3bd69644c0e9ad8540d18251d6
Brian Chan <brian.chan@liferay.com>
2021-05-18 19:11:28 -0700

    snapshot-master

--- build_all_images.sh
+++ build_all_images.sh
@@ -467,7 +467,7 @@ function main {
 		releases.liferay.com/portal/7.3.6-ga7/liferay-ce-portal-tomcat-7.3.6-ga7-20210301155526191.7z
 		releases.liferay.com/portal/7.4.0-ga1/liferay-ce-portal-tomcat-7.4.0-ga1-20210419204607406.7z
 		#releases.liferay.com/portal/snapshot-7.1.x/201902130905/liferay-portal-tomcat-7.1.x.7z
-		#releases.liferay.com/portal/snapshot-master/201902131509/liferay-portal-tomcat-master.7z
+		releases.liferay.com/portal/snapshot-master/latest/liferay-portal-tomcat-master.7z
 		#files.liferay.com/private/ee/portal/snapshot-ee-6.2.x/201808160944/liferay-portal-tomcat-ee-6.2.x.zip
 		#files.liferay.com/private/ee/portal/snapshot-7.1.x-private/201808162051/liferay-portal-tomcat-7.1.x-private.zip
 	)
