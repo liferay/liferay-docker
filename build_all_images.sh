c535ce94f54e7c2cda8345f440e79d14204887f5
Dávid Balatoni <balcsida@gmail.com>
2021-06-10 10:28:11 +0000

    SRE-159 Add 7.1.10-dxp-24

--- build_all_images.sh
+++ build_all_images.sh
@@ -319,6 +319,13 @@ function build_bundle_images_dxp_71 {
 		"" \
 		dxp-23-7110,hotfix-5503-7110 \
 		files.liferay.com/private/ee/fix-packs/7.1.10/hotfix/liferay-hotfix-5503-7110.zip
+
+	build_bundle_image \
+		7.1.10-dxp-24 \
+		files.liferay.com/private/ee/portal/7.1.10-dxp-24/liferay-dxp-tomcat-7.1.10-dxp-24-slim-20210607121821009.7z \
+		"" \
+		dxp-24-7110,hotfix-5545-7110 \
+		files.liferay.com/private/ee/fix-packs/7.1.10/hotfix/liferay-hotfix-5545-7110.zip
 }
 
 function build_bundle_images_dxp_72 {
