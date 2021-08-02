1d0922dc5200859dc1f46964cb38ce3f0dce7e23
Brian Chan <brian.chan@liferay.com>
2021-08-02 19:43:51 -0300

    7.4 GA3

--- build_all_images.sh
+++ build_all_images.sh
@@ -481,11 +481,11 @@ function build_bundle_images_dxp_73 {
 
 function build_bundle_images_dxp_74 {
 	build_bundle_image \
-		7.4.11-ep2 \
-		files.liferay.com/private/ee/portal/7.4.11-ep2/liferay-dxp-tomcat-7.4.11-ep2-20210609203546147.7z
+		7.4.12-ep3 \
+		files.liferay.com/private/ee/portal/7.4.12/liferay-dxp-tomcat-7.4.12-ep3-20210728022041024.7z
 
 	build_bundle_image \
-		7.4.12-ep3-nightly \
+		7.4.13-ep4-nightly \
 		files.liferay.com/private/ee/portal/nightly/liferay-dxp-tomcat-7.4.10.7z
 }
 
@@ -513,7 +513,7 @@ function main {
 		releases.liferay.com/portal/7.1.3-ga4/liferay-ce-portal-tomcat-7.1.3-ga4-20190508171117552.7z
 		releases.liferay.com/portal/7.2.1-ga2/liferay-ce-portal-tomcat-7.2.1-ga2-20191111141448326.7z
 		releases.liferay.com/portal/7.3.7-ga8/liferay-ce-portal-tomcat-7.3.7-ga8-20210610183559721.7z
-		releases.liferay.com/portal/7.4.1-ga2/liferay-ce-portal-tomcat-7.4.1-ga2-20210609223456272.7z
+		releases.liferay.com/portal/7.4.2-ga3/liferay-ce-portal-tomcat-7.4.2-ga3-20210728053338694.7z
 		#releases.liferay.com/portal/snapshot-7.1.x/201902130905/liferay-portal-tomcat-7.1.x.7z
 		releases.liferay.com/portal/snapshot-master/latest/liferay-portal-tomcat-master.7z
 		#files.liferay.com/private/ee/portal/snapshot-ee-6.2.x/201808160944/liferay-portal-tomcat-ee-6.2.x.zip
