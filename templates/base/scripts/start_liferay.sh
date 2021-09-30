250965c225a589f3a5a0cbfb5b4169db9773649d
Ákos Kreutz <akos.kreutz@liferay.com>
2021-09-30 14:21:45 +0200

    SRE-269 Added a thread dump probe.

--- templates/base/scripts/start_liferay.sh
+++ templates/base/scripts/start_liferay.sh
@@ -5,6 +5,12 @@ function main {
 	echo "[LIFERAY] Starting ${LIFERAY_PRODUCT_NAME}. To stop the container with CTRL-C, run this container with the option \"-it\"."
 	echo ""
 
+	if [ "${LIFERAY_THREAD_DUMP_PROBE_ENABLED}" == "true" ]
+	then
+		cat $LIFERAY_THREAD_DUMP_DIRECTORY/*.tdump
+		rm $LIFERAY_THREAD_DUMP_DIRECTORY/*.tdump
+	fi
+
 	if [ "${LIFERAY_JPDA_ENABLED}" == "true" ]
 	then
 		exec ${LIFERAY_HOME}/tomcat/bin/catalina.sh jpda run
