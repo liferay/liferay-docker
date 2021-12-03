2c910abea784442669961667b1906ce2f8386471
Raymond Augé <raymond.auge@liferay.com>
2021-12-03 15:34:55 -0500

    DOCKER-65 SIGTERM is not propogated to the DXP process so Graceful shutdown is broken
    Prevent the entrypoint from forking on bash SHEBANG and breaking the signal handling meant to be on PID 1
    With suggestions from @thecosmicfrog
    
    Signed-off-by: Raymond Augé <raymond.auge@liferay.com>

--- templates/base/scripts/liferay_entrypoint.sh
+++ templates/base/scripts/liferay_entrypoint.sh
@@ -3,7 +3,11 @@
 source /usr/local/bin/_liferay_common.sh
 
 function handle_kill_TERM {
-	kill -TERM ${START_LIFERAY_PID}
+	exec 5<>/dev/tcp/localhost/8005 && echo "SHUTDOWN" >&5
+
+	if [ $? -gt 0 ]; then
+		kill -TERM ${START_LIFERAY_PID}
+	fi
 }
 
 function main {
