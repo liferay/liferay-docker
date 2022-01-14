49bfdf1682405c195778604b885e31a27ed52234
Dávid Balatoni <balcsida@gmail.com>
2022-01-14 15:47:03 +0000

    DOCKER-69 Rename PASSWORD to TOKEN

--- _common.sh
+++ _common.sh
@@ -119,16 +119,16 @@ function get_tomcat_version {
 # Login to Docker Hub
 # GLOBALS:
 #   DOCKER_HUB_USERNAME
-#   DOCKER_HUB_PASSWORD (or token)
+#   DOCKER_HUB_TOKEN (or password)
 #######################################
 function login_docker_hub {
-	if [ -n "${DOCKER_HUB_USERNAME}" ] && [ -n "${DOCKER_HUB_PASSWORD}" ]
+	if [ -n "${DOCKER_HUB_USERNAME}" ] && [ -n "${DOCKER_HUB_TOKEN}" ]
 	then
 		echo ""
 		echo "Logging in to Docker Hub."
 		echo ""
 
-		echo "${DOCKER_HUB_PASSWORD}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin
+		echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin
 	fi
 }
 
