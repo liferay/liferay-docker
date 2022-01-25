44faf4709a506356d31e917ff8c84f55a3ce322c
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-01-25 16:41:03 +0100

    DOCKER-71 Examples for the Connector image - this folder can be mounted to /mnt/liferay to test

---
+++ examples/connector/connectors/every_minute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "The every minute job runs and sleeps for 5 seconds."
+
+sleep 5
---
+++ examples/connector/connectors/every_two_minutes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "The every 2 minutes job runs and sleeps for 5 seconds."
+
+sleep 5
