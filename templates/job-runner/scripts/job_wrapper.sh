1e3639890d25ef9247bf0ada15845e0ecaf7775f
Brian Chan <brian.chan@liferay.com>
2022-02-08 13:52:30 -0300

    DOCKER-71 Simplify

--- templates/job-runner/scripts/job_wrapper.sh
+++ templates/job-runner/scripts/job_wrapper.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 function main {
-	run_job "${1}"
-}
-
-function run_job {
-	local job="${1}"
-
 	echo ""
-	echo "Starting to run job ${job}."
+	echo "Starting job ${1}."
 	echo ""
 
 	time /bin/bash "/mnt/liferay/jobs/${1}.sh"
