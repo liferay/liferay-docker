4eed406a846a22e7d31b8c67cd1b34fffd7f25a7
Ákos Kreutz <akos.kreutz@liferay.com>
2022-03-30 15:23:34 +0200

    DOCKER-82 Created new build scripts

---
+++ build_jdk11_jdk8_image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source ./_common.sh
+
+function build_docker_image {
+	local image_version=$(./release_notes.sh get-version)
+
+	DOCKER_IMAGE_TAGS=()
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/jdk11-jdk8:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/jdk11-jdk8")
+
+	docker build \
+		--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+		--build-arg LABEL_NAME="Liferay JDK11 JDK8" \
+		--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+		--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+		--build-arg LABEL_VERSION="${image_version}" \
+		$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+		"${TEMP_DIR}" || exit 1
+}
+
+function main {
+	make_temp_directory templates/jdk11-jdk8
+
+	build_docker_image
+
+	log_in_to_docker_hub
+
+	push_docker_images "${1}"
+
+	clean_up_temp_directory
+}
+
+main "${@}"
