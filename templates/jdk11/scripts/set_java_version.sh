319bd982d2a59b2084eeace8e8d02ee30c53e9b1
Ákos Kreutz <akos.kreutz@liferay.com>
2022-03-30 17:31:15 +0200

    DOCKER-82 update-java-alternatives bugfix

--- templates/jdk11/scripts/set_java_version.sh
+++ templates/jdk11/scripts/set_java_version.sh
@@ -8,21 +8,24 @@ function create_symlink {
 }
 
 function main {
-	local architecture=$(dpkg --print-architecture)
-
-	create_symlink "${architecture}" "zulu-8"
-	create_symlink "${architecture}" "zulu-11"
-
 	if [ -n "${JAVA_VERSION}" ]
 	then
+
+		if [[ ! -e "/usr/lib/jvm/zulu11" ]] && [[ ! -e "/usr/lib/jvm/zulu8" ]]
+		then
+			local zulu_version=$(echo "${JAVA_VERSION}" | tr -dc '0-9')
+			update-java-alternatives -s zulu-"${zulu_version}"-"${architecture}"
+		fi
+
+		local architecture=$(dpkg --print-architecture)
+
+		create_symlink "${architecture}" "zulu-8"
+		create_symlink "${architecture}" "zulu-11"
+
 		if [ -e "/usr/lib/jvm/${JAVA_VERSION}" ]
 		then
 			JAVA_HOME=/usr/lib/jvm/${JAVA_VERSION}
 			PATH=/usr/lib/jvm/${JAVA_VERSION}/bin/:${PATH}
-
-			local zulu_version=$(echo "${JAVA_VERSION}" | tr -dc '0-9')
-
-			update-java-alternatives -s zulu-"${zulu_version}"-"${architecture}"
 
 			echo "[LIFERAY] Using ${JAVA_VERSION} JDK. You can use another JDK by setting the \"JAVA_VERSION\" environment varible."
 			echo ""
