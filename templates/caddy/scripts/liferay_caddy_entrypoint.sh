90cbf203162f18eb35c29c7a034336bafa26d6ae
Brian Chan <brian.chan@liferay.com>
2022-06-17 14:00:41 -0700

    DOCKER-130 SF

--- templates/caddy/scripts/liferay_caddy_entrypoint.sh
+++ templates/caddy/scripts/liferay_caddy_entrypoint.sh
@@ -1,9 +1,12 @@
-for i in $(echo $CORS_ALLOWED_ORIGIN | tr "," "\n"); do
-    ADDRESS="https://${i}" # strictly https
-    cat >> /etc/caddy.d/cors.txt << EOF
-    @origin${ADDRESS} header Origin ${ADDRESS}
+for i in $(echo ${CORS_ALLOWED_ORIGIN} | tr "," "\n")
+do
+    ADDRESS="https://${i}"
+
+	cat >> /etc/caddy.d/cors.txt << EOF
+	@origin${ADDRESS} header Origin ${ADDRESS}
     header @origin${ADDRESS} Access-Control-Allow-Origin "${ADDRESS}"
     header @origin${ADDRESS} Vary Origin
 EOF
 done
+
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
