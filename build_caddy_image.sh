afe738d3d7512bb1aa3b7789ddb258bb62d90fc1
Brian Chan <brian.chan@liferay.com>
2022-06-17 12:09:00 -0700

    DOCKER-130 Remove useless property because the image name has the info

--- build_caddy_image.sh
+++ build_caddy_image.sh
@@ -4,11 +4,11 @@ source ./_common.sh
 
 function build_docker_image {
 	local base_image_version=$(./release_notes.sh get-version)
-	local caddy_image_version=0.1.0
+	local image_version=0.1.0
 
 	DOCKER_IMAGE_TAGS=()
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/caddy:${caddy_image_version}-d${base_image_version}-${TIMESTAMP}")
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/caddy:${caddy_image_version%.*}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/caddy:${image_version}-d${base_image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/caddy:${image_version%.*}")
 	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/caddy")
 
 	if [ "${1}" == "push" ]
@@ -17,7 +17,6 @@ function build_docker_image {
 
 		docker buildx build \
 			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
-			--build-arg LABEL_CADDY_VERSION="${caddy_image_version}" \
 			--build-arg LABEL_NAME="Liferay Caddy" \
 			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
 			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
@@ -32,7 +31,6 @@ function build_docker_image {
 
 		docker build \
 			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
-			--build-arg LABEL_CADDY_VERSION="${caddy_image_version}" \
 			--build-arg LABEL_NAME="Liferay Caddy" \
 			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
 			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
