f9001ece86167d2e8f3abab4457d19d94d8fc784
Brian Chan <brian.chan@liferay.com>
2022-06-19 17:06:10 -0700

    DOCKER-130 Use a function

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -1,12 +1,16 @@
-for i in $(echo ${LIFERAY_CADDY_CORS_ALLOWED_ORIGIN} | tr "," "\n")
-do
-    ADDRESS="https://${i}"
+function main {
+    for i in $(echo ${LIFERAY_CADDY_CORS_ALLOWED_ORIGIN} | tr "," "\n")
+    do
+        ADDRESS="https://${i}"
 
-	cat >> /etc/caddy.d/cors.txt << EOF
-	@origin${ADDRESS} header Origin ${ADDRESS}
-    header @origin${ADDRESS} Access-Control-Allow-Origin "${ADDRESS}"
-    header @origin${ADDRESS} Vary Origin
+    	cat >> /etc/caddy.d/cors.txt << EOF
+    	@origin${ADDRESS} header Origin ${ADDRESS}
+        header @origin${ADDRESS} Access-Control-Allow-Origin "${ADDRESS}"
+        header @origin${ADDRESS} Vary Origin
 EOF
-done
+    done
 
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+    caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+}
+
+main
