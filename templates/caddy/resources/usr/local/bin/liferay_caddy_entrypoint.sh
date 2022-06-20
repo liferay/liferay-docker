6b5c283f180be6e426d3912886654fdbf8b31006
Brian Chan <brian.chan@liferay.com>
2022-06-19 17:16:33 -0700

    DOCKER-130 Rename

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -1,12 +1,12 @@
 function main {
 	for i in $(echo ${LIFERAY_CADDY_CORS_ALLOWED_ORIGIN} | tr "," "\n")
 	do
-		local address="https://${i}"
+		local url="https://${i}"
 
 		cat >> /etc/caddy.d/cors.txt << EOF
-@origin${address} header Origin ${address}
-header @origin${address} Access-Control-Allow-Origin "${address}"
-header @origin${address} Vary Origin
+@origin${url} header Origin ${url}
+header @origin${url} Access-Control-Allow-Origin "${url}"
+header @origin${url} Vary Origin
 EOF
 	done
 
