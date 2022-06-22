20c963588ced25e07e8759ec9140d4f03e338942
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-06-22 14:01:11 +0200

    SRE-1245 Adding the search service

--- managed-dxp/build_services.sh
+++ managed-dxp/build_services.sh
@@ -75,6 +75,31 @@ function build_liferay_dxp {
 	compose_add 1 "        - /opt/shared-volume:/opt/shared-volume"
 }
 
+function build_search {
+	docker build \
+		--tag search:${VERSION} \
+		templates/search
+
+	compose_add 1 "${SERVICE}:"
+    compose_add 1 "    environment:"
+    compose_add 1 "        - discovery.type=single-node"
+    compose_add 1 "        - xpack.ml.enabled=false"
+    compose_add 1 "        - xpack.monitoring.enabled=false"
+    compose_add 1 "        - xpack.security.enabled=false"
+    compose_add 1 "        - xpack.sql.enabled=false"
+    compose_add 1 "        - xpack.watcher.enabled=false"
+    compose_add 1 "    healthcheck:"
+    compose_add 1 "        interval: 40s"
+    compose_add 1 "        retries: 3"
+    compose_add 1 "        test: curl localhost:9200/_cat/health | grep green"
+    compose_add 1 "        timeout: 5s"
+    compose_add 1 "    image: search:${VERSION}"
+    compose_add 1 "    mem_limit: 8G"
+    compose_add 1 "    ports:"
+    compose_add 1 "        - \"9200:9200\""
+    compose_add 1 "        - \"9300:9300\""
+}
+
 function build_webserver {
 	local balance_members=$(get_config ".\"${SERVICE}\".balance_members")
 
