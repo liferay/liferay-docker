58aab857856f0b0438404d8fe1dc1d41c6b3cea3
Raymond Augé <raymond.auge@liferay.com>
2022-06-27 12:33:36 -0400

    LCD-14446 Configure CORS in the caddy docker image using lxc-dxp-metadata virtual host domains

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -1,14 +1,19 @@
 function main {
-	for i in $(echo ${LIFERAY_CADDY_CORS_ALLOWED_ORIGIN} | tr "," "\n")
-	do
-		local url="https://${i}"
+	local domains_file=/etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.domains
 
-		cat >> /etc/caddy.d/liferay_caddy_file << EOF
+	if [ -f $domains_file ]
+	then
+		for i in $(cat $domains_file)
+		do
+			local url="https://${i}"
+
+			cat >> /etc/caddy.d/liferay_caddy_file << EOF
 @origin${url} header Origin ${url}
 header @origin${url} Access-Control-Allow-Origin "${url}"
 header @origin${url} Vary Origin
 EOF
-	done
+		done
+	fi
 
 	caddy run --adapter caddyfile --config /etc/caddy/Caddyfile
 }
