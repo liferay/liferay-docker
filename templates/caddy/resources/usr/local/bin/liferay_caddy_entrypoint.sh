af60e7fa039d44e454ee755b9029e19907ac9172
Brian Chan <brian.chan@liferay.com>
2022-06-27 11:29:24 -0700

    LCD-14446 Simplify

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -1,19 +1,14 @@
 function main {
-	local domains_file=/etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.domains
+	for i in $(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.domains 2>/dev/null)
+	do
+		local url="https://${i}"
 
-	if [ -f $domains_file ]
-	then
-		for i in $(cat $domains_file)
-		do
-			local url="https://${i}"
-
-			cat >> /etc/caddy.d/liferay_caddy_file << EOF
+		cat >> /etc/caddy.d/liferay_caddy_file << EOF
 @origin${url} header Origin ${url}
 header @origin${url} Access-Control-Allow-Origin "${url}"
 header @origin${url} Vary Origin
 EOF
-		done
-	fi
+	done
 
 	caddy run --adapter caddyfile --config /etc/caddy/Caddyfile
 }
