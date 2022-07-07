4b2ce54e6bb5516129dd1a5641878b312615e863
Raymond Augé <raymond.auge@liferay.com>
2022-07-07 16:37:28 -0400

    LCD-14505 allow testing with http
    Just add '-e ALLOW_INSECURE=true' to the docker run command

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -8,6 +8,17 @@ function main {
 header @origin${url} Access-Control-Allow-Origin "${url}"
 header @origin${url} Vary Origin
 EOF
+
+		if [ "${ALLOW_INSECURE}" == "true" ]
+		then
+			url="http://${i}"
+
+			cat >> /etc/caddy.d/liferay_caddy_file << EOF
+@origin${url} header Origin ${url}
+header @origin${url} Access-Control-Allow-Origin "${url}"
+header @origin${url} Vary Origin
+EOF
+		fi
 	done
 
 	caddy run --adapter caddyfile --config /etc/caddy/Caddyfile
