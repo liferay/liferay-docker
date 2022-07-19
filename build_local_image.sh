038fc280f09b6d5b5eb2470745273a114170f4d7
Raymond Augé <raymond.auge@liferay.com>
2022-07-19 14:14:33 -0400

    DOCKER-129 Improve the process of locally built image
    For example:
    
    ]$ ./build_local_image.sh ~/bundles/ dxp snapshot no_warm_up no_test_image
    
    Builds the image in less than 30 seconds

--- build_local_image.sh
+++ build_local_image.sh
@@ -26,14 +26,14 @@ function build_docker_image {
 function check_usage {
 	if [ ! -n "${3}" ]
 	then
-		echo "Usage: ${0} path-to-bundle image-name version <push>"
+		echo "Usage: ${0} path-to-bundle image-name version <push> [no_warm_up] [no_test_image]"
 		echo ""
 		echo "Example: ${0} ../bundles/master portal-snapshot demo-cbe09fb0 <push>"
 
 		exit 1
 	fi
 
-	check_utils curl docker java
+	check_utils curl docker java rsync
 }
 
 function main {
@@ -43,21 +43,27 @@ function main {
 
 	prepare_temp_directory "${@}"
 
-	prepare_tomcat
+	prepare_tomcat "${@}"
+
+	curl -q https://raw.githubusercontent.com/liferay/liferay-portal/master/tools/servers/tomcat/bin/setenv.sh \
+		-o "${TEMP_DIR}/liferay/tomcat/bin/setenv.sh"
 
 	build_docker_image "${@}"
 
-	test_docker_image
-
-	log_in_to_docker_hub
-
-	push_docker_images "${4}"
+	test_docker_image "${@}"
 
 	clean_up_temp_directory
 }
 
 function prepare_temp_directory {
-	cp -a "${1}" "${TEMP_DIR}/liferay"
+	rsync -aq "${1}" "${TEMP_DIR}/liferay" \
+		--exclude '*.zip' \
+		--exclude 'portal-setup-wizard.properties' \
+		--exclude 'data/elasticsearch*' \
+		--exclude 'logs/*' \
+		--exclude 'osgi/state' \
+		--exclude 'osgi/test' \
+		--exclude 'tmp'
 }
 
 main "${@}"
