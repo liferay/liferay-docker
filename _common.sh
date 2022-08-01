7b6586c598aa9ba8aec1c36bf73116de9a7930c8
Brian Chan <brian.chan@liferay.com>
2022-08-01 11:43:02 -0300

    DOCKER-129 fix rebase error

--- _common.sh
+++ _common.sh
@@ -201,7 +201,7 @@ function prepare_tomcat {
 	rm -fr "${TEMP_DIR}"/liferay/tomcat/logs/*
 }
 
-function remove_temp_dockerfile_platform_variable {
+function remove_temp_dockerfile_target_platform {
 	sed -i'.bak' 's/${TARGETARCH}'/$(get_current_arch)/ "${TEMP_DIR}"/Dockerfile
 	sed -i'' 's/--platform=${TARGETPLATFORM} //g' "${TEMP_DIR}"/Dockerfile
 }
