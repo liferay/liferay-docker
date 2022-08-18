5a7e7c3d556752cfe04f4cf6d0c36e42a6dcd2e8
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-08-18 20:07:30 +0200

    SRE-1219 Remove the need for separate entrypoint for crontab

--- managed-dxp/templates/backup/resources/usr/local/bin/liferay_backup_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Cron expression: ${LIFERAY_BACKUP_CRON_EXPRESSION}"
-echo "${LIFERAY_BACKUP_CRON_EXPRESSION} /usr/local/bin/register_job.sh backup" >> "/mnt/liferay/job-crontab"
-
-/usr/local/bin/liferay_job_runner_entrypoint.sh
