92a69ceb297d94f54629630e02033576354e812e
Brian Chan <brian.chan@liferay.com>
2022-09-07 09:09:18 -0300

    Web server

--- managed-dxp/templates/web-server/resources/usr/local/bin/web_server_entrypoint.sh
+++ managed-dxp/templates/web-server/resources/usr/local/bin/web_server_entrypoint.sh
@@ -1,54 +1,58 @@
 #!bin/bash
 
-function add_conf_line {
-	echo "${1}" >> "/etc/apache2/sites-available/liferay.conf"
-}
-
 function generate_liferay_conf {
-	add_conf_line "<VirtualHost *:80>"
-	add_conf_line "    DocumentRoot /var/www/html"
-	add_conf_line "    ServerAdmin webmaster@localhost"
-	add_conf_line ""
-	add_conf_line "    <Proxy \"balancer://cluster\">"
+	function write {
+		echo "${1}" >> "/etc/apache2/sites-available/liferay.conf"
+	}
+
+	write "<VirtualHost *:80>"
+	write "    DocumentRoot /var/www/html"
+	write "    ProxyPreserveHost On"
+	write "    ProxyPass \"/\" \"balancer://cluster/\""
+	write "    ServerAdmin webmaster@localhost"
+	write ""
+	write "    <Proxy \"balancer://cluster\">"
 
 	for balance_member in ${ORCA_WEB_SERVER_BALANCE_MEMBERS//,/ }
 	do
+		local ajp_address="${balance_member##*::}"
 		local route="${balance_member%%::*}"
-		local host_port="${balance_member##*::}"
-		add_conf_line "        BalancerMember \"ajp://${host_port}\" route=${route} loadfactor=1"
+
+		write "        BalancerMember \"ajp://${ajp_address}\" loadfactor=1 route=${route}"
 	done
 
-	add_conf_line "        ProxySet stickysession=JSESSIONID"
-	add_conf_line "    </Proxy>"
-	add_conf_line "    ProxyPreserveHost On"
-	add_conf_line "    ProxyPass \"/\" \"balancer://cluster/\""
-	add_conf_line "</VirtualHost>"
+	write "        ProxySet stickysession=JSESSIONID"
+	write "    </Proxy>"
+	write "</VirtualHost>"
 
-	echo "Generated liferay site configuration: "
-	cat "/etc/apache2/sites-available/liferay.conf"
+	echo "Generated /etc/apache2/sites-available/liferay.conf."
+
+	cat /etc/apache2/sites-available/liferay.conf
 }
 
 function main {
 	generate_liferay_conf
 
-	setup_sites
+	set_up_sites
 
 	start_apache2
 }
 
-function setup_sites {
+function set_up_sites {
 	a2dissite "000-default.conf"
 	a2ensite "liferay.conf"
 }
 
 function start_apache2 {
-	source "/etc/apache2/envvars"
-	mkdir "/var/run/apache2"
-	chown www-data:www-data "/var/run/apache2"
+	mkdir /var/run/apache2
+
+	chown www-data:www-data /var/run/apache2
+
+	source /etc/apache2/envvars
 
 	/usr/sbin/apache2 -k start
 
-	tail -f "/var/log/apache2/error.log"
+	tail -f /var/log/apache2/error.log
 }
 
 main
