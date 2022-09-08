268715ee1968211fbd2c446d0650fe39872999f4
Brian Chan <brian.chan@liferay.com>
2022-09-08 09:12:44 -0300

    SF

--- orca/templates/liferay/resources/usr/local/bin/install_patch_on_build.sh
+++ orca/templates/liferay/resources/usr/local/bin/install_patch_on_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(find "/opt/liferay/patching-tool/patches" -maxdepth 1 -type f -name "liferay-*.zip" | wc -l) == 1 ]
+if [ $(find "/opt/liferay/patching-tool/patches" -maxdepth 1 -name "liferay-*.zip" -type f | wc -l) == 1 ]
 then
 	/opt/liferay/patching-tool/patching-tool.sh install
 
