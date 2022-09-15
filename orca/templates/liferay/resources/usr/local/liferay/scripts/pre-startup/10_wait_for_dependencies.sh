ab8ebf0899d02c107311c88de6993115155c1db6
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-09-15 14:19:04 +0200

    Orca SF

--- orca/templates/liferay/resources/usr/local/liferay/scripts/pre-startup/10_wait_for_dependencies.sh
+++ orca/templates/liferay/resources/usr/local/liferay/scripts/pre-startup/10_wait_for_dependencies.sh
@@ -3,39 +3,36 @@
 function wait_for_mysql {
 	local jdbc_driver_class_name="${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_DRIVER_UPPERCASEC_LASS_UPPERCASEN_AME}"
 
-	if [ ! -n "${jdbc_driver_class_name}" ]
+	if [[ "${jdbc_driver_class_name}" != *mariadb* ]] && [[ "${jdbc_driver_class_name}" != *mysql* ]]
 	then
 		return
 	fi
 
-	if (echo "${jdbc_driver_class_name}" | grep -i "mariadb" &>/dev/null) || (echo "${jdbc_driver_class_name}" | grep -i "mysql" &>/dev/null)
+	local db_host="${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_URL}"
+
+	db_host="${db_host##*://}"
+	db_host="${db_host%%/*}"
+	db_host="${db_host%%:*}"
+
+	local db_password=${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD}
+
+	if [ -n "${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD_FILE}" ]
 	then
-		local db_host="${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_URL}"
-
-		db_host="${db_host##*://}"
-		db_host="${db_host%%/*}"
-		db_host="${db_host%%:*}"
-
-		local db_password=${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD}
-
-		if [ -n "${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD_FILE}" ]
-		then
-			db_password=$(cat "${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD_FILE}")
-		fi
-
-		local db_username=${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_USERNAME}
-
-		echo "Connecting to database server ${db_username}@${db_host}."
-
-		while ! (echo "select 1" | mysql -h "${db_host}" -p"${db_password}" -u "${db_username}" &>/dev/null)
-		do
-			echo "Waiting for database server ${db_username}@${db_host}."
-
-			sleep 3
-		done
-
-		echo "Database server ${db_username}@${db_host} is available."
+		db_password=$(cat "${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_PASSWORD_FILE}")
 	fi
+
+	local db_username=${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_USERNAME}
+
+	echo "Connecting to database server ${db_username}@${db_host}."
+
+	while ! (echo "select 1" | mysql -h "${db_host}" -p"${db_password}" -u "${db_username}" &>/dev/null)
+	do
+		echo "Waiting for database server ${db_username}@${db_host}."
+
+		sleep 3
+	done
+
+	echo "Database server ${db_username}@${db_host} is available."
 }
 
 function wait_for_search {
