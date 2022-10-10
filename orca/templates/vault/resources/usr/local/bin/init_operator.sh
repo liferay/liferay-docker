cbf639e0dcdbda2b58c1d8815a370426016e6206
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-10-10 12:46:28 +0200

    SRE-1719 Only require on unseal key by default

--- orca/templates/vault/resources/usr/local/bin/init_operator.sh
+++ orca/templates/vault/resources/usr/local/bin/init_operator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vault operator init
+vault operator init -key-shares=1 -key-threshold=1
 
 echo ""
 echo "Next steps:"
@@ -9,4 +9,6 @@ echo "1. Store the generated keys in 1Password."
 echo ""
 echo "2. Export the environment variable ORCA_VAULT_TOKEN to the initial root token on this container (see above)."
 echo ""
-echo "3. Run init_secrets.sh"
+echo "3. Run orca unseal on the host."
+echo ""
+echo "4. Run init_secrets.sh"
