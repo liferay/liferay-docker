31a89e4ebecb8c9e0484e9ec947da27847b793fc
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-10-26 12:50:41 +0200

    SRE-1942 Simplify

--- orca/templates/liferay/resources/usr/local/liferay/scripts/pre-startup/02_start_zabbix_agent.sh
+++ orca/templates/liferay/resources/usr/local/liferay/scripts/pre-startup/02_start_zabbix_agent.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-function main {
-	if [ "${ORCA_LIFERAY_ZABBIX_AGENT_ENABLED}" == "true" ]
-	then
-		echo ""
-		echo "[LIFERAY] Starting Zabbix Agent2."
-		echo ""
+if [ "${ORCA_LIFERAY_ZABBIX_AGENT_ENABLED}" == "true" ]
+then
+	echo "Starting zabbix agent."
 
-		/usr/sbin/zabbix_agent2 -c /etc/zabbix/zabbix_agent2.conf &
-	fi
-}
-
-main
+	/usr/sbin/zabbix_agent2 -c /etc/zabbix/zabbix_agent2.conf &
+else
+	echo "Set the environment variable ORCA_LIFERAY_ZABBIX_AGENT_ENABLED to true to start the Zabbix agent."
+fi
