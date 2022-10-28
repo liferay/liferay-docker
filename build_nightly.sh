fc38baa4656d95997d220219d1dfebc45826f1ee
Brian Chan <brian.chan@liferay.com>
2022-10-28 11:25:01 -0300

    Because crontab still kicks my butt

--- build_nightly.sh
+++ build_nightly.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+touch /tmp/build_nightly-${RANDOM}.txt
+echo "${PATH}" > /tmp/build_nightly-${RANDOM}.txt
+echo $(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)") > /tmp/build_nightly-${RANDOM}.txt
+
 cd $(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)")
 
 LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
