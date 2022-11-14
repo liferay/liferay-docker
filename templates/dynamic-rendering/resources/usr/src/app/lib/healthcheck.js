e1c00f5ee7951392187b4b40e23fcbd94c287f5c
Keven Leone <keven.leone@liferay.com>
2022-11-14 15:17:28 -0300

    DOCKER-121 SF with Prettier

--- templates/dynamic-rendering/resources/usr/src/app/lib/healthcheck.js
+++ templates/dynamic-rendering/resources/usr/src/app/lib/healthcheck.js
@@ -1,28 +1,28 @@
 const http = require('http');
 
 const EXIT_CODE = {
-  ERROR: 1,
-  SUCCESS: 0,
+	ERROR: 1,
+	SUCCESS: 0,
 };
 
 const options = {
-  path: 'http://localhost:3000/http://localhost:3001',
-  port: 3000,
-  timeout: 2000,
+	path: 'http://localhost:3000/http://localhost:3001',
+	port: 3000,
+	timeout: 2000,
 };
 
 const healthCheck = http.request(options, (response) => {
-  console.log(`HEALTHCHECK STATUS: ${response.statusCode}`);
+	console.log(`HEALTHCHECK STATUS: ${response.statusCode}`);
 
-  process.exit(
-    response.statusCode == 200 ? EXIT_CODE.SUCCESS : EXIT_CODE.ERROR
-  );
+	process.exit(
+		response.statusCode == 200 ? EXIT_CODE.SUCCESS : EXIT_CODE.ERROR
+	);
 });
 
 healthCheck.on('error', function (error) {
-  console.error(`HEALTHCHECK ERROR: ${error.message}`);
+	console.error(`HEALTHCHECK ERROR: ${error.message}`);
 
-  process.exit(EXIT_CODE.ERROR);
+	process.exit(EXIT_CODE.ERROR);
 });
 
 healthCheck.end();
--- templates/dynamic-rendering/resources/usr/src/app/server.js
+++ templates/dynamic-rendering/resources/usr/src/app/server.js
@@ -5,25 +5,25 @@ const http = require('http');
 const prerender = require('prerender');
 
 const prerenderServer = prerender({
-  chromeFlags: [
-    '--disable-dev-shm-usage',
-    '--disable-gl-drawing-for-tests',
-    '--disable-gpu',
-    '--hide-scrollbars',
-    '--no-proxy-server',
-    '--no-sandbox',
-    '--proxy-server="direct://"',
-    '--proxy-bypass-list=*',
-    '--remote-debugging-port=9222',
-    '--user-data-dir=/tmp/chrome-data',
-  ],
-  chromeLocation: '/usr/bin/google-chrome',
+	chromeFlags: [
+		'--disable-dev-shm-usage',
+		'--disable-gl-drawing-for-tests',
+		'--disable-gpu',
+		'--hide-scrollbars',
+		'--no-proxy-server',
+		'--no-sandbox',
+		'--proxy-server="direct://"',
+		'--proxy-bypass-list=*',
+		'--remote-debugging-port=9222',
+		'--user-data-dir=/tmp/chrome-data',
+	],
+	chromeLocation: '/usr/bin/google-chrome',
 });
 
 const isCacheEnabled = Number(process.env.MEMORY_CACHE) || 0;
 
 if (isCacheEnabled === 1) {
-  prerenderServer.use(cache);
+	prerenderServer.use(cache);
 }
 
 prerenderServer.use(prerender.blacklist());
@@ -33,8 +33,8 @@ prerenderServer.use(prerender.removeScriptTags());
 prerenderServer.start();
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('status: ok');
+	res.writeHead(200);
+	res.end('status: ok');
 };
 
 const httpServer = http.createServer(requestListener);
