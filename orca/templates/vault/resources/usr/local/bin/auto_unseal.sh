44fc86ed2807d1390eb3d1b8a41b366cfdbd1169
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-01-27 09:09:58 +0100

    SRE-2145 Too many quotation marks

--- orca/templates/vault/resources/usr/local/bin/auto_unseal.sh
+++ orca/templates/vault/resources/usr/local/bin/auto_unseal.sh
@@ -4,4 +4,4 @@ source /usr/local/bin/_common.sh
 
 wait_for_operator "\"standby\":true"
 
-vault operator unseal "$(cat /opt/liferay/vault/data/unseal_key)""
+vault operator unseal "$(cat /opt/liferay/vault/data/unseal_key)"
