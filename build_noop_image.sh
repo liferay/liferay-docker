14a6e35d84afefbadb4419dbd19f986485f3aba1
Brian Chan <brian.chan@liferay.com>
2023-02-23 16:35:45 +0100

    LPS-176095 See 3a72fad2

--- build_noop_image.sh
+++ build_noop_image.sh
@@ -6,8 +6,8 @@ function build_docker_image {
 	local image_version=$(./release_notes.sh get-version)
 
 	DOCKER_IMAGE_TAGS=()
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/noop:${image_version}-${TIMESTAMP}")
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}liferay/noop")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/noop:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/noop")
 
 	if [ "${1}" == "push" ]
 	then
