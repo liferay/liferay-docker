06931197be7995045987d0ed658976c46c9bd93a
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-02-27 15:15:31 +0100

    DOCKER-176 SF

--- templates/bundle/resources/usr/local/bin/dl_inspect.sh
+++ templates/bundle/resources/usr/local/bin/dl_inspect.sh
@@ -3,85 +3,71 @@
 function check_usage {
 	if [ ! -e document_library ]
 	then
-		cd /opt/liferay/data &>/dev/null
-	fi
+		if [ -e /opt/liferay/data ]
+		then
+			cd /opt/liferay/data || exit 3
+		else
+			echo "Run this script from the Liferay data directory, which contains document_library."
 
-	if [ ! -e document_library ]
-	then
-		echo "Run this script from the Liferay data directory, which contains document_library."
-
-		exit 1
-	fi
-}
-
-function generated_dir_size {
-	local size=$(du -s ${1} 2>/dev/null)
-
-	if [ -n "${size}" ]
-	then
-		size=$(echo ${size} | sed -e s/[^0-9]*//g)
-		size=$((size / 1024))
-
-		local count=$(find ${1} -type f | wc -l)
-
-		echo ${1},${count},${count},${size},${size}
+			exit 1
+		fi
 	fi
 }
 
 function generate_data {
-	rm -fr ${TMP_DIR}
-	mkdir -p ${TMP_DIR}
+	rm -fr "${TMP_DIR}"
+	mkdir -p "${TMP_DIR}"
 
-	cd document_library
+	cd document_library || exit 3
 
 	pwd=$(pwd)
 
 	for companyId in *
 	do
-		if [ ! -d ${pwd}/${companyId} ]
+		if [ ! -d "${pwd}/${companyId}" ]
 		then
 			continue
 		fi
 
-		cd ${pwd}/${companyId}
+		cd "${pwd}/${companyId}" || exit 3
 
-		mkdir -p ${TMP_DIR}/${companyId}
+		mkdir -p "${TMP_DIR}/${companyId}"
 
-		cd ${pwd}/${companyId}/0
+		cd "${pwd}/${companyId}/0" || exit 3
 
 		for dir in adaptive document_preview document_thumbnail
 		do
-			generated_dir_size ${dir} >> ${TMP_DIR}/${companyId}/generated_files
+			generated_dir_size "${dir}" >> "${TMP_DIR}/${companyId}/generated_files"
 		done
 
-		cd ${pwd}/${companyId}
+		cd "${pwd}/${companyId}" || exit 3
 
 		for repository in *
 		do
-			if [[ ${repository} -eq 0 ]] || [ ! -d ${pwd}/${companyId}/${repository} ] || (! ls ${pwd}/${companyId}/${repository} | grep . &>/dev/null)
+			if [[ "${repository}" -eq 0 ]] || [ ! -d "${pwd}/${companyId}/${repository}" ] || [[ $(find "${pwd}/${companyId}/${repository}" -maxdepth 1 -type f | wc -l 2>/dev/null) -eq 0 ]]
 			then
 				continue
 			fi
 
-			cd ${pwd}/${companyId}/${repository}
+			cd "${pwd}/${companyId}/${repository}" || exit 3
 
 			for file in *
 			do
-				cd ${pwd}/${companyId}/${repository}/${file}
+				cd "${pwd}/${companyId}/${repository}/${file}" || exit 3
 
-				if (ls | grep . &>/dev/null)
+				if [[ $(find . -maxdepth 1 -type f | wc -l) -gt 0 ]]
 				then
 					for version in *
 					do
-						local full_type=$(file -b ${version})
+						local full_type=$(file -b "${version}")
 						local type=${full_type%% *}
 
 						if [[ ${type} == "Zip" ]]
 						then
-							if (! unzip -l ${version} &>/dev/null)
+							if (! unzip -l "${version}" &>/dev/null)
 							then
 								type="BrokenZip"
-							elif (unzip -l ${version} | grep manifest.xml &>/dev/null)
+							elif (unzip -l "${version}" | grep manifest.xml &>/dev/null)
 							then
 								type="LAR"
 							fi
@@ -89,17 +75,17 @@ function generate_data {
 
 						if [[ ${type} == "ISO" ]]
 						then
-							if (echo ${full_type} | grep MP4 &>/dev/null)
+							if (echo "${full_type}" | grep MP4 &>/dev/null)
 							then
 								type=MP4
 							fi
 						fi
 
-						local size=$(stat --printf="%s" ${version})
-						local md5=$(md5sum ${version} | sed -e "s/\ .*//")
+						local size=$(stat --printf="%s" "${version}")
+						local md5=$(md5sum "${version}" | sed -e "s/\ .*//")
 
-						echo ${size} ${md5} >> ${TMP_DIR}/${companyId}/type_$type
-						echo "${companyId}/${repository}/${file}/${version} ${type} ${size} ${md5} ${full_type})" >> ${TMP_DIR}/${companyId}/all
+						echo "${size} ${md5}" >> "${TMP_DIR}/${companyId}/type_$type"
+						echo "${companyId}/${repository}/${file}/${version} ${type} ${size} ${md5} ${full_type})" >> "${TMP_DIR}/${companyId}/all"
 					done
 				fi
 			done
@@ -107,6 +93,19 @@ function generate_data {
 	done
 }
 
+function generated_dir_size {
+	local size=$(du -s "${1}" 2>/dev/null)
+
+	if [ -n "${size}" ]
+	then
+		size=$(echo "${size}" | sed -e s/[^0-9]*//g)
+		size=$((size / 1024))
+
+		local count=$(find "${1}" -type f | wc -l)
+
+		echo "${1},${count},${count},${size},${size}"
+	fi
+}
 
 function main {
 	TMP_DIR=/tmp/dl_inspect_cache
@@ -124,11 +123,11 @@ function main {
 }
 
 function print_data {
-	cd ${TMP_DIR}
+	cd "${TMP_DIR}" || exit 3
 
 	for companyId in *
 	do
-		cd ${companyId}
+		cd "${companyId}" || exit 3
 
 		echo "CSV Data for company ${companyId}"
 		echo ""
@@ -136,22 +135,22 @@ function print_data {
 
 		cat generated_files
 
-		for type in $(ls type_* 2>/dev/null)
+		for type in type_*
 		do
 			type=${type#type_}
 			echo -en "${type},"
 
-			local count=$(wc -l type_${type} | sed -e "s/\ .*//")
+			local count=$(wc -l "type_${type}" | sed -e "s/\ .*//")
 			echo -en "${count},"
 
-			local unique=$(cat type_${type} | sed -e "s/.*\ //" | sort | uniq | wc -l)
+			local unique=$(sed -e "s/.*\ //" < "type_${type}"| sort | uniq | wc -l)
 			echo -en "${unique},"
 
-			local size=$(cat type_${type} | sed -e "s/\ .*//" | tr '\n' '+' | sed -e "s/\+$/\n/" | bc)
+			local size=$(sed -e "s/\ .*//" < "type_${type}"| tr '\n' '+' | sed -e "s/\+$/\n/" | bc)
 			size=$((size / 1048576))
 			echo -en "${size},"
 
-			local unique_size=$(cat type_${type} | sort | uniq | sed -e "s/\ .*//" | tr '\n' '+' | sed -e "s/\+$/\n/" | bc)
+			local unique_size=$(sort < "type_${type}"| uniq | sed -e "s/\ .*//" | tr '\n' '+' | sed -e "s/\+$/\n/" | bc)
 			unique_size=$((unique_size / 1048576))
 			echo -en "${unique_size}"
 
@@ -160,7 +159,7 @@ function print_data {
 
 		echo ""
 
-		cd ..
+		cd "${TMP_DIR}" || exit 3
 	done
 }
 
