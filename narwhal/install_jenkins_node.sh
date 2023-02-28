45b2c836339a99f690895d9f7da270350eb4b15e
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-02-28 13:13:11 +0100

    Install Docker from Docker

--- narwhal/install_jenkins_node.sh
+++ narwhal/install_jenkins_node.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+mkdir -m 0755 /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
 apt-get update
-apt-get --yes install docker docker.io jq openjdk-11-jdk p7zip-full
+apt-get --yes install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin jq openjdk-11-jdk p7zip-full
 snap install yq
 
 adduser jenkins
