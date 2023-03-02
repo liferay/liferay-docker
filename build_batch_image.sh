3074c51849c9baf362d71a512a20488395155ab3
Raymond Augé <raymond.auge@liferay.com>
2023-03-02 13:44:29 -0500

    LPS-176097 Add "Batch engine" docker image to be used for batch jobs

---
+++ build_batch_image.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source ./_common.sh
+
+function build_docker_image {
+	local image_version=$(./release_notes.sh get-version)
+
+	DOCKER_IMAGE_TAGS=()
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/batch:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/batch")
+
+	if [ "${1}" == "push" ]
+	then
+		check_docker_buildx
+
+		docker buildx build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay BATCH" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			--builder "liferay-buildkit" \
+			--platform "${LIFERAY_DOCKER_IMAGE_PLATFORMS}" \
+			--push \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	else
+		remove_temp_dockerfile_target_platform
+
+		docker build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay BATCH" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	fi
+}
+
+function main {
+	make_temp_directory templates/batch
+
+	log_in_to_docker_hub
+
+	build_docker_image "${1}"
+
+	clean_up_temp_directory
+}
+
+main "${@}"
