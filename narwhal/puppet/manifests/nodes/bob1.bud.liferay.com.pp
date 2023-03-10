7e205bb808dbbffdbf7d899befdf2e59d6c4c14d
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-03-10 16:20:15 +0100

    SRE-3042 Add pts_ci_node module to bob1

--- narwhal/puppet/manifests/nodes/bob1.bud.liferay.com.pp
+++ narwhal/puppet/manifests/nodes/bob1.bud.liferay.com.pp
@@ -1,4 +1,6 @@
 node 'bob1.bud.liferay.com' {
   include pts_system
+
+  include pts_ci_node
   #include pts_puppet_server
 }
--- narwhal/puppet/manifests/nodes/bob2.bud.liferay.com.pp
+++ narwhal/puppet/manifests/nodes/bob2.bud.liferay.com.pp
@@ -1,4 +1,5 @@
 node 'bob2.bud.liferay.com' {
   include pts_system
+
   include pts_ci_node
 }
