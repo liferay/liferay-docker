eb723fb741a1389e6acf88ad19d9b855e0f54e68
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-03-13 11:18:05 +0100

    SRE-3085 Narwhal shellchecker cleanup

--- narwhal/templates/hotfix-builder/resources/usr/local/bin/liferay_hotfix_builder_entrypoint.sh
+++ narwhal/templates/hotfix-builder/resources/usr/local/bin/liferay_hotfix_builder_entrypoint.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 function add_file {
-	file_dir=$(dirname ${1})
+	file_dir=$(dirname "${1}")
 
-	mkdir -p ${BUILD_DIR}/hotfix/binaries/${file_dir}
+	mkdir -p "${BUILD_DIR}/hotfix/binaries/${file_dir}"
 
-	cp ${PATCHED_DIR}/${1} ${BUILD_DIR}/hotfix/binaries/${file_dir}
+	cp "${PATCHED_DIR}/${1}" "${BUILD_DIR}/hotfix/binaries/${file_dir}"
 }
 
 function calculate_checksums {
-	cd ${BUILD_DIR}/hotfix/binaries/
+	lcd "${BUILD_DIR}/hotfix/binaries/"
 
-	for file in $(find .)
+	find . -print0 | while IFS= read -r -d '' file
 	do
-		md5sum ${file} >> ../checksums.txt
+		md5sum "${file}" >> ../checksums.txt
 	done
 }
 
 function clone_repository {
-	if [ -e /opt/liferay/dev/projects/${1} ]
+	if [ -e /opt/liferay/dev/projects/"${1}" ]
 	then
-		return ${SKIPPED}
+		return "${SKIPPED}"
 	fi
 
 	mkdir -p /opt/liferay/dev/projects/
-	cd /opt/liferay/dev/projects/
+	lcd /opt/liferay/dev/projects/
 
-	git clone git@github.com:liferay/${1}.git
+	git clone git@github.com:liferay/"${1}".git
 }
 
 function compare_jars {
-	jar1=${PATCHED_DIR}/${1}
-	jar2=${UPDATE_DIR}/${1}
+	jar1=${PATCHED_DIR}/"${1}"
+	jar2=${UPDATE_DIR}/"${1}"
 
 	function list_file {
-		unzip -v ${1} | \
+		unzip -v "${1}" | \
 			# Remove heades and footers
 			grep "Defl:N" | \
 			# Remove 0 byte files
@@ -54,12 +54,12 @@ function compare_jars {
 			sed -e "s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\ [0-9][0-9]:[0-9][0-9]//"
 	}
 
-	local file_changes=$((
-		list_file ${jar1}
-		list_file ${jar2}
+	local file_changes=$( (
+		list_file "${jar1}"
+		list_file "${jar2}"
 	) | sort | uniq -c)
 
-	if [ $(echo "${file_changes}" | grep "Defl:N" | wc -l) -eq 0 ]
+	if [ $(echo "${file_changes}" | grep -c "Defl:N") -eq 0 ]
 	then
 		return 2
 	fi
@@ -80,14 +80,14 @@ function compare_jars {
 function compile_dxp {
 	PATCHED_DIR=/opt/liferay/dev/projects/bundles
 
-	if [ -e ${BUILD_DIR}/built-sha ] && [ $(cat ${BUILD_DIR}/built-sha) == ${NARWHAL_GIT_SHA} ]
+	if [ -e "${BUILD_DIR}"/built-sha ] && [ $(cat "${BUILD_DIR}"/built-sha) == "${NARWHAL_GIT_SHA}" ]
 	then
 		echo "${NARWHAL_GIT_SHA} is already built in the ${BUILD_DIR}, skipping the compile_dxp step."
 
-		return ${SKIPPED}
+		return "${SKIPPED}"
 	fi
 
-	cd /opt/liferay/dev/projects/liferay-portal-ee
+	lcd /opt/liferay/dev/projects/liferay-portal-ee
 
 	ant all
 
@@ -95,65 +95,65 @@ function compile_dxp {
 
 	if [ "${exit_code}" -eq 0 ]
 	then
-		echo "${NARWHAL_GIT_SHA}" >${BUILD_DIR}/built-sha
+		echo "${NARWHAL_GIT_SHA}" > "${BUILD_DIR}"/built-sha
 	fi
 
 	return ${exit_code}
 }
 
 function create_folders {
-	BUILD_DIR=/opt/liferay/build/
+	BUILD_DIR=/opt/liferay/build
 
-	mkdir -p ${BUILD_DIR}
+	mkdir -p "${BUILD_DIR}"
 
-	echo 0 > ${BUILD_DIR}/.step
+	echo 0 > "${BUILD_DIR}"/.step
 }
 
 function create_hotfix {
-	rm -fr ${BUILD_DIR}/hotfix
-	mkdir -p ${BUILD_DIR}/hotfix
+	rm -fr "${BUILD_DIR}"/hotfix
+	mkdir -p "${BUILD_DIR}"/hotfix
 
-	diff -rq ${PATCHED_DIR} ${UPDATE_DIR} | grep -v "/work/Catalina" | while read change
+	diff -rq "${PATCHED_DIR}" "${UPDATE_DIR}" | grep -v /work/Catalina | while read -r change
 	do
-		if (echo ${change} | grep "^Only in ${UPDATE_DIR}" &>/dev/null)
+		if (echo "${change}" | grep "^Only in ${UPDATE_DIR}" &>/dev/null)
 		then
 			local deleted_file=${change#Only in }
-			deleted_file=$(echo ${deleted_file} | sed -e "s#: #/#" | sed -e "s#${UPDATE_DIR}##")
+			deleted_file=$(echo "${deleted_file}" | sed -e "s#: #/#" | sed -e "s#${UPDATE_DIR}##")
 			deleted_file=${deleted_file#/}
-			echo ${deleted_file}
+			echo "${deleted_file}"
 
-			if (in_scope ${deleted_file})
+			if (in_scope "${deleted_file}")
 			then
 				echo "Deleted ${deleted_file}"
 
-				echo "${deleted_file}" >> ${BUILD_DIR}/hotfix/deleted_files.txt
+				echo "${deleted_file}" >> "${BUILD_DIR}"/hotfix/deleted_files.txt
 			fi
-		elif (echo ${change} | grep "^Only in ${PATCHED_DIR}" &>/dev/null)
+		elif (echo "${change}" | grep "^Only in ${PATCHED_DIR}" &>/dev/null)
 		then
 			local new_file=${change#Only in }
-			new_file=$(echo ${new_file} | sed -e "s#: #/#" | sed -e "s#${PATCHED_DIR}##")
+			new_file=$(echo "${new_file}" | sed -e "s#: #/#" | sed -e "s#${PATCHED_DIR}##")
 			new_file=${new_file#/}
 
-			if (in_scope ${new_file})
+			if (in_scope "${new_file}")
 			then
 				echo "New file ${new_file}"
-				add_file ${new_file}
+				add_file "${new_file}"
 
-				echo "${new_file}" >> ${BUILD_DIR}/hotfix/new_files.txt
+				echo "${new_file}" >> "${BUILD_DIR}"/hotfix/new_files.txt
 			fi
 		else
 			local changed_file=${change#Files }
 			changed_file=${changed_file%% *}
-			changed_file=$(echo ${changed_file} | sed -e "s#${PATCHED_DIR}##")
+			changed_file=$(echo "${changed_file}" | sed -e "s#${PATCHED_DIR}##")
 			changed_file=${changed_file#/}
 
-			if (in_scope ${changed_file})
+			if (in_scope "${changed_file}")
 			then
-				if (echo ${changed_file} | grep -q ".[jw]ar$")
+				if (echo "${changed_file}" | grep -q ".[jw]ar$")
 				then
-					manage_jar ${changed_file} &
+					manage_jar "${changed_file}" &
 				else
-					add_file ${changed_file}
+					add_file "${changed_file}"
 				fi
 			fi
 		fi
@@ -167,25 +167,25 @@ function echo_time {
 }
 
 function get_dxp_version {
-	cd /opt/liferay/dev/projects/liferay-portal-ee
+	lcd /opt/liferay/dev/projects/liferay-portal-ee
 
-	local major=$(cat release.properties | grep -F "release.info.version.major[master-private]=")
-	local minor=$(cat release.properties | grep -F "release.info.version.minor[master-private]=")
-	local bug_fix=$(cat release.properties | grep -F "release.info.version.bug.fix[master-private]=")
-	local trivial=$(cat release.properties | grep -F "release.info.version.trivial=")
+	local major=$(grep -F "release.info.version.major[master-private]=" release.properties)
+	local minor=$(grep -F "release.info.version.minor[master-private]=" release.properties)
+	local bug_fix=$(grep -F "release.info.version.bug.fix[master-private]=" release.properties)
+	local trivial=$(grep -F "release.info.version.trivial=" release.properties)
 
-	echo ${major##*=}.${minor##*=}.${bug_fix##*=}-u${trivial##*=}
+	echo "${major##*=}.${minor##*=}.${bug_fix##*=}-u${trivial##*=}"
 }
 
 function git_update {
-	if [ -e ${BUILD_DIR}/built-sha ] && [ $(cat ${BUILD_DIR}/built-sha) == ${NARWHAL_GIT_SHA} ]
+	if [ -e "${BUILD_DIR}"/built-sha ] && [ $(cat "${BUILD_DIR}"/built-sha) == "${NARWHAL_GIT_SHA}" ]
 	then
 		echo "${NARWHAL_GIT_SHA} is already built in the ${BUILD_DIR}, skipping the git checkout step."
 
-		return ${SKIPPED}
+		return "${SKIPPED}"
 	fi
 
-	cd /opt/liferay/dev/projects/liferay-portal-ee
+	lcd /opt/liferay/dev/projects/liferay-portal-ee
 
 	if (git remote | grep -q upstream)
 	then
@@ -200,12 +200,16 @@ function git_update {
 }
 
 function in_scope {
-	if (echo ${1} | grep -q "^osgi/") || (echo ${1} | grep -q "^tomcat-.*/webapps/ROOT/")
+	if (echo "${1}" | grep -q "^osgi/") || (echo "${1}" | grep -q "^tomcat-.*/webapps/ROOT/")
 	then
 		return 0
 	else
 		return 1
 	fi
+}
+
+function lcd {
+	cd "${1}" || exit 3
 }
 
 function main {
@@ -227,7 +231,7 @@ function main {
 	time_run pre_compile_setup
 
 	DXP_VERSION=$(get_dxp_version)
-	UPDATE_DIR=/opt/liferay/bundles/${DXP_VERSION}
+	UPDATE_DIR=/opt/liferay/bundles/"${DXP_VERSION}"
 
 	time_run prepare_update &
 
@@ -248,49 +252,49 @@ function main {
 }
 
 function manage_jar {
-	if (compare_jars ${1})
+	if (compare_jars "${1}")
 	then
 		echo "Changed .jar file: ${1}"
 
-		add_file ${1}
+		add_file "${1}"
 	fi
 }
 
 function next_step {
-	local step=$(cat ${BUILD_DIR}/.step)
+	local step=$(cat "${BUILD_DIR}"/.step)
 
 	step=$((step + 1))
 
-	echo ${step} > ${BUILD_DIR}/.step
+	echo ${step} > "${BUILD_DIR}"/.step
 
 	printf '%02d' ${step}
 }
 
 function package {
-	cd ${BUILD_DIR}/hotfix
+	lcd "${BUILD_DIR}"/hotfix
 
-	zip -r ../liferay-hotfix-${NARWHAL_BUILD_ID}.zip *
+	zip -r ../liferay-hotfix-"${NARWHAL_BUILD_ID}".zip ./*
 
-	cd ..
+	lcd "${BUILD_DIR}"
 
 	rm -fr hotfix
 }
 
 function pre_compile_setup {
-	cd /opt/liferay/dev/projects/liferay-portal-ee
+	lcd /opt/liferay/dev/projects/liferay-portal-ee
 
 	if [ -e "build.profile-dxp.properties" ]
 	then
 		echo "build.profile-dxp.properties exists, skipping pre_compile_setup."
 
-		return ${SKIPPED}
+		return "${SKIPPED}"
 	fi
 
 	ant setup-profile-dxp
 }
 
 function prepare_update {
-	if [ -e ${UPDATE_DIR} ]
+	if [ -e "${UPDATE_DIR}" ]
 	then
 		echo "Bundle already exists in /opt/liferay/bundles/${DXP_VERSION}."
 
@@ -303,39 +307,39 @@ function prepare_update {
 }
 
 function setup_ssh {
-	mkdir -p ${HOME}/.ssh
+	mkdir -p "${HOME}"/.ssh
 
-	ssh-keyscan github.com >> ${HOME}/.ssh/known_hosts
+	ssh-keyscan github.com >> "${HOME}"/.ssh/known_hosts
 
-	echo "${NARWHAL_GITHUB_SSH_KEY}" > ${HOME}/.ssh/id_rsa
-	chmod 600 ${HOME}/.ssh/id_rsa
+	echo "${NARWHAL_GITHUB_SSH_KEY}" > "${HOME}"/.ssh/id_rsa
+	chmod 600 "${HOME}"/.ssh/id_rsa
 }
 
 function time_run {
 	local run_id=$(echo "${@}" | tr " " "_")
 	local start_time=$(date +%s)
 
-	local log_file=${BUILD_DIR}/build_${start_time}_step_$(next_step)_${run_id}.txt
+	local log_file="${BUILD_DIR}/build_${start_time}_step_$(next_step)_${run_id}.txt"
 
-	echo ">>> Starting the \"${@}\" phase. Log: ${log_file}. $(date)"
+	echo ">>> Starting the \"${*}\" phase. Log: ${log_file}. $(date)"
 
-	${@} &>${log_file}
+	"${@}" &> "${log_file}"
 
 	local exit_code=${?}
 
 	local end_time=$(date +%s)
 
-	if [ ${exit_code} == ${SKIPPED} ]
+	if [ "${exit_code}" == "${SKIPPED}" ]
 	then
-		echo ">>> Skipped \"${@}\". $(date)"
+		echo ">>> Skipped \"${*}\". $(date)"
 	else
 		local seconds=$((end_time - start_time))
 
-		echo ">>> Finished \"${@}\" in $(echo_time ${seconds}). Exit code ${exit_code}. $(date)"
+		echo ">>> Finished \"${*}\" in $(echo_time ${seconds}). Exit code ${exit_code}. $(date)"
 
 		if [ "${exit_code}" -gt 0 ]
 		then
-			echo "${@} exited with error, full log file: ${log_file}. Printing the last 100 lines:"
+			echo "${*} exited with error, full log file: ${log_file}. Printing the last 100 lines:"
 
 			tail -n 100 "${log_file}"
 
