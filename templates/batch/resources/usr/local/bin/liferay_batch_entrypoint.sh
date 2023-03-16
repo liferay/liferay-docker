ac6f3516bcb111f6587535772caa65364e6d9ab5
Raymond Augé <raymond.auge@liferay.com>
2023-03-16 14:25:28 -0400

    all public envs start with LIFERAY_BATCH_

--- templates/batch/resources/usr/local/bin/liferay_batch_entrypoint.sh
+++ templates/batch/resources/usr/local/bin/liferay_batch_entrypoint.sh
@@ -2,32 +2,33 @@
 ## Supported Environment Variables
 ################################################################################
 #
-# BATCH_CURL_FLAGS      (optional) set flags on all curl commands
-# BATCH_DIRECTORY       (optional) the top directory in which to locate batch
-#                       data files (default to "/batch")
-# BATCH_FILE_EXTENSION  (optional) the extension of batch data files to process
-#                       (default to "*.batch-engine-data.json")
-# BATCH_OAUTH_APP_ERC   (required) specify by external reference code the oauth
-#                       application to use
-# BATCH_VERBOSE         (optional) sets the verbose output (any value will be
-#                       considered true). When true also sets curl -v flag
+# LIFERAY_BATCH_CURL_FLAGS      (optional) set flags on all curl commands
+# LIFERAY_BATCH_DIRECTORY       (optional) the top directory in which to locate
+#                               batch data files (default to "/batch")
+# LIFERAY_BATCH_FILE_EXTENSION  (optional) the extension of batch data files to
+#                               process (default to "*.batch-engine-data.json")
+# LIFERAY_BATCH_OAUTH_APP_ERC   (required) specify by external reference code
+#                               the oauth application to use
+# LIFERAY_BATCH_VERBOSE         (optional) sets the verbose output (any value
+#                               will be considered true). When true also sets
+#                               curl -v flag
 #
 
-BATCH_DIRECTORY=${BATCH_DIRECTORY:-/batch}
-BATCH_FILE_EXTENSION=${BATCH_FILE_EXTENSION:-*.batch-engine-data.json}
+LIFERAY_BATCH_DIRECTORY=${LIFERAY_BATCH_DIRECTORY:-/batch}
+LIFERAY_BATCH_FILE_EXTENSION=${LIFERAY_BATCH_FILE_EXTENSION:-*.batch-engine-data.json}
 
 if [ -e /etc/liferay/localdev/rootCA.pem ]
 then
-	BATCH_CURL_FLAGS="${BATCH_CURL_FLAGS} --cacert /etc/liferay/localdev/rootCA.pem"
+	LIFERAY_BATCH_CURL_FLAGS="${LIFERAY_BATCH_CURL_FLAGS} --cacert /etc/liferay/localdev/rootCA.pem"
 fi
 
-if [ ! -z ${BATCH_VERBOSE:+x} ]
+if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 then
-	BATCH_CURL_FLAGS="${BATCH_CURL_FLAGS} -v"
+	LIFERAY_BATCH_CURL_FLAGS="${LIFERAY_BATCH_CURL_FLAGS} -v"
 fi
 
 function main {
-	local DATA_FILES=$(find $BATCH_DIRECTORY -type f -name "$BATCH_FILE_EXTENSION")
+	local DATA_FILES=$(find ${LIFERAY_BATCH_DIRECTORY} -type f -name "$LIFERAY_BATCH_FILE_EXTENSION")
 
 	if [ "${DATA_FILES}" == "" ]
 	then
@@ -35,30 +36,30 @@ function main {
 		exit 1
 	fi
 
-	if [ "$BATCH_OAUTH_APP_ERC" == "" ]
+	if [ "$LIFERAY_BATCH_OAUTH_APP_ERC" == "" ]
 	then
 		cat <<EOF
 No OAuth Profile was selected for JOB processing!
 
-Please set the environment variable BATCH_OAUTH_APP_ERC in your
+Please set the environment variable LIFERAY_BATCH_OAUTH_APP_ERC in your
 LCP.json file.
 
 e.g.
 
 "env": {
-	"BATCH_OAUTH_APP_ERC": "foo-oauth-application-headless-server"
+	"LIFERAY_BATCH_OAUTH_APP_ERC": "foo-oauth-application-headless-server"
 },
 
 EOF
 		exit 1
 	fi
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
-		echo "BATCH_OAUTH_APP_ERC = ${BATCH_OAUTH_APP_ERC}"
+		echo "LIFERAY_BATCH_OAUTH_APP_ERC = ${LIFERAY_BATCH_OAUTH_APP_ERC}"
 	fi
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "########################"
 		echo "Mounted Config:"
@@ -66,49 +67,49 @@ EOF
 		find /etc/liferay/lxc/dxp-metadata -type l -not -ipath "*/..data" -print -exec sed 's/^/    /' {} \; -exec echo "" \;
 	fi
 
-	DXP_MAIN_DOMAIN=$(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.mainDomain)
-	DXP_SERVER_PROTOCOL=$(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.server.protocol)
-	OAUTH2_CLIENT_ID=$(cat /etc/liferay/lxc/ext-init-metadata/${BATCH_OAUTH_APP_ERC}.oauth2.headless.server.client.id)
-	OAUTH2_CLIENT_SECRET=$(cat /etc/liferay/lxc/ext-init-metadata/${BATCH_OAUTH_APP_ERC}.oauth2.headless.server.client.secret)
-	OAUTH2_TOKEN_URI=$(cat /etc/liferay/lxc/ext-init-metadata/${BATCH_OAUTH_APP_ERC}.oauth2.token.uri)
+	LIFERAY_BATCH_DXP_MAIN_DOMAIN=$(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.mainDomain)
+	LIFERAY_BATCH_DXP_SERVER_PROTOCOL=$(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.server.protocol)
+	LIFERAY_BATCH_OAUTH2_CLIENT_ID=$(cat /etc/liferay/lxc/ext-init-metadata/${LIFERAY_BATCH_OAUTH_APP_ERC}.oauth2.headless.server.client.id)
+	LIFERAY_BATCH_OAUTH2_CLIENT_SECRET=$(cat /etc/liferay/lxc/ext-init-metadata/${LIFERAY_BATCH_OAUTH_APP_ERC}.oauth2.headless.server.client.secret)
+	LIFERAY_BATCH_OAUTH2_TOKEN_URI=$(cat /etc/liferay/lxc/ext-init-metadata/${LIFERAY_BATCH_OAUTH_APP_ERC}.oauth2.token.uri)
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "########################"
-		echo "DXP_MAIN_DOMAIN: ${DXP_MAIN_DOMAIN}"
-		echo "DXP_SERVER_PROTOCOL: ${DXP_SERVER_PROTOCOL}"
-		echo "BATCH_OAUTH_APP_ERC: ${BATCH_OAUTH_APP_ERC}"
-		echo "OAUTH2_CLIENT_ID: ${OAUTH2_CLIENT_ID}"
-		echo "OAUTH2_CLIENT_SECRET: ${OAUTH2_CLIENT_SECRET}"
-		echo "OAUTH2_TOKEN_URI: ${OAUTH2_TOKEN_URI}"
+		echo "LIFERAY_BATCH_DXP_MAIN_DOMAIN: ${LIFERAY_BATCH_DXP_MAIN_DOMAIN}"
+		echo "LIFERAY_BATCH_DXP_SERVER_PROTOCOL: ${LIFERAY_BATCH_DXP_SERVER_PROTOCOL}"
+		echo "LIFERAY_BATCH_OAUTH_APP_ERC: ${LIFERAY_BATCH_OAUTH_APP_ERC}"
+		echo "LIFERAY_BATCH_OAUTH2_CLIENT_ID: ${LIFERAY_BATCH_OAUTH2_CLIENT_ID}"
+		echo "LIFERAY_BATCH_OAUTH2_CLIENT_SECRET: ${LIFERAY_BATCH_OAUTH2_CLIENT_SECRET}"
+		echo "LIFERAY_BATCH_OAUTH2_TOKEN_URI: ${LIFERAY_BATCH_OAUTH2_TOKEN_URI}"
 		echo "########################"
 	fi
 
 	local TOKEN_RESULT=$(\
 		curl \
 			-s \
-			$BATCH_CURL_FLAGS \
+			$LIFERAY_BATCH_CURL_FLAGS \
 			-X POST \
-			"${DXP_SERVER_PROTOCOL}://${DXP_MAIN_DOMAIN}${OAUTH2_TOKEN_URI}" \
+			"${LIFERAY_BATCH_DXP_SERVER_PROTOCOL}://${LIFERAY_BATCH_DXP_MAIN_DOMAIN}${LIFERAY_BATCH_OAUTH2_TOKEN_URI}" \
 			-H 'Content-type: application/x-www-form-urlencoded' \
-			-d "grant_type=client_credentials&client_id=${OAUTH2_CLIENT_ID}&client_secret=${OAUTH2_CLIENT_SECRET}" \
+			-d "grant_type=client_credentials&client_id=${LIFERAY_BATCH_OAUTH2_CLIENT_ID}&client_secret=${LIFERAY_BATCH_OAUTH2_CLIENT_SECRET}" \
 		| jq -r '.')
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "TOKEN_RESULT: ${TOKEN_RESULT}"
 	fi
 
-	ACCESS_TOKEN=$(jq -r '.access_token' <<< $TOKEN_RESULT)
+	LIFERAY_BATCH_ACCESS_TOKEN=$(jq -r '.access_token' <<< $TOKEN_RESULT)
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
-		echo "ACCESS_TOKEN: ${ACCESS_TOKEN}"
+		echo "LIFERAY_BATCH_ACCESS_TOKEN=${LIFERAY_BATCH_ACCESS_TOKEN}"
 	fi
 
-	if [ "${ACCESS_TOKEN}" == "null" ]
+	if [ "${LIFERAY_BATCH_ACCESS_TOKEN}" == "null" ]
 	then
-		echo "Failed to obtain ACCESS_TOKEN!"
+		echo "Failed to obtain LIFERAY_BATCH_ACCESS_TOKEN!"
 		exit 1
 	fi
 
@@ -132,7 +133,7 @@ process_batch() {
 		BATCH_HREF="/${BATCH_HREF}"
 	fi
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "BATCH_HREF=${BATCH_HREF}"
 	fi
@@ -144,7 +145,7 @@ process_batch() {
 		PARAMETERS="?${PARAMETERS}"
 	fi
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "PARAMETERS=${PARAMETERS}"
 	fi
@@ -154,10 +155,10 @@ process_batch() {
 			-s \
 			$BATCH_CURL_FLAGS \
 			-X 'POST' \
-			"${DXP_SERVER_PROTOCOL}://${DXP_MAIN_DOMAIN}${BATCH_HREF}${PARAMETERS}" \
+			"${LIFERAY_BATCH_DXP_SERVER_PROTOCOL}://${LIFERAY_BATCH_DXP_MAIN_DOMAIN}${BATCH_HREF}${PARAMETERS}" \
 			-H 'accept: application/json' \
 			-H 'Content-Type: application/json' \
-			-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+			-H "Authorization: Bearer ${LIFERAY_BATCH_ACCESS_TOKEN}" \
 			-d "${BATCH_ITEMS}" \
 		| jq -r '.')
 
@@ -167,7 +168,7 @@ process_batch() {
 		exit 1
 	fi
 
-	if [ ! -z ${BATCH_VERBOSE:+x} ]
+	if [ ! -z ${LIFERAY_BATCH_VERBOSE:+x} ]
 	then
 		echo "RESULT=${RESULT}"
 	fi
@@ -183,9 +184,9 @@ process_batch() {
 				-s \
 				$BATCH_CURL_FLAGS \
 				-X 'GET' \
-				"${DXP_SERVER_PROTOCOL}://${DXP_MAIN_DOMAIN}/o/headless-batch-engine/v1.0/import-task/by-external-reference-code/${BATCH_EXTERNAL_REFERENCE_CODE}" \
+				"${LIFERAY_BATCH_DXP_SERVER_PROTOCOL}://${LIFERAY_BATCH_DXP_MAIN_DOMAIN}/o/headless-batch-engine/v1.0/import-task/by-external-reference-code/${BATCH_EXTERNAL_REFERENCE_CODE}" \
 				-H 'accept: application/json' \
-				-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+				-H "Authorization: Bearer ${LIFERAY_BATCH_ACCESS_TOKEN}" \
 			| jq -r '.')
 
 		BATCH_STATUS=$(jq -r '.executeStatus//.status' <<< "$RESULT")
