5a688c987aaf0fd2c8bced66fccffb2cf8820beb
Brian Chan <brian.chan@liferay.com>
2023-03-22 17:53:28 -0700

    Because crontab ain't working

--- build_nightly.sh
+++ build_nightly.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-touch /tmp/build_nightly-${RANDOM}.txt
-echo "${PATH}" > /tmp/build_nightly-${RANDOM}.txt
-echo $(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)") > /tmp/build_nightly-${RANDOM}.txt
+while true
+do
+	LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
 
-cd $(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)")
+	echo ""
+	echo `date`
+	echo ""
 
-LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
+	sleep 1d
+done
