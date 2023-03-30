d597dc224a1d9bdaef30dffa9369f11224b5cf3c
Brian Chan <brian.chan@liferay.com>
2023-03-30 14:13:41 -0300

    LPS-178706 Rename/SF

---
+++ templates/jar-runner/resources/usr/local/bin/liferay_jar_runner_entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -e /opt/liferay/caroot/rootCA.pem ]
+then
+	export CAROOT=/opt/liferay/caroot
+	export TRUST_STORES=java,system
+
+	mkcert -install
+fi
+
+if [ -e /usr/local/bin/liferay_jar_runner_set_up.sh ]
+then
+	./usr/local/bin/liferay_jar_runner_set_up.sh
+fi
+
+java ${LIFERAY_JAR_RUNNER_JAVA_OPTS} -jar /opt/liferay/jar-runner.jar
+
+if [ -e /usr/local/bin/liferay_jar_runner_tear_down.sh ]
+then
+	./usr/local/bin/liferay_jar_runner_tear_down.sh
+fi
