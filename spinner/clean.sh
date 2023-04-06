542a53e8a19a5eaa916c8a8c1eebac76ffbb7721
Brian Chan <brian.chan@liferay.com>
2023-04-06 04:23:21 -0700

    Spinner clean.sh

--- spinner/clean.sh
+++ spinner/clean.sh
@@ -4,28 +4,31 @@ function lcd {
 	cd "${1}" || exit 3
 }
 
-docker_compose="docker compose"
+function main {
+	if [[ $(find . -name "env-*" -type d | wc -l) -eq 0 ]]
+	then
+		echo "There are no environments to clean."
 
-if (command -v docker-compose &>/dev/null)
-then
-	docker_compose="docker-compose"
-fi
+		exit 2
+	fi
 
-base_dir=$(pwd)
+	local docker_compose="docker compose"
 
-if [[ $(find . -name "env-*" -type d | wc -l) -eq 0 ]]
-then
-	echo "There were no environments to clean."
+	if (command -v docker-compose &>/dev/null)
+	then
+		docker_compose="docker-compose"
+	fi
 
-	exit 2
-fi
+	for environment in "$(pwd)"/env*
+	do
+		echo "Cleaning up ${environment}."
 
-for environment in "$(pwd)"/env*
-do
-	echo "Cleaning up ${environment}."
+		lcd "${environment}"
 
-	lcd "${environment}"
-	${docker_compose} down --rmi local -v
+		${docker_compose} down --rmi local -v
 
-	rm -fr "${environment}"
-done
+		rm -fr "${environment}"
+	done
+}
+
+main
