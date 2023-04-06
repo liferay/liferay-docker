9110f7a517cac55f749fe012affaa65b827a1331
Gregory Amerson <gregory.amerson@liferay.com>
2023-04-06 10:10:00 -0500

    ISSITES-6034 add set_up and tear_down scripts

--- templates/node-runner/resources/usr/local/bin/liferay_node_runner_entrypoint.sh
+++ templates/node-runner/resources/usr/local/bin/liferay_node_runner_entrypoint.sh
@@ -2,7 +2,19 @@
 
 if [ -e /opt/node/caroot/rootCA.pem ]
 then
-	export NODE_EXTRA_CA_CERTS=/opt/node/caroot/rootCA.pem
+	export NODE_EXTRA_CA_CERTS=/opt/liferay/caroot/rootCA.pem
 fi
 
+if [ -e /usr/local/bin/liferay_node_runner_set_up.sh ]
+then
+	./usr/local/bin/liferay_node_runner_set_up.sh
+fi
+
+$LIFERAY_NODE_RUNNER_INSTALL
+
 $LIFERAY_NODE_RUNNER_START
+
+if [ -e /usr/local/bin/liferay_node_runner_tear_down.sh ]
+then
+	./usr/local/bin/liferay_node_runner_tear_down.sh
+fi
