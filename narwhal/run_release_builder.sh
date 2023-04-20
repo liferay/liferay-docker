f13c5968ea9606351e782b321fcf5e9a85097533
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-04-20 12:33:59 +0200

    LRP-4468 Renames and 1Password support

--- narwhal/run_release_builder.sh
+++ narwhal/run_release_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CACHE_DIR="${HOME}/.hotfix-builder-cache"
+CACHE_DIR="${HOME}/.release-builder-cache"
 NARWHAL_GIT_SHA="fix-pack-fix-240397335"
 
 if [ ! -d "${CACHE_DIR}" ]
@@ -10,9 +10,12 @@ then
 	sudo install -d "${CACHE_DIR}" -m 0775 -o 1000
 fi
 
-cd templates/hotfix-builder || exit 3
+cd templates/release-builder || exit 3
 
-if [ ! -s "${SSH_AUTH_SOCK}" ]
+if [ -e "${HOME}/.1password/agent.sock" ]
+then
+	SSH_CONFIG="-e SSH_AUTH_SOCK=/ssh-agent -v ${HOME}/.1password/agent.sock:/ssh-agent"
+elif [ ! -s "${SSH_AUTH_SOCK}" ]
 then
 	SSH_CONFIG="-e SSH_AUTH_SOCK=/ssh-agent -v ${SSH_AUTH_SOCK}:/ssh-agent"
 else
@@ -24,10 +27,12 @@ else
 		SSH_PUBKEY_FILE="$HOME/.ssh/id_rsa"
 	else
 		echo "No \${SSH_AUTH_SOCK} or public key present. Exiting."
+
 		exit 1
 	fi
 	SSH_CONFIG="-e NARWHAL_GITHUB_SSH_KEY=\"$(cat "${HOME}"/"${SSH_PUBKEY_FILE}")\""
 fi
 
-docker -l warning build . --quiet -t hotfix-builder && \
-	docker run -it -v "${CACHE_DIR}:/opt/liferay/" -e NARWHAL_BUILD_ID=1 -e NARWHAL_GIT_SHA="${NARWHAL_GIT_SHA}" "${SSH_CONFIG}" release-builder
+# shellcheck disable=SC2086
+docker -l warning build . --quiet -t release-builder && \
+	docker run -it -v "${CACHE_DIR}:/opt/liferay/" -e NARWHAL_BUILD_ID=1 -e NARWHAL_GIT_SHA="${NARWHAL_GIT_SHA}" ${SSH_CONFIG} release-builder
