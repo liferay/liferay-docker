3bd6998be1cf9be34efb5c00659459ac7ed0eeca
Brian Chan <brian.chan@liferay.com>
2023-04-28 11:02:04 +0530

    Just inline it?

--- orca/templates/teleport-proxy/resources/usr/local/bin/generate-certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openssl req -subj '/CN=192.168.233.141/O=Liferay/C=HU' -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 -keyout /etc/teleport/server.key -out /etc/teleport/server.crt
