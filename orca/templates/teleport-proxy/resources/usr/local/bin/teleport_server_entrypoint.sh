f756a6002a455b614d6d51d599246da7648df31d
Brian Chan <brian.chan@liferay.com>
2023-04-28 11:02:19 +0530

    SF

--- orca/templates/teleport-proxy/resources/usr/local/bin/teleport_server_entrypoint.sh
+++ orca/templates/teleport-proxy/resources/usr/local/bin/teleport_server_entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/teleport/server.crt ]
 then
-	openssl req -subj '/CN=192.168.233.141/O=Liferay/C=HU' -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 -keyout /etc/teleport/server.key -out /etc/teleport/server.crt
+	openssl req -subj "/CN=192.168.233.141/O=Liferay/C=HU" -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 -keyout /etc/teleport/server.key -out /etc/teleport/server.crt
 fi
 
 teleport start -c /etc/teleport/teleport.yaml
