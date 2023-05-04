6e1e91999e6b640d1098388670da00acfb96d824
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-05-04 11:17:26 +0200

    SRE-3086 Style changes

--- orca/templates/db/resources/usr/local/bin/make_primary.sh
+++ orca/templates/db/resources/usr/local/bin/make_primary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-local db_password=$(cat ${MARIADB_ROOT_PASSWORD_FILE})
+db_password=$(cat "${MARIADB_ROOT_PASSWORD_FILE}")
 
-echo "SET GLOBAL wsrep_provider_options='pc.bootstrap=YES'" | mysql -h127.0.0.1 -p${db_password} -u root
+echo "SET GLOBAL wsrep_provider_options='pc.bootstrap=YES'" | mysql -h127.0.0.1 "-p${db_password}" -u root
