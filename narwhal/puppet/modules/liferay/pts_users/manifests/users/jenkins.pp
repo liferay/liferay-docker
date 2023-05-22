c68db69f5ad3218b54c6550838746dc409126f49
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-05-22 15:42:17 +0200

    SRE-3511 Add jenkins' ssh key

--- narwhal/puppet/modules/liferay/pts_users/manifests/users/jenkins.pp
+++ narwhal/puppet/modules/liferay/pts_users/manifests/users/jenkins.pp
@@ -17,6 +17,7 @@ class pts_users::users::jenkins {
 			sshkey_owner => 'root',
 			sshkeys =>
 				[
+				'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGELcHwoA3CySiLLnHICPzF9NeK9uTknapOoCFU2Mk7j root@bob1',
 				'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLzDAYeiaDNX1bZo3v6UfzIuTXHZ5a3+bmMZ/ekYsnLdynrOV2Czo+k0srjz7oSDY0DK4sMJWMfGMMGMSicUteCVYVyA/KLWweF6GpSzCh27VISwG9t05CDqTNm8/E8ACSJ2IZwTUeAZnTkDPqT/wZC+0xhfMX+aMmdWSHo0NVNhtU75FyCKeYStz5BLi3UNx+/w8dqQUHkGSmtLzj0IwXUgsQ1TS4reAthRNnihF7LBA2RntHaO2d1zgy4pvRPtPgVt9m0FHddhGedZ8YlSNcCG6AxlaZ6eI8Rl0OJarEg8nnwwZaOfqoJIXp6/TBq9LCxpt4RhYLTPd5XPWn8w/sQfpEZyJwohRoSOXPYDUChykDxn+V/78QcDF7oIJq9dcTs/D1jgHaMWi/REG77lENuuoAhEpk7S/6eGjGgG8yHghS65xQ6JWTH+FHnUqksFwyvVYQNkJ2nLrc5GCYIuSnCWHVxl5yk2jzs8da9DNNiGsSwFWYAOTzBV11Leq5GSM= jenkins@bob1',
 			],
 			uid => 1000,
