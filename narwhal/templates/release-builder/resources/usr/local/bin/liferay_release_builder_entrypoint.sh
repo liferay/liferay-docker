e4afad5371ab70f05cb69e5b4a5d6ca10f8cc361
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-05-31 12:12:40 +0200

    LRP-4541 Remove licensing code for now so tomcat can successfully start up

--- narwhal/templates/release-builder/resources/usr/local/bin/liferay_release_builder_entrypoint.sh
+++ narwhal/templates/release-builder/resources/usr/local/bin/liferay_release_builder_entrypoint.sh
@@ -29,7 +29,7 @@ function main {
 
 	background_run clone_repository liferay-binaries-cache-2020
 	background_run clone_repository liferay-portal-ee
-	time_run clone_repository liferay-release-tool-ee
+	#time_run clone_repository liferay-release-tool-ee
 	wait
 
 	time_run setup_remote
@@ -38,7 +38,7 @@ function main {
 
 	background_run init_gcs
 	background_run update_portal_git
-	time_run update_release_tool_git
+	#time_run update_release_tool_git
 	wait
 
 	time_run pre_compile_setup
@@ -49,9 +49,11 @@ function main {
 
 	if [ "${NARWHAL_OUTPUT}" == "release" ]
 	then
-		time_run add_licensing
+		#time_run add_licensing
 
 		time_run compile_dxp
+
+		time_run warm_up_tomcat
 
 		time_run package_bundle
 
@@ -59,7 +61,7 @@ function main {
 	else
 		time_run add_hotfix_testing_code
 
-		time_run add_licensing
+		#time_run add_licensing
 
 		background_run prepare_release_dir
 		time_run compile_dxp
