fb1f81aa2d091927ea54f004c370df7447649795
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-06-26 21:30:11 +0200

    SRE-2426 Make it more readable and remove duplicated lines + abc order parameter names

--- templates/bundle/resources/usr/local/bin/generate_database_report.sh
+++ templates/bundle/resources/usr/local/bin/generate_database_report.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
 
-function generate_reports {
+function check_usage {
+	lc_check_utils mysql
+
 	LIFERAY_REPORTS_DIRECTORY="${LIFERAY_HOME}/data/reports"
 
 	mkdir -p "${LIFERAY_REPORTS_DIRECTORY}"
-
-	if (! mysql -V | grep mysql &>/dev/null)
-	then
-		echo "MySQL client is not available to generate reports using ./generate_report.sh."
-	else
-		echo "Generating database status and query reports to ${LIFERAY_REPORTS_DIRECTORY}"
-
-		mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -u ${LCP_SECRET_DATABASE_USER} -p${LCP_SECRET_DATABASE_PASSWORD} -E -e "SHOW ENGINE INNODB STATUS;" > ${LIFERAY_REPORTS_DIRECTORY}/database_status_$(date +'%Y-%m-%d_%H-%M-%S').txt
-
-		(
-			echo "<h1>INNODB_LOCK_WAITS</h1>" > outputfile
-			mysql -D INFORMATION_SCHEMA --connect-timeout=10 -u ${LCP_SECRET_DATABASE_USER} -p${LCP_SECRET_DATABASE_PASSWORD} -H -e "SELECT * FROM INNODB_LOCK_WAITS;"
-
-			mysql -D INFORMATION_SCHEMA --connect-timeout=10 -u ${LCP_SECRET_DATABASE_USER} -p${LCP_SECRET_DATABASE_PASSWORD} -H -e "SELECT * FROM INNODB_LOCKS WHERE LOCK_TRX_ID IN (SELECT BLOCKING_TRX_ID FROM INNODB_LOCK_WAITS);"
-
-			echo "<h1>AVAILABLE VIRTUAL INSTANCES</h1>"
-			mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -H -e "SELECT * FROM VirtualHost;"
-
-			echo "<h1>COUNT OF RECORDS ON EACH TABLE</h1>"
-			mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -H -e "SELECT TABLE_NAME, TABLE_ROWS from information_schema.TABLES;"
-
-			echo "<h1>DDMTEMPLATE CONTENTS</h1>"
-			mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -H -e "SELECT * FROM DDMTemplate;"
-
-			echo "<h1>FRAGMENT SOURCES(HTML, CSS ..)</h1>"
-			mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -H -e "SELECT * FROM FragmentEntryLink;"
-
-			echo "<h1>QUARTZ TRIGGERS</h1>"
-			mysql -D "$LCP_SECRET_DATABASE_NAME" --connect-timeout=10 -H -e "SELECT * FROM QUARTZ_TRIGGERS;"
-
-		) > ${LIFERAY_REPORTS_DIRECTORY}/query_report_$(date +'%Y-%m-%d_%H-%M-%S').html
-
-		echo "Generated database status and query reports to ${LIFERAY_REPORTS_DIRECTORY}"
-	fi
 }
 
-generate_reports
+function generate_reports {
+	echo "Generating database status and query reports to ${LIFERAY_REPORTS_DIRECTORY}"
+
+	(
+		run_query "${LCP_SECRET_DATABASE_NAME}" "SHOW ENGINE INNODB STATUS;"
+
+		run_query INFORMATION_SCHEMA "SELECT * FROM INNODB_LOCK_WAITS;"
+
+		run_query INFORMATION_SCHEMA "SELECT * FROM INNODB_LOCKS WHERE LOCK_TRX_ID IN (SELECT BLOCKING_TRX_ID FROM INNODB_LOCK_WAITS);"
+
+		run_query "$LCP_SECRET_DATABASE_NAME" "SELECT * FROM VirtualHost;"
+
+		run_query "$LCP_SECRET_DATABASE_NAME" "SELECT TABLE_NAME, TABLE_ROWS from information_schema.TABLES;"
+
+		run_query "$LCP_SECRET_DATABASE_NAME" "SELECT * FROM DDMTemplate;"
+
+		run_query "$LCP_SECRET_DATABASE_NAME" "SELECT * FROM FragmentEntryLink;"
+
+		run_query "$LCP_SECRET_DATABASE_NAME" "SELECT * FROM QUARTZ_TRIGGERS;"
+
+	) > ${LIFERAY_REPORTS_DIRECTORY}/query_report_$(date +'%Y-%m-%d_%H-%M-%S').html
+
+	echo "Generated database status and query reports to ${LIFERAY_REPORTS_DIRECTORY}"
+}
+
+function main {
+	check_usage
+
+	generate_reports
+}
+
+function run_query {
+	echo "<h1>${2}</h1>"
+
+	mysql --connect-timeout=10 -D "${1}" -e "${2}" -H -u "${LCP_SECRET_DATABASE_USER}" -p"${LCP_SECRET_DATABASE_PASSWORD}"
+}
+
+main "${@}"
