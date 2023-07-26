9a3dfe91cb3fffc0b682572b273524203f4afd44
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-07-26 11:30:47 +0200

    SRE-3277 Use _common as we have used this name before

--- narwhal/source_code_sharing/update_liferay_dxp_source_code_of_versions.sh
+++ narwhal/source_code_sharing/update_liferay_dxp_source_code_of_versions.sh
@@ -4,7 +4,7 @@
 
 set -o pipefail
 
-source "$(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)")/_local_common.sh"
+source "$(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)")/_common.sh"
 
 BASE_DIR="${PWD}"
 
