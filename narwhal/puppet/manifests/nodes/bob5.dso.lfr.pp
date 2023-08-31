b3574c1100ea4be88b0ee24780b04c6b8a7adccb
Tamas Papp <tamas.papp@liferay.com>
2023-08-31 10:37:01 +0200

    SRE-3042 Add pts_threatstack

--- narwhal/puppet/manifests/nodes/bob5.dso.lfr.pp
+++ narwhal/puppet/manifests/nodes/bob5.dso.lfr.pp
@@ -1,4 +1,5 @@
 node 'bob5.dso.lfr' {
 	include pts_ci_node
 	include pts_system
+	include pts_threatstack
 }
--- narwhal/puppet/manifests/nodes/bob6.dso.lfr.pp
+++ narwhal/puppet/manifests/nodes/bob6.dso.lfr.pp
@@ -1,4 +1,5 @@
 node 'bob6.dso.lfr' {
 	include pts_ci_node
 	include pts_system
+	include pts_threatstack
 }
