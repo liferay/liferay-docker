d5eb15949828d62fac4b5e9a01fde33e0f8951ba
Szantina Szanto <szantina.szanto@liferay.com>
2023-09-01 13:21:15 +0200

    DOCKER-216 redo of caddy test

--- test_caddy_image.sh
+++ test_caddy_image.sh
@@ -1,40 +1,163 @@
 #!/bin/bash
 
-function check_container_running {
-    if docker ps | grep -q test-container
-    then
-        echo "Container started successfully"
-        echo "<html><body><h1>Hello!</h1></body></html>" > test_files/index.html
+source ./_common.sh
 
-        sleep 2
+function check_usage {
+	if [ ! -n "${LIFERAY_DOCKER_IMAGE_ID}" ] || [ ! -n "${LIFERAY_DOCKER_LOGS_DIR}" ]
+	then
+		echo "Usage: ${0}"
+		echo ""
+		echo "The script reads the following environment variables:"
+		echo ""
+		echo "    LIFERAY_DOCKER_IMAGE_ID: ID of Docker image"
+		echo "    LIFERAY_DOCKER_LOGS_DIR: Path to the logs directory"
+		echo ""
+		echo "Example: LIFERAY_DOCKER_IMAGE_ID=liferay/caddy LIFERAY_DOCKER_LOGS_DIR=logs-202306270130 ${0}"
 
-        response=$(curl -s http://localhost:8080/index.html)
+		exit 1
+	fi
 
-        if [[ $response == *"Hello!"* ]]
-        then
-            echo "Content served properly from /public_html folder"
-        else
-            echo "Content serving test failed"
-        fi
+	check_utils curl docker
+}
 
-        docker stop test-container
-        docker rm test-container
+function clean_up_test_directory {
+	if [ "${TEST_RESULT}" -eq 0 ]
+	then
+		rm -fr "${TEST_DIR}"
+	fi
+}
 
-    else
-        echo "Container failed to start"
-    fi
+function generate_logs {
+	if [ "${TEST_RESULT}" -gt 0 ]
+	then
+        mkdir -p "${PWD}/${LIFERAY_DOCKER_LOGS_DIR}"
+		docker logs > "${PWD}/${LIFERAY_DOCKER_LOGS_DIR}/test.log" "${CONTAINER_ID}"
+	fi
+}
+
+function log_test_failure {
+	TEST_RESULT=1
+
+	if [ -n "${1}" ]
+	then
+		echo "[${1}] FAILED"
+	else
+		echo "[${FUNCNAME[1]}] FAILED"
+	fi
+}
+
+function log_test_success {
+    TEST_RESULT=0
+	if [ -n "${1}" ]
+	then
+		echo "[${1}] SUCCESS"
+	else
+		echo "[${FUNCNAME[1]}] SUCCESS"
+	fi
 }
 
 function main {
-    run_container
+	check_usage
 
-    check_container_running
+	prepare_mount
+
+	start_container
+
+	test_health_status
+
+	test_docker_image_files
+
+	generate_logs
+
+	stop_container
+
+	clean_up_test_directory
+
+	exit "${TEST_RESULT}"
 }
 
-function run_container {
-    docker run -d --name test-container -p 8080:80 -v $(pwd)/test_files:/public_html liferay/caddy
+function prepare_mount {
+    local timestamp=$(date "$(date)" "+%Y%m%d%H%M")
 
-    sleep 5
+	TEST_DIR="${PWD}/temp-test-${timestamp}"
+
+	mkdir -p "${TEST_DIR}"
+
+    echo "<html>HELLO</html>" > "${TEST_DIR}/index.html"
+}
+
+function start_container {
+	echo "Starting container from image ${LIFERAY_DOCKER_IMAGE_ID}."
+
+	CONTAINER_ID=$(docker run -d -p 8080:80 -v "${TEST_DIR}:/public_html" "${LIFERAY_DOCKER_IMAGE_ID}")
+}
+
+function stop_container {
+	echo "Stopping container."
+
+	docker kill "${CONTAINER_ID}" > /dev/null
+	docker rm "${CONTAINER_ID}" > /dev/null
+}
+
+function test_docker_image_files {
+	test_page http://localhost:8080/index.html "HELLO"
+}
+
+function test_health_status {
+	echo -en "Waiting for health status"
+
+	for counter in {1..200}
+	do
+		echo -en "."
+
+		local health_status=$(docker inspect --format="{{json .State.Status}}" "${CONTAINER_ID}")
+		local serving_status=$(docker logs "${CONTAINER_ID}" 2>&1 | grep -c "serving initial configuration")
+
+		if [ "${health_status}" == "\"running\"" ] && [ "${serving_status}" -gt 0 ]
+		then
+			echo ""
+
+			log_test_success
+
+			return
+		fi
+
+		sleep 3
+	done
+
+	echo ""
+
+	log_test_failure
+
+	echo "Container health status is: ${health_status}."
+}
+
+function test_page {
+	local content
+
+	content=$(curl --fail --max-time 60 -s --show-error -L "${1}")
+
+	local exit_code=$?
+
+	if [ ${exit_code} -gt 0 ]
+	then
+		log_test_failure "${FUNCNAME[1]}"
+
+		echo "${content}"
+		echo ""
+		echo "curl exit code is: ${exit_code}."
+	else
+		if [[ "${content}" =~ .*"${2}".* ]]
+		then
+			log_test_success "${FUNCNAME[1]}"
+		else
+			log_test_failure "${FUNCNAME[1]}"
+
+			echo "${content}"
+			echo ""
+			echo "The \"${2}\" string is not present on the page."
+		fi
+	fi
 }
 
 main "${@}"
