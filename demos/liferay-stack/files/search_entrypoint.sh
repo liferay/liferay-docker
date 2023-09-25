17c21a391594174925187afaaff5e312495956a2
Zoltán Takács <zoltan.takacs@liferay.com>
2023-09-25 17:22:26 +0200

    DOCKER-226 Remove no longer used Liferay Stack in favor of Spinner

--- demos/liferay-stack/files/search_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-    
-/usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-icu analysis-kuromoji analysis-smartcn analysis-stempel
-
-exec /usr/local/bin/docker-entrypoint.sh elasticsearch
