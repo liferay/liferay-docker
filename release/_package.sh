918beabef041547cad77df768228f5168f3d0a16
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-09-28 11:01:00 +0200

    LRP-4642 This directory is not needed anymore

--- release/_package.sh
+++ release/_package.sh
@@ -89,4 +89,6 @@ function package_release {
 	lc_cd "${_PROJECTS_DIR}"/liferay-portal-ee/sql
 
 	zip -qr "${_BUILD_DIR}/release/liferay-dxp-sql-${_DXP_VERSION}-${_BUILD_TIMESTAMP}.zip" . -i "*.sql"
+
+	rm -fr "${_BUILD_DIR}/release/liferay-dxp"
 }
