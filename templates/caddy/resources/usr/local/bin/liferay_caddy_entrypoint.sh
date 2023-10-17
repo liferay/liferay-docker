4feaddaae48e7b12ca63d871512d08031862b0a5
Raymond Augé <raymond.auge@liferay.com>
2023-10-17 11:25:43 -0400

    DOCKER-230 Liferay/caddy doesn't respect the value of `com.liferay.lxc.dxp.server.protocol` property

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -6,26 +6,17 @@ function main {
 		LIFERAY_ROUTES_DXP="/etc/liferay/lxc/dxp-metadata"
 	fi
 
+	local protocol=$(cat ${LIFERAY_ROUTES_DXP}/com.liferay.lxc.dxp.server.protocol 2>/dev/null)
+
 	for i in $(cat ${LIFERAY_ROUTES_DXP}/com.liferay.lxc.dxp.domains 2>/dev/null)
 	do
-		local url="https://${i}"
+		local url="${protocol}://${i}"
 
 		cat >> /etc/caddy.d/liferay_caddy_file << EOF
 @origin${url} header Origin ${url}
 header @origin${url} Access-Control-Allow-Origin "${url}"
 header @origin${url} Vary Origin
 EOF
-
-		if [ "${LIFERAY_CADDY_ALLOW_HTTP}" == "true" ]
-		then
-			url="http://${i}"
-
-			cat >> /etc/caddy.d/liferay_caddy_file << EOF
-@origin${url} header Origin ${url}
-header @origin${url} Access-Control-Allow-Origin "${url}"
-header @origin${url} Vary Origin
-EOF
-		fi
 	done
 
 	if [ -n "${LIFERAY_CADDY_404_URL}" ]
