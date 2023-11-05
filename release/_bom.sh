0e5dee9eb67c33f76a59a0674948cbcb995ade31
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-11-05 10:35:31 +0100

    LRP-4660 Proper replacements of everything we need to change

--- release/_bom.sh
+++ release/_bom.sh
@@ -22,7 +22,14 @@ function generate_boms {
 
 	lc_download "https://repository.liferay.com/nexus/service/local/repositories/liferay-public-releases/content/com/liferay/portal/release.dxp.api/${base_version}/release.dxp.api-${base_version}-sources.jar"
 
-	sed -e "s#<version>${base_version}</version>#<version>${_DXP_VERSION}</version>#" < release.dxp.bom-${base_version}.pom > release.dxp.bom-${_DXP_VERSION}.pom
+	for pom in release.dxp.bom release.dxp.bom.compile.only
+	do
+		sed -e "s#<version>${base_version}</version>#<version>${_DXP_VERSION}</version>#" < "${pom}-${base_version}.pom" | \
+		sed -e "s#<connection>scm:git:git@github.com:liferay/liferay-portal.git</connection>#<connection>scm:git:git@github.com:liferay/liferay-dxp.git</connection>#" | \
+		sed -e "s#<developerConnection>scm:git:git@github.com:liferay/liferay-portal.git</developerConnection>#<developerConnection>scm:git:git@github.com:liferay/liferay-dxp.git</developerConnection>#" | \
+		sed -e "s#<tag>.*</tag>#<tag>${_DXP_VERSION}</tag>#" | \
+		sed -e "s#<url>https://github.com/liferay/liferay-portal</url>#<url>https://github.com/liferay/liferay-dxp</url>#" > "${pom}-${_DXP_VERSION}.pom"
+	done
 
 	return 1
 }
