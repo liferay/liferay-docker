aebf96ab21e8166dba9614ab5edc39e8d3ff3140
C02DJ2KJMD6R <spencer.woo@liferay.com>
2023-12-28 11:35:02 -0500

    DOCKER-240 explicitly set zero-length argument option flag

--- _common.sh
+++ _common.sh
@@ -233,8 +233,8 @@ function prepare_tomcat {
 }
 
 function remove_temp_dockerfile_target_platform {
-	sed -i'.bak' 's/${TARGETARCH}'/$(get_current_arch)/ "${TEMP_DIR}"/Dockerfile
-	sed -i'' 's/--platform=${TARGETPLATFORM} //g' "${TEMP_DIR}"/Dockerfile
+	sed -i='.bak' 's/${TARGETARCH}'/$(get_current_arch)/ "${TEMP_DIR}"/Dockerfile
+	sed -i='' 's/--platform=${TARGETPLATFORM} //g' "${TEMP_DIR}"/Dockerfile
 }
 
 function start_tomcat {
