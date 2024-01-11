05c98fc1805619d85c8a2d20c60b8220c33a7a4a
Brian Chan <brian.chan@liferay.com>
2024-01-10 16:30:29 -0800

    SF

--- release/_promotion.sh
+++ release/_promotion.sh
@@ -3,20 +3,19 @@
 function prepare_api_jars_for_pomotion {
 	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
 
-	if [ -z "${LIFERAY_RELEASE_NEXUS_REPOSITORY_USER}" ] || [ -z "${LIFERAY_RELEASE_NEXUS_REPOSITORY_PASSWORD}" ]
+	if [ -z "${LIFERAY_RELEASE_NEXUS_REPOSITORY_PASSWORD}" ] || [ -z "${LIFERAY_RELEASE_NEXUS_REPOSITORY_USER}" ]
 	then
-		 lc_log ERROR "Either \${LIFERAY_RELEASE_NEXUS_REPOSITORY_USER} or \${LIFERAY_RELEASE_NEXUS_REPOSITORY_PASSWORD} is undefined."
+		 lc_log ERROR "Either \${LIFERAY_RELEASE_NEXUS_REPOSITORY_PASSWORD} or \${LIFERAY_RELEASE_NEXUS_REPOSITORY_USER} is undefined."
 
 		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
 	fi
 
 	local nexus_repository_name="${1}"
-
 	local nexus_repository_url="https://repository.liferay.com/nexus/service/local/repositories"
 
 	for jar_rc_name in "release.dxp.api-${_ARTIFACT_RC_VERSION}.jar" "release.dxp.api-${_ARTIFACT_RC_VERSION}-sources.jar"
 	do
-		jar_release_name="${jar_rc_name/-${LIFERAY_RELEASE_RC_BUILD_TIMESTAMP}/}"
+		local jar_release_name="${jar_rc_name/-${LIFERAY_RELEASE_RC_BUILD_TIMESTAMP}/}"
 
 		_download_bom_file "${nexus_repository_url}/${nexus_repository_name}/content/com/liferay/portal/release.dxp.api/${_ARTIFACT_RC_VERSION}/${jar_rc_name}" "${_PROMOTION_DIR}/${jar_release_name}"
 	done
@@ -33,7 +32,6 @@ function prepare_poms_for_promotion {
 	fi
 
 	local nexus_repository_name="${1}"
-
 	local nexus_repository_url="https://repository.liferay.com/nexus/service/local/repositories"
 
 	for pom_name in release.dxp.api release.dxp.bom release.dxp.bom.compile.only release.dxp.bom.third.party
@@ -45,8 +43,8 @@ function prepare_poms_for_promotion {
 }
 
 function _download_bom_file {
-	local file_url="${1}"
 	local file_name="${2}"
+	local file_url="${1}"
 
 	_download_from_nexus "${file_url}" "${file_name}" || return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
 	_download_from_nexus "${file_url}.md5" "${file_name}.MD5" || return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
@@ -84,12 +82,13 @@ function _verify_checksum {
 
 	(
 		sed -z "s/\n$//" "${file}.sha512"
-		echo "  ${file}"
+
+		echo "    ${file}"
 	) | sha512sum -c - --status
 
 	if [ "${?}" != "0" ]
 	then
-		lc_log ERROR "Unable verify the checksum of ${file}."
+		lc_log ERROR "Unable to verify the checksum of ${file}."
 
 		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
 	fi
