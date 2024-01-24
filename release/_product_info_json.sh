ff8da069b270f4c7380473873e4c37e6b79b3d3e
Brian Chan <brian.chan@liferay.com>
2024-01-24 16:34:07 +0900

    SF

--- release/_product_info_json.sh
+++ release/_product_info_json.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 function generate_product_info_json {
-	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
-
 	function write {
-		echo -en "${1}" >> "${_PROMOTION_DIR}/.product_info_tmp.json"
+		echo -en "${1}" >> "${_PROMOTION_DIR}/.product_info.json.tmp"
 		echo -en "${1}"
 	}
 
@@ -12,66 +10,67 @@ function generate_product_info_json {
 		write "${1}\n"
 	}
 
-	sed -i \
+	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
+
+	lc_log DEBUG "Updating ${_PROMOTION_DIR}/.product_info.json."
+
+	sed \
 		-e "s@\(${product_version}.*\),\"promoted\":\"true\",\(.*\)@\1,\"promoted\":\"false\",\2@" \
 		-e "s/^\}/,/" \
-		"${_PROMOTION_DIR}/.product_info_tmp.json"
+		-i \
+		"${_PROMOTION_DIR}/.product_info.json.tmp"
 
-	local release_date_raw=$(lc_get_property "${_PROMOTION_DIR}/release.properties" release.date)
-	local release_date_formatted=$(date -d "${release_date_raw}" "+%m/%d/%Y")
+	local release_date=$(lc_get_property "${_PROMOTION_DIR}/release.properties" release.date)
 
-	local bundle_url_raw=$(lc_get_property "${_PROMOTION_DIR}/release.properties" bundle.url)
-	local bundle_url_formatted=$(obfuscate_url "${bundle_url_raw}" "${release_date_formatted}")
+	release_date=$(date -d "${release_date}" "+%m/%d/%Y")
 
-	local docker_image=$(lc_get_property "${_PROMOTION_DIR}/release.properties" liferay.docker.image)
+	local bundle_url=$(lc_get_property "${_PROMOTION_DIR}/release.properties" bundle.url)
 
-	local md5_url_raw="$(lc_get_property "${_PROMOTION_DIR}/release.properties" bundle.url).MD5"
-	local md5_url_formatted=$(obfuscate_url "${md5_url_raw}" "${release_date_formatted}")
+	bundle_url=$(obfuscate_url "${bundle_url}" "${release_date}")
 
-	LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" lc_download "${md5_url_raw}" "${_PROMOTION_DIR}/bundle_md5.txt"
+	local md5_url="$(lc_get_property "${_PROMOTION_DIR}/release.properties" bundle.url).MD5"
 
-	local md5_hash=$(<"${_PROMOTION_DIR}/bundle_md5.txt")
+	LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" lc_download "${md5_url}" "${_PROMOTION_DIR}/bundle_md5.txt"
 
 	local product_version=$(lc_get_property "${_PROMOTION_DIR}/release.properties" liferay.product.version)
 
 	local product_key="${product_version,,}"
+
 	product_key="${product_key// /-}"
 
-	local target_platform_version_raw=$(lc_get_property "${_PROMOTION_DIR}/release.properties" target.platform.version)
-	local target_platform_version_formatted="${target_platform_version_raw/-/.}"
+	local target_platform_version=$(lc_get_property "${_PROMOTION_DIR}/release.properties" target.platform.version)
 
-	local tomcat_version=$(lc_get_property "${_PROMOTION_DIR}/release.properties" app.server.tomcat.version)
-
-	lc_log DEBUG "Adding snippet to ${_PROMOTION_DIR}/.product_info.json."
+	target_platform_version="${target_platform_version/-/.}"
 
 	writeln "\"${product_key}\": {"
-	writeln "    \"appServerTomcatVersion\": \"${tomcat_version}\","
-	writeln "    \"bundleChecksumMD5\": \"${md5_hash}\","
-	writeln "    \"bundleChecksumMD5Url\": \"${md5_url_formatted}\","
-	writeln "    \"bundleUrl\": \"${bundle_url_formatted}\","
-	writeln "    \"liferayDockerImage\": \"${docker_image}\","
+	writeln "    \"appServerTomcatVersion\": \"$(lc_get_property "${_PROMOTION_DIR}/release.properties" app.server.tomcat.version)\","
+	writeln "    \"bundleChecksumMD5\": \"$(<"${_PROMOTION_DIR}/bundle_md5.txt")\","
+	writeln "    \"bundleChecksumMD5Url\": \"$(obfuscate_url "${md5_url}" "${release_date}")\","
+	writeln "    \"bundleUrl\": \"${bundle_url}\","
+	writeln "    \"liferayDockerImage\": \"$(lc_get_property "${_PROMOTION_DIR}/release.properties" liferay.docker.image)\","
 	writeln "    \"liferayProductVersion\": \"${product_version}\","
 	writeln "    \"promoted\": \"true\","
-	writeln "    \"releaseDate\": \"${release_date_formatted}\","
-	writeln "    \"targetPlatformVersion\": \"${target_platform_version_formatted}\""
+	writeln "    \"releaseDate\": \"${release_date}\","
+	writeln "    \"targetPlatformVersion\": \"${target_platform_version}\""
 	writeln "}"
-	echo "}" >> "${_PROMOTION_DIR}/.product_info_tmp.json"
+	echo "}" >> "${_PROMOTION_DIR}/.product_info.json.tmp"
 
-	jq '.' "${_PROMOTION_DIR}/.product_info_tmp.json" > "${_PROMOTION_DIR}/.product_info.json"
+	jq '.' "${_PROMOTION_DIR}/.product_info.json.tmp" > "${_PROMOTION_DIR}/.product_info.json"
 }
 
 function get_file_product_info_json {
 	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
 
-	LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" lc_download "https://releases.liferay.com/tools/workspace/.product_info.json" "${_PROMOTION_DIR}/.product_info_tmp.json"
+	LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" lc_download "https://releases.liferay.com/tools/workspace/.product_info.json" "${_PROMOTION_DIR}/.product_info.json.tmp"
 
-	cp -f "${_PROMOTION_DIR}/.product_info_tmp.json" "${LIFERAY_COMMON_LOG_DIR}/.product_info.json-BACKUP.txt"
+	cp -f "${_PROMOTION_DIR}/.product_info.json.tmp" "${LIFERAY_COMMON_LOG_DIR}/.product_info.json-BACKUP.txt"
 
-	sed -i -r -z \
+	sed \
 		-e 's@\r?\n        "@"@g' \
 		-e 's@\r?\n    \}(,)?@\}\1@g' \
 		-e 's@[ ]+"@"@g' \
-		"${_PROMOTION_DIR}/.product_info_tmp.json"
+		-i -r -z \
+		"${_PROMOTION_DIR}/.product_info.json.tmp"
 }
 
 function get_file_release_properties {
@@ -83,8 +82,8 @@ function get_file_release_properties {
 function obfuscate_url {
 	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
 
-	url="${1}"
-	release_date="${2}"
+	local release_date="${2}"
+	local url="${1}"
 
 	"${_RELEASE_ROOT_DIR}/url-coder-1.0.0.jar" "${url}" "${release_date}"
 }
@@ -92,7 +91,7 @@ function obfuscate_url {
 function upload_product_info_json {
 	trap 'return ${LIFERAY_COMMON_EXIT_CODE_BAD}' ERR
 
-	lc_log INFO "Making a backup copy to /www/releases.liferay.com/tools/workspace/.product_info.json.BACKUP on the server"
+	lc_log INFO "Backing up to /www/releases.liferay.com/tools/workspace/.product_info.json.BACKUP."
 
 	ssh -i lrdcom-vm-1 root@lrdcom-vm-1 cp -f "/www/releases.liferay.com/tools/workspace/.product_info.json" "/www/releases.liferay.com/tools/workspace/.product_info.json.BACKUP"
 
