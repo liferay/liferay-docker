26f4268f18099a3fce10b0d309cd99de10dfb550
Zsolt Balogh <zsolt.balogh@liferay.com>
2024-02-15 11:50:00 +0100

    LRP-4672 Simplify

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -5,8 +5,6 @@ source _product_info_json.sh
 source _promotion.sh
 source _publishing.sh
 
-LIFERAY_RELEASE_FORK_NAME=${LIFERAY_RELEASE_FORK_NAME:="liferay/liferay-portal.ee"}
-
 function check_usage {
 	if [ -z "${LIFERAY_RELEASE_RC_BUILD_TIMESTAMP}" ] || [ -z "${LIFERAY_RELEASE_VERSION}" ]
 	then
@@ -56,8 +54,7 @@ function print_help {
 	echo ""
 	echo "The script reads the following environment variables:"
 	echo ""
-	echo "    LIFERAY_RELEASE_FORK_NAME (optional): The repository owner's git username, defaults to 'liferay' and the repository's name"
-	echo "    LIFERAY_RELEASE_GITHUB_PAT (optional): The user's Personal Access Token"
+	echo "    LIFERAY_RELEASE_GITHUB_PAT (optional): GitHub personal access token to tag the releases"
 	echo "    LIFERAY_RELEASE_NEXUS_REPOSITORY_PASSWORD (optional): Nexus user's password"
 	echo "    LIFERAY_RELEASE_NEXUS_REPOSITORY_USER (optional): Nexus user with the right to upload BOM files"
 	echo "    LIFERAY_RELEASE_PRODUCT_NAME (optional): Set to \"portal\" for CE. The default is \"DXP\"."
@@ -89,6 +86,13 @@ function promote_packages {
 }
 
 function tag_release {
+	local repository=liferay-portal-ee
+
+	if [ "${LIFERAY_RELEASE_PRODUCT_NAME}" == "portal" ]
+	then
+		repository=liferay-portal
+	fi
+
 	if [ -n "${LIFERAY_RELEASE_GITHUB_PAT}" ]
 	then
 		if (! curl --data "{
@@ -101,7 +105,7 @@ function tag_release {
 			--header "Accept: application/vnd.github+json" \
 			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
 			--header "X-GitHub-Api-Version: 2022-11-28" \
-			--location https://api.github.com/repos/"${LIFERAY_RELEASE_FORK_NAME}"/git/tags \
+			--location https://api.github.com/repos/liferay/${repository}/git/tags \
 			--retry 3 \
 			--request POST \
 			--max-time 10 \
