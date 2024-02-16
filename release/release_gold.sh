fdf90db2ce697321cdd034815a5c414c74a60204
Brian Chan <brian.chan@liferay.com>
2024-02-16 15:19:23 +0000

    SF

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -101,6 +101,7 @@ function tag_release {
 	fi
 
 	if (! curl
+			"https://api.github.com/repos/liferay/${repository}/git/tags" \
 			--data-binary @- << EOF
 				{
 					"message": "",
@@ -113,13 +114,12 @@ function tag_release {
 			--header "Accept: application/vnd.github+json" \
 			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
 			--header "X-GitHub-Api-Version: 2022-11-28" \
-			--location https://api.github.com/repos/liferay/${repository}/git/tags \
-			--retry 3 \
-			--request POST \
 			--max-time 10 \
+			--request POST \
+			--retry 3 \
 			--silent)
 	then
-		lc_log ERROR "Unable to tag release using GitHub API."
+		lc_log ERROR "Unable to tag release."
 
 		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
 	fi
