f0f1cbe05582cbd200160c445cd0f4c4e04b59ca
Brian Chan <brian.chan@liferay.com>
2024-05-27 13:00:15 -0300

    LPD-24644 Good enough

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -39,7 +39,7 @@ function check_usage {
 function invoke_github_api {
 	local curl_response=$(\
 		curl \
-			"${1}" \
+			"https://api.github.com/repos/liferay/${1}" \
 			--data "${2}" \
 			--fail \
 			--header "Accept: application/vnd.github+json" \
@@ -164,7 +164,7 @@ function tag_release {
 		END
 	)
 
-	invoke_github_api "https://api.github.com/repos/liferay/${repository}/git/tags" "${tag_data}"
+	invoke_github_api "${repository}/git/tags" "${tag_data}"
 
 	if [ $? -eq "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}" ]
 	then
@@ -181,7 +181,7 @@ function tag_release {
 		END
 	)
 
-	invoke_github_api "https://api.github.com/repos/liferay/${repository}/git/refs" "${ref_data}"
+	invoke_github_api "${repository}/git/refs" "${ref_data}"
 
 	if [ $? -eq "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}" ]
 	then
