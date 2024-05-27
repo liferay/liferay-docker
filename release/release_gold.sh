1e2f163e351c0a9cce3974dc17d0fc9d39639c26
Renato Rego <renato.rego@liferay.com>
2024-05-27 10:28:20 -0300

    LPD-24644 Use parameter full name instead of abbreviation

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -40,12 +40,12 @@ function invoke_github_api {
 	local curl_response=$(\
 		curl \
 			"${1}" \
-			-i \
 			--data "${2}" \
 			--fail \
 			--header "Accept: application/vnd.github+json" \
 			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
 			--header "X-GitHub-Api-Version: 2022-11-28" \
+			--include \
 			--max-time 10 \
 			--request POST \
 			--retry 3 \
