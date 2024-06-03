93263f9803bc6a8f8715505e6a8f417642cfd2dd
Pedro Paccola <pedro.paccola@liferay.com>
2024-06-03 13:28:57 -0300

    DOCKER-250 Add a randomized delay in between checks

--- templates/bundle/resources/usr/local/bin/startup_lock.sh
+++ templates/bundle/resources/usr/local/bin/startup_lock.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 function add_lock {
+	echo "Acquiring lock."
+
 	hostname > "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
 
-	sleep 1
+	sleep 2
 
 	if [ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
 	then
@@ -12,28 +14,38 @@ function add_lock {
 		wait_until_free
 
 		add_lock
+	else
+		echo "Lock acquired by $(hostname)."
 	fi
 }
 
 function wait_until_free {
-	while [ -e "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" ] && [ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
+	while
+		[ -e "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" ] &&
+		[ -n "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ] &&
+		[ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
 	do
 		echo "Wait for $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") to start up."
 
 		if [ "$(find "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" -mmin +2)" ]
 		then
-			echo "Lock created by $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") was not updated for 2 minutes, removing."
+			echo "Lock created by $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") was not updated for 2 minutes, unlocking."
 
-			rm -f "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
+			echo "" > "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
+
+			break
 		fi
 
 		sleep 3
 	done
-
-	echo "Acquiring lock."
 }
 
 function main {
+	local delay=$((RANDOM % 10 + 1))
+
+	echo "Delaying lock check for $delay seconds."
+	sleep $delay
+
 	wait_until_free
 
 	add_lock
