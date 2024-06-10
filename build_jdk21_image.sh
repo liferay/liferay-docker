cfe84866edd26c02b4c0dc242e3eea5fe0841492
Szantina Szanto <szantina.szanto@liferay.com>
2024-06-10 12:59:38 +0200

    DOCKER-255 Add jdk21 image files and ensure compatibility with jdk-common

---
+++ build_jdk21_image.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+source ./_common.sh
+
+function build_docker_image {
+	local image_version=$(./release_notes.sh get-version)
+
+	DOCKER_IMAGE_TAGS=()
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/jdk21:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/jdk21")
+
+	if [ "${1}" == "push" ]
+	then
+		check_docker_buildx
+
+		docker buildx build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay JDK21" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			--build-arg LABEL_ZULU_21_AMD64_VERSION="${LIFERAY_DOCKER_ZULU_21_AMD64_VERSION}" \
+			--build-arg LABEL_ZULU_21_ARM64_VERSION="${LIFERAY_DOCKER_ZULU_21_ARM64_VERSION}" \
+			--builder "liferay-buildkit" \
+			--platform "${LIFERAY_DOCKER_IMAGE_PLATFORMS}" \
+			--push \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	else
+		remove_temp_dockerfile_target_platform
+
+		docker build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay JDK21" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			--build-arg LABEL_ZULU_21_AMD64_VERSION="${LIFERAY_DOCKER_ZULU_21_AMD64_VERSION}" \
+			--build-arg LABEL_ZULU_21_ARM64_VERSION="${LIFERAY_DOCKER_ZULU_21_ARM64_VERSION}" \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	fi
+}
+
+function main {
+	delete_local_images "${LIFERAY_DOCKER_REPOSITORY}/jdk21"
+
+	make_temp_directory templates/jdk21 templates/jdk-common
+
+	log_in_to_docker_hub
+
+	build_docker_image "${1}"
+
+	clean_up_temp_directory
+}
+
+main "${@}"
