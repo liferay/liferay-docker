c551271f8cdbdaa50f31fcd1ad0d026aa5db7267
Brian Chan <brian.chan@liferay.com>
2024-06-20 09:58:23 -0300

    DOCKER-255 Please test my refactor of DRY

---
+++ _build_jdk_image.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+source ./_common.sh
+
+function _build_docker_image {
+	local jdk_friendly_name="${2}"
+	local jdk_image_name="${3}"
+	local jdk_version_number="${4}"
+
+	delete_local_images "${LIFERAY_DOCKER_REPOSITORY}/${jdk_image_name}"
+
+	make_temp_directory templates/${jdk_image_name} templates/_jdk
+
+	log_in_to_docker_hub
+
+	local image_version=$(./release_notes.sh get-version)
+
+	DOCKER_IMAGE_TAGS=()
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/${jdk_image_name}:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/${jdk_image_name}")
+
+	local zulu_amd64_version="LIFERAY_DOCKER_ZULU_${jdk_version_number}_AMD64_VERSION"
+	local zulu_arm64_version="LIFERAY_DOCKER_ZULU_${jdk_version_number}_ARM64_VERSION"
+
+	if [ "${1}" == "push" ]
+	then
+		check_docker_buildx
+
+		docker buildx build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay ${jdk_friendly_name}" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			--build-arg LABEL_ZULU_${jdk_version_number}_AMD64_VERSION="${!zulu_amd64_version}" \
+			--build-arg LABEL_ZULU_${jdk_version_number}_ARM64_VERSION="${!zulu_arm64_version}" \
+			--builder "liferay-buildkit" \
+			--platform "${LIFERAY_DOCKER_IMAGE_PLATFORMS}" \
+			--push \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	else
+		remove_temp_dockerfile_target_platform
+
+		docker build \
+			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+			--build-arg LABEL_NAME="Liferay ${jdk_friendly_name}" \
+			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+			--build-arg LABEL_VERSION="${image_version}" \
+			--build-arg LABEL_ZULU_${jdk_version_number}_AMD64_VERSION="${!zulu_amd64_version}" \
+			--build-arg LABEL_ZULU_${jdk_version_number}_ARM64_VERSION="${!zulu_arm64_version}" \
+			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
+			"${TEMP_DIR}" || exit 1
+	fi
+
+	clean_up_temp_directory
+}
--- build_jdk11_image.sh
+++ build_jdk11_image.sh
@@ -2,56 +2,10 @@
 
 source ./_common.sh
 
-function build_docker_image {
-	local image_version=$(./release_notes.sh get-version)
-
-	DOCKER_IMAGE_TAGS=()
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/jdk11:${image_version}-${TIMESTAMP}")
-	DOCKER_IMAGE_TAGS+=("${LIFERAY_DOCKER_REPOSITORY}/jdk11")
-
-	if [ "${1}" == "push" ]
-	then
-		check_docker_buildx
-
-		docker buildx build \
-			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
-			--build-arg LABEL_NAME="Liferay JDK11" \
-			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
-			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
-			--build-arg LABEL_VERSION="${image_version}" \
-			--build-arg LABEL_ZULU_11_AMD64_VERSION="${LIFERAY_DOCKER_ZULU_11_AMD64_VERSION}" \
-			--build-arg LABEL_ZULU_11_ARM64_VERSION="${LIFERAY_DOCKER_ZULU_11_ARM64_VERSION}" \
-			--builder "liferay-buildkit" \
-			--platform "${LIFERAY_DOCKER_IMAGE_PLATFORMS}" \
-			--push \
-			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
-			"${TEMP_DIR}" || exit 1
-	else
-		remove_temp_dockerfile_target_platform
-
-		docker build \
-			--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
-			--build-arg LABEL_NAME="Liferay JDK11" \
-			--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
-			--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
-			--build-arg LABEL_VERSION="${image_version}" \
-			--build-arg LABEL_ZULU_11_AMD64_VERSION="${LIFERAY_DOCKER_ZULU_11_AMD64_VERSION}" \
-			--build-arg LABEL_ZULU_11_ARM64_VERSION="${LIFERAY_DOCKER_ZULU_11_ARM64_VERSION}" \
-			$(get_docker_image_tags_args "${DOCKER_IMAGE_TAGS[@]}") \
-			"${TEMP_DIR}" || exit 1
-	fi
-}
+source ./_build_jdk_image.sh
 
 function main {
-	delete_local_images "${LIFERAY_DOCKER_REPOSITORY}/jdk11"
-
-	make_temp_directory templates/jdk11 templates/_jdk
-
-	log_in_to_docker_hub
-
-	build_docker_image "${1}"
-
-	clean_up_temp_directory
+	_build_docker_image "${1}" "JDK11" "jdk11" "11"
 }
 
 main "${@}"
