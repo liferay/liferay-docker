9636f482b19421e4e120649fe11a29991caa79e7
Pedro Malta <pedro.malta@liferay.com>
2024-07-03 11:42:17 -0300

    LPD-22313 Sorting the JQL in the Jira cloud url

--- release/track_release_blockers.sh
+++ release/track_release_blockers.sh
@@ -35,7 +35,7 @@ function main {
 
 	local jira_api_response=$(\
 		curl \
-			"https://liferay.atlassian.net/rest/api/3/search?fields=issuekey&jql=project%20%3D%20%22PUBLIC%20-%20Liferay%20Product%20Delivery%22%20and%20labels%20%3D%20release-blocker%20and%20status%20%21%3D%20Closed" \
+			"https://liferay.atlassian.net/rest/api/3/search?fields=issuekey&jql=labels%20%3D%20release-blocker%20and%20project%20%3D%20%22PUBLIC%20-%20Liferay%20Product%20Delivery%22%20and%20status%20%21%3D%20Closed" \
 			--fail \
 			--header "Accept: application/json" \
 			--max-time 10 \
