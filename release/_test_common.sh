399a2efe3a8ae995385606c54e2922bcc35a9bab
Renato Rego <renato.rego@liferay.com>
2024-07-29 19:21:30 -0300

    LPD-31468 Use colors to highlight tests results

--- release/_test_common.sh
+++ release/_test_common.sh
@@ -3,9 +3,9 @@
 function assert_equals {
 	if [ "${1}" = 1 ] || [ "${1}" = true ]
 	then
-		echo "${FUNCNAME[1]} SUCCESS"
+		echo -e "${FUNCNAME[1]} \e[1;32mSUCCESS\e[0m"
 	else
-		echo "${FUNCNAME[1]} FAILED"
+		echo -e "${FUNCNAME[1]} \e[1;31mFAILED\e[0m"
 
 		if [ -n "${2}" ]
 		then
