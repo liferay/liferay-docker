46782131a2375634922cdd11d244756cc35c1f15
Lucas Miranda <lucas.miranda@liferay.com>
2024-08-14 17:52:04 -0300

    LPD-32530 Bash does not delete everything if * is between parentheses

--- release/_bom.sh
+++ release/_bom.sh
@@ -177,7 +177,7 @@ function generate_distro_jar {
 	./catalina.sh stop
 
 	rm -f "${_BUNDLES_DIR}/osgi/modules/biz.aQute.remote.agent-6.4.0.jar"
-	rm -fr "${_BUNDLES_DIR}/tomcat/logs/*"
+	rm -fr "${_BUNDLES_DIR}"/tomcat/logs/*
 }
 
 function generate_pom_release_api {
