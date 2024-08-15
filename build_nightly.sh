8618f42ca361d9fae2ec4967d9e72d62f65d453a
Brian Chan <brian.chan@liferay.com>
2024-08-15 08:16:48 -0300

    Use development mode for nightly (once a week) to rebuild everything)

--- build_nightly.sh
+++ build_nightly.sh
@@ -4,11 +4,15 @@ while true
 do
 	git pull origin master
 
-	LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
-
 	if [ $(date +%w) == 0 ]
 	then
 		docker system prune --all --force
+
+		git clean -dfx
+
+		LIFERAY_DOCKER_DEVELOPER_MODE=true LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
+	else
+		LIFERAY_DOCKER_IMAGE_FILTER=7.4.13.nightly ./build_all_images.sh --push
 	fi
 
 	echo ""
