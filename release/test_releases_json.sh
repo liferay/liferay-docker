385ef4ed8dc9cebb7248ed2efd654718ee4d2e9e
Pedro Malta <pedro.malta@liferay.com>
2024-08-19 16:25:43 -0300

    LPD-32410 Adding test for process_new_product

--- release/test_releases_json.sh
+++ release/test_releases_json.sh
@@ -8,12 +8,16 @@ function main {
 	set_up
 
 	test_merge_json_snippets dxp
+	test_process_new_product
 	test_promote_product_versions dxp
 
 	tear_down
 }
 
 function set_up {
+	export LIFERAY_RELEASE_PRODUCT_NAME="dxp"
+	export _PRODUCT_VERSION="2024.q1.9999"
+	export _PROMOTION_DIR="${PWD}"
 	export _RELEASE_ROOT_DIR="${PWD}"
 
 	_process_product dxp &> /dev/null
@@ -21,9 +25,14 @@ function set_up {
 	_promote_product_versions dxp &> /dev/null
 
 	_merge_json_snippets &> /dev/null
+
+	_process_new_product &> /dev/null
 }
 
 function tear_down {
+	unset LIFERAY_RELEASE_PRODUCT_NAME
+	unset _PRODUCT_VERSION
+	unset _PROMOTION_DIR
 	unset _RELEASE_ROOT_DIR
 
 	rm ./*.json
@@ -39,6 +48,14 @@ function test_merge_json_snippets {
 	local latest_count="$(grep -c "${latest_url}" releases.json)"
 
 	assert_equals "${earliest_count}" 1 "${latest_count}" 1
+}
+
+function test_process_new_product {
+	local actual_number_of_promoted_versions=$(jq "map(select(.promoted == \"true\")) | length" 0000-00-00-releases.json)
+	local expected_promoted_versions_dxp=$(grep -c '' "${_RELEASE_ROOT_DIR}/supported-dxp-versions.txt")
+	local expected_promoted_versions_portal=$(grep -c '' "${_RELEASE_ROOT_DIR}/supported-portal-versions.txt")
+
+	assert_equals "${actual_number_of_promoted_versions}" $((expected_promoted_versions_dxp + expected_promoted_versions_portal - 1))
 }
 
 function test_promote_product_versions {
