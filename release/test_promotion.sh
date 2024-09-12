8f81ccb59f86b25987951c5ff1efac9f984edb99
Lucas Miranda <lucas.miranda@liferay.com>
2024-09-12 16:17:59 -0300

    LPD-24037 Test generate_distro_jar

--- release/test_promotion.sh
+++ release/test_promotion.sh
@@ -2,57 +2,64 @@
 
 source ../_liferay_common.sh
 source ../_test_common.sh
+source _bom.sh
 source _promotion.sh
 
 function main {
 	set_up
 
-	prepare_jars_for_promotion xanadu
-	prepare_poms_for_promotion xanadu
-
-	test_prepare_resources_for_promotion
+	test_generate_distro_jar
 
 	tear_down
 }
 
 function set_up {
-	export LIFERAY_COMMON_EXIT_CODE_BAD=1
 	export LIFERAY_RELEASE_PRODUCT_NAME="dxp"
-	export LIFERAY_RELEASE_RC_BUILD_TIMESTAMP="1721635298"
 	export LIFERAY_RELEASE_VERSION="2024.q2.6"
-
-	export _PRODUCT_VERSION="${LIFERAY_RELEASE_VERSION}"
-
-	export _ARTIFACT_RC_VERSION="${_PRODUCT_VERSION}-${LIFERAY_RELEASE_RC_BUILD_TIMESTAMP}"
-
 	export _RELEASE_ROOT_DIR="${PWD}"
 
-	export _PROMOTION_DIR="${_RELEASE_ROOT_DIR}/release-data/promotion/files"
+	export _BUILD_DIR="${_RELEASE_ROOT_DIR}/release-data/build"
+	export _BUNDLES_DIR="${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp"
+	export _PRODUCT_VERSION="${LIFERAY_RELEASE_VERSION}"
+	export _PROJECTS_DIR="${_RELEASE_ROOT_DIR}"/../..
 
-	mkdir -p "${_PROMOTION_DIR}"
+	lc_cd "${_RELEASE_ROOT_DIR}/test-dependencies"
 
-	lc_cd "${_PROMOTION_DIR}"
+	lc_download \
+		https://releases-cdn.liferay.com/dxp/2024.q2.6/liferay-dxp-tomcat-2024.q2.6-1721635298.zip \
+		liferay-dxp-tomcat-2024.q2.6-1721635298.zip 1> /dev/null
+
+	unzip -q liferay-dxp-tomcat-2024.q2.6-1721635298.zip
+
+	lc_cd "${_RELEASE_ROOT_DIR}"
+
+	mkdir -p release-data
+
+	lc_cd release-data
+
+	mkdir -p "${_RELEASE_ROOT_DIR}/release-data/build/boms"
 }
 
 function tear_down {
-	lc_cd ..
+	pgrep --full --list-name "${_BUNDLES_DIR}" | awk '{print $1}' | xargs --no-run-if-empty kill -9
 
-	rm -fr "${_PROMOTION_DIR}"
+	rm -fr "${_BUNDLES_DIR}"
+	rm -fr "${_RELEASE_ROOT_DIR}/release-data/build/boms"
+	rm -f "${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp-tomcat-2024.q2.6-1721635298.zip"
 
-	unset LIFERAY_COMMON_EXIT_CODE_BAD
 	unset LIFERAY_RELEASE_PRODUCT_NAME
-	unset LIFERAY_RELEASE_RC_BUILD_TIMESTAMP
 	unset LIFERAY_RELEASE_VERSION
-	unset _ARTIFACT_RC_VERSION
+	unset _BUILD_DIR
+	unset _BUNDLES_DIR
 	unset _PRODUCT_VERSION
-	unset _PROMOTION_DIR
+	unset _PROJECTS_DIR
 	unset _RELEASE_ROOT_DIR
 }
 
-function test_prepare_resources_for_promotion {
-	assert_equals \
-		"$(find "${_PROMOTION_DIR}" -name "release.dxp.distro-${LIFERAY_RELEASE_VERSION}.jar" | grep -c /)" 1 \
-		"$(find "${_PROMOTION_DIR}" -name "release.dxp.distro-${LIFERAY_RELEASE_VERSION}.pom" | grep -c /)" 1
+function test_generate_distro_jar {
+	generate_distro_jar &> /dev/null
+
+	assert_equals "$(find "${_RELEASE_ROOT_DIR}" -name "release.dxp.distro-${LIFERAY_RELEASE_VERSION}*.jar" | grep -c /)" 1
 }
 
 main
