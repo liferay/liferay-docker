40214abf4e3dba49b709808737dc18eaee615a8e
Lucas Miranda <lucas.miranda@liferay.com>
2024-10-18 11:59:10 -0300

    LPD-37968 Add test to cover generation of BOM

--- release/test_bom.sh
+++ release/test_bom.sh
@@ -13,12 +13,15 @@ function main {
 	fi
 
 	test_generate_pom_release_bom_compile_only_dxp
+	test_generate_pom_release_bom_dxp
 	test_generate_pom_release_bom_third_party_dxp
 
 	LIFERAY_RELEASE_PRODUCT_NAME="portal"
+	_BUNDLES_DIR="${_RELEASE_ROOT_DIR}/test-dependencies/liferay-portal"
 	_PRODUCT_VERSION="7.4.3.120-ga120"
 
 	test_generate_pom_release_bom_compile_only_portal
+	test_generate_pom_release_bom_portal
 	test_generate_pom_release_bom_third_party_portal
 
 	tear_down
@@ -39,7 +42,23 @@ function set_up {
 
 		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	fi
-	
+
+	lc_cd "${_RELEASE_ROOT_DIR}/test-dependencies"
+
+	lc_download \
+		https://releases-cdn.liferay.com/dxp/2024.q2.6/liferay-dxp-tomcat-2024.q2.6-1721635298.zip \
+		liferay-dxp-tomcat-2024.q2.6-1721635298.zip 1> /dev/null
+
+	unzip -q liferay-dxp-tomcat-2024.q2.6-1721635298.zip
+
+	export _BUNDLES_DIR="${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp"
+
+	lc_download \
+		https://releases-cdn.liferay.com/portal/7.4.3.120-ga120/liferay-portal-tomcat-7.4.3.120-ga120-1718225443.zip \
+		liferay-portal-tomcat-7.4.3.120-ga120-1718225443.zip 1> /dev/null
+
+	unzip -q liferay-portal-tomcat-7.4.3.120-ga120-1718225443.zip
+
 	cd "${_PROJECTS_DIR}"/liferay-portal-ee
 
 	git branch --delete "${_PRODUCT_VERSION}" &> /dev/null
@@ -52,8 +71,14 @@ function set_up {
 }
 
 function tear_down {
+	rm -fr "${_BUNDLES_DIR}"
+	rm -fr "${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp"
+	rm -f "${_RELEASE_ROOT_DIR}/test-dependencies/liferay-dxp-tomcat-2024.q2.6-1721635298.zip"
+	rm -f "${_RELEASE_ROOT_DIR}/test-dependencies/liferay-portal-tomcat-7.4.3.120-ga120-1718225443.zip"
+
 	unset LIFERAY_RELEASE_PRODUCT_NAME
 	unset _BUILD_TIMESTAMP
+	unset _BUNDLES_DIR
 	unset _PRODUCT_VERSION
 	unset _PROJECTS_DIR
 	unset _RELEASE_ROOT_DIR
@@ -78,6 +103,26 @@ function test_generate_pom_release_bom_compile_only_portal {
 		test-dependencies/expected.portal.release.bom.compile.only.pom
 
 	rm release.${LIFERAY_RELEASE_PRODUCT_NAME}.bom.compile.only-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.pom
+}
+
+function test_generate_pom_release_bom_dxp {
+	generate_pom_release_bom &>/dev/null
+
+	assert_equals \
+		release.${LIFERAY_RELEASE_PRODUCT_NAME}.bom-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.pom \
+		test-dependencies/expected.dxp.release.bom.pom
+
+	rm release.${LIFERAY_RELEASE_PRODUCT_NAME}.bom-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.pom
+}
+
+function test_generate_pom_release_bom_portal {
+	generate_pom_release_bom &>/dev/null
+
+	assert_equals \
+		release.${LIFERAY_RELEASE_PRODUCT_NAME}.bom-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.pom \
+		test-dependencies/expected.portal.release.bom.pom
+
+	rm release.${LIFERAY_RELEASE_PRODUCT_NAME}.bom-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.pom
 }
 
 function test_generate_pom_release_bom_third_party_dxp {
