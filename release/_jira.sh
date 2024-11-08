ba3859997625fd4c2dc79c233531b7be2c2d4fc4
Pedro Malta <pedro.malta@liferay.com>
2024-11-08 14:14:16 -0300

    LPD-35965 Create add comment jira ticket function

---
+++ release/_jira.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source ../_liferay_common.sh
+
+function add_comment_jira_ticket {
+	local data=$(
+		cat <<- END
+		{
+			"body": {
+				"content": [
+					{
+						"content": [
+							{
+								"text": "${1}",
+								"type": "text"
+							}
+						],
+						"type": "paragraph"
+					}
+				],
+				"type": "doc",
+				"version": 1
+			}
+		}
+		END
+	)
+
+	_invoke_jira_api "https://liferay.atlassian.net/rest/api/3/issue/${2}/comment" "${data}"
+}
+
+function _invoke_jira_api {
+	local http_response=$(curl \
+		"${1}" \
+		--data "${2}" \
+		--fail \
+		--header "Accept: application/json" \
+		--header "Content-Type: application/json" \
+		--max-time 10 \
+		--request "POST" \
+		--retry 3 \
+		--silent \
+		--user "${LIFERAY_RELEASE_JIRA_USER}:${LIFERAY_RELEASE_JIRA_TOKEN}")
+
+	if [ "$(echo "${http_response}" | jq --exit-status '.id?')" != "null" ]
+	then
+		echo "${http_response}" | jq --raw-output '.key'
+
+		return "${LIFERAY_COMMON_EXIT_CODE_OK}"
+	fi
+
+	echo "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+}
