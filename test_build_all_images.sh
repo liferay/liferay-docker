451cb191c952c92848f8b6d7378f1e434e8c869a
Renato Rego <renato.rego@liferay.com>
2024-11-22 09:34:55 -0300

    DOCKER-261 Add unit test

---
+++ test_build_all_images.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source build_all_images.sh --test
+source _test_common.sh
+
+function main {
+	test_get_latest_available_zulu_version
+}
+
+function test_get_latest_available_zulu_version {
+	_test_get_latest_available_zulu_version "8" "amd64"
+	_test_get_latest_available_zulu_version "8" "arm64"
+	_test_get_latest_available_zulu_version "11" "amd64"
+	_test_get_latest_available_zulu_version "11" "arm64"
+	_test_get_latest_available_zulu_version "21" "amd64"
+	_test_get_latest_available_zulu_version "21" "arm64"
+}
+
+function _test_get_latest_available_zulu_version {
+	echo -e "Running _test_get_latest_available_zulu_version for JDK ${1}, ${2}\n"
+
+	local latest_available_zulu_version=$(\
+		get_latest_available_zulu_version "${1}" "${2}")
+
+	if [ "${1}" == "21" ]
+	then
+		assert_equals "${latest_available_zulu_version}" "21.30.15"
+	else
+		assert_equals \
+			"${latest_available_zulu_version}" \
+			$(curl -H 'accept: */*' -L -s "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?arch=${2}&bundle_type=jdk&ext=deb&hw_bitness=64&javafx=false&java_version=${1}&os=linux" | \
+				jq -r '.zulu_version | join(".")' | cut -f1,2,3 -d'.')
+	fi
+}
+
+main
