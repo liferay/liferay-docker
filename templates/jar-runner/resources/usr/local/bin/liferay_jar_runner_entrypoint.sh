165659ca3e99926976c52232251d5e049e5799e8
Raymond Augé <raymond.auge@liferay.com>
2024-12-12 15:01:13 -0500

    DOCKER-266 Remove mkcert in jar-runner:latest

--- templates/jar-runner/resources/usr/local/bin/liferay_jar_runner_entrypoint.sh
+++ templates/jar-runner/resources/usr/local/bin/liferay_jar_runner_entrypoint.sh
@@ -4,14 +4,6 @@ function main {
 	export JAVA_HOME=/usr/lib/jvm/${JAVA_VERSION}
 	export PATH=/usr/lib/jvm/${JAVA_VERSION}/bin/:${PATH}
 
-	if [ -e /opt/liferay/caroot/rootCA.pem ]
-	then
-		export CAROOT=/opt/liferay/caroot
-		export TRUST_STORES=java
-
-		mkcert -install
-	fi
-
 	if [ -e /usr/local/bin/liferay_jar_runner_set_up.sh ]
 	then
 		/usr/local/bin/liferay_jar_runner_set_up.sh
