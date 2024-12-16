c22346e7eb10625bf3ab682ea9525836468b03bd
Renato Rego <renato.rego@liferay.com>
2024-12-16 09:52:51 -0300

    LPD-29773 Reuse code

--- release/test_publishing.sh
+++ release/test_publishing.sh
@@ -7,23 +7,12 @@ source _publishing.sh
 function main {
 	setup
 
-	_update_bundles_yml &> /dev/null
-
-	_PRODUCT_VERSION="7.4.13-u130"
-
-	_update_bundles_yml &> /dev/null
-
-	_PRODUCT_VERSION="2024.q3.1"
-
-	_update_bundles_yml &> /dev/null
-
 	test_update_bundles_yml
 
 	tear_down
 }
 
 function setup {
-	export _PRODUCT_VERSION="7.4.3.125-ga125"
 	export _RELEASE_ROOT_DIR="${PWD}"
 
 	export _BASE_DIR="${_RELEASE_ROOT_DIR}/test-dependencies"
@@ -33,12 +22,23 @@ function tear_down {
 	git reset HEAD~3 --hard &> /dev/null
 
 	unset _BASE_DIR
-	unset _PRODUCT_VERSION
 	unset _RELEASE_ROOT_DIR
 }
 
 function test_update_bundles_yml {
+	_run_update_bundles_yml "7.4.3.125-ga125"
+	_run_update_bundles_yml "7.4.13-u130"
+	_run_update_bundles_yml "2024.q3.1"
+
 	assert_equals "${_BASE_DIR}/bundles.yml" "${_BASE_DIR}/expected.bundles.yml"
+}
+
+function _run_update_bundles_yml {
+	_PRODUCT_VERSION="${1}"
+
+	echo -e "Running _update_bundles_yml for ${_PRODUCT_VERSION}\n"
+
+	_update_bundles_yml &> /dev/null
 }
 
 main
