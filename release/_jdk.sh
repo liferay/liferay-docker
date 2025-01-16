cbdb5f942194b55f765f2b02c19d0c004de5b136
Renato Rego <renato.rego@liferay.com>
2025-01-16 15:33:50 -0300

    LPD-45787 SF

--- release/_jdk.sh
+++ release/_jdk.sh
@@ -3,26 +3,26 @@
 source ../_liferay_common.sh
 
 function set_jdk_version {
-	local jdk_version=zulu8
+	local jdk_version="zulu8"
 
 	if (echo "${_PRODUCT_VERSION}" | grep -q "q")
 	then
 		if [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '.' -f 1)" -ge 2025 ]]
 		then
-			jdk_version=zulu17
+			jdk_version="zulu17"
 		fi
 	fi
 
 	if [[ "$(echo "${_PRODUCT_VERSION}" | grep "ga")" ]] &&
-	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '-' -f 2 | sed 's/ga//g')" -ge 132 ]]
+	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '-' -f 2 | sed "s/ga//g")" -ge 132 ]]
 	then
-		jdk_version=zulu17
+		jdk_version="zulu17"
 	fi
 
 	if [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '-' -f 1)" == "7.4.13" ]] &&
 	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '-' -f 2 | tr -d u)" -ge 132 ]]
 	then
-		jdk_version=zulu17
+		jdk_version="zulu17"
 	fi
 
 	if [ ! -d "/opt/java/${jdk_version}" ]
