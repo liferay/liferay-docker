ec4693fc005e85a8be6fc1469ea8b49249b66093
Renato Rego <renato.rego@liferay.com>
2025-01-17 11:53:35 -0300

    LPD-45787 Add new unit test

--- release/test_product.sh
+++ release/test_product.sh
@@ -7,6 +7,7 @@ source _product.sh
 function main {
 	set_up
 
+	test_get_java_specification_version
 	test_warm_up_tomcat
 
 	test_warm_up_tomcat_already_warmed
@@ -17,6 +18,7 @@ function main {
 function set_up {
 	export _BUILD_DIR="${PWD}"
 	export _BUNDLES_DIR="${PWD}/test-dependencies/liferay-dxp"
+	export _CURRENT_JAVA_HOME="${JAVA_HOME}"
 
 	lc_cd test-dependencies
 
@@ -36,6 +38,14 @@ function tear_down {
 
 	unset _BUILD_DIR
 	unset _BUNDLES_DIR
+	unset _CURRENT_JAVA_HOME
+}
+
+function test_get_java_specification_version {
+	_test_get_java_specification_version "jdk8" "1.8"
+	_test_get_java_specification_version "zulu8" "1.8"
+	_test_get_java_specification_version "jdk17" "17"
+	_test_get_java_specification_version "zulu17" "17"
 }
 
 function test_warm_up_tomcat {
@@ -51,6 +61,16 @@ function test_warm_up_tomcat_already_warmed {
 	assert_equals \
 		"$(warm_up_tomcat 1> /dev/null; echo "${?}")" \
 		"${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+}
+
+function _test_get_java_specification_version {
+	JAVA_HOME="/opt/java/${1}"
+
+	echo -e "Running _test_get_java_specification_version for ${JAVA_HOME}\n"
+
+	assert_equals "$(get_java_specification_version)" "${2}"
+
+	JAVA_HOME="${_CURRENT_JAVA_HOME}"
 }
 
 main
