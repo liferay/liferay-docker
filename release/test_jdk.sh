32f82e1d3f4f192de55613364a6a1be228c43a22
Renato Rego <renato.rego@liferay.com>
2025-02-28 18:21:34 -0300

    LRP-4991 Add new test

--- release/test_jdk.sh
+++ release/test_jdk.sh
@@ -17,7 +17,10 @@ function main {
 
 function set_up {
 	export _CURRENT_JAVA_HOME="${JAVA_HOME}"
+	export _CURRENT_JAVA_OPTS="${JAVA_OPTS}"
 	export _CURRENT_PATH="${PATH}"
+	export _JDK_PARAMETERS_8="${JAVA_OPTS} -XX:MaxPermSize=256m"
+	export _JDK_PARAMETERS_17=$(echo "${JAVA_OPTS}" | sed "s/-XX:MaxPermSize=[^ ]*//g")
 
 	export _JDK_VERSION_8="zulu8"
 
@@ -46,19 +49,26 @@ function tear_down {
 	PATH="${_CURRENT_PATH}"
 
 	unset _CURRENT_JAVA_HOME
+	unset _CURRENT_JAVA_OPTS
 	unset _CURRENT_PATH
+	unset _JDK_PARAMETERS_8
+	unset _JDK_PARAMETERS_17
 	unset _JDK_VERSION_8
 	unset _JDK_VERSION_17
 }
 
 function test_set_jdk_version_and_parameters {
-	_test_set_jdk_version_and_parameters "2024.q2.0" "/opt/java/${_JDK_VERSION_8}"
-	_test_set_jdk_version_and_parameters "2025.q1.0" "/opt/java/${_JDK_VERSION_17}"
-	_test_set_jdk_version_and_parameters "7.3.10-u36" "/opt/java/${_JDK_VERSION_8}"
-	_test_set_jdk_version_and_parameters "7.4.3-ga131" "/opt/java/${_JDK_VERSION_8}"
-	_test_set_jdk_version_and_parameters "7.4.3-ga132" "/opt/java/${_JDK_VERSION_17}"
-	_test_set_jdk_version_and_parameters "7.4.13-u131" "/opt/java/${_JDK_VERSION_8}"
-	_test_set_jdk_version_and_parameters "7.4.13-u132" "/opt/java/${_JDK_VERSION_17}"
+	JAVA_OPTS="-XX:MaxPermSize=256m"
+
+	_test_set_jdk_version_and_parameters "2024.q3.0" "/opt/java/${_JDK_VERSION_8}" "${JAVA_OPTS}"
+
+	_test_set_jdk_version_and_parameters "2024.q2.0" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+	_test_set_jdk_version_and_parameters "2025.q1.0" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
+	_test_set_jdk_version_and_parameters "7.3.10-u36" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+	_test_set_jdk_version_and_parameters "7.4.3-ga131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+	_test_set_jdk_version_and_parameters "7.4.3-ga132" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
+	_test_set_jdk_version_and_parameters "7.4.13-u131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+	_test_set_jdk_version_and_parameters "7.4.13-u132" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
 }
 
 function _test_set_jdk_version_and_parameters {
@@ -68,7 +78,11 @@ function _test_set_jdk_version_and_parameters {
 
 	set_jdk_version_and_parameters 1> /dev/null
 
-	assert_equals "${JAVA_HOME}" "${2}"
+	assert_equals \
+		"${JAVA_HOME}" "${2}" \
+		"${JAVA_OPTS}" "${3}"
+
+	JAVA_OPTS="${_CURRENT_JAVA_OPTS}"
 }
 
 main
