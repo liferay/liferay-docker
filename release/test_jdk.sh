0be1d941779bf298a98dd574697dd7de57d14a55
Brian Chan <brian.chan@liferay.com>
2025-03-03 10:36:17 +0100

    LRP-4991 Sort

--- release/test_jdk.sh
+++ release/test_jdk.sh
@@ -58,11 +58,12 @@ function tear_down {
 }
 
 function test_set_jdk_version_and_parameters {
+	_test_set_jdk_version_and_parameters "2024.q2.0" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+
 	JAVA_OPTS="-XX:MaxPermSize=256m"
 
 	_test_set_jdk_version_and_parameters "2024.q3.0" "/opt/java/${_JDK_VERSION_8}" "${JAVA_OPTS}"
 
-	_test_set_jdk_version_and_parameters "2024.q2.0" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
 	_test_set_jdk_version_and_parameters "2025.q1.0" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
 	_test_set_jdk_version_and_parameters "7.3.10-u36" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
 	_test_set_jdk_version_and_parameters "7.4.3-ga131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
