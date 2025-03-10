f19c19fc3df942336a9d199b9130ec57af51d95c
Davi Santos <davi.santos@liferay.com>
2025-03-10 08:51:02 -0300

    LPD-46258 Refactor _invoke_github_api function to make LIFERAY_RELEASE_REPOSITORY_OWNER an internal parameter

--- release/_github.sh
+++ release/_github.sh
@@ -3,34 +3,29 @@
 source ../_liferay_common.sh
 
 function invoke_github_api_delete {
-	_invoke_github_api "${1}" "${2}" "DELETE"
+	_invoke_github_api "${1}" "${2}" "${3}" "DELETE"
 
 	echo $?
 }
 
 function invoke_github_api_post {
-	_invoke_github_api "${1}" "${2}" "POST"
+    _invoke_github_api "${1}" "${2}" "${3}" "POST"
 
-	echo $?
+    echo $?
 }
 
 function _invoke_github_api {
-	if [ -z "${LIFERAY_RELEASE_REPOSITORY_OWNER}" ]
-	then
-		LIFERAY_RELEASE_REPOSITORY_OWNER=liferay
-	fi
-
 	local curl_response=$(\
 		curl \
-			"https://api.github.com/repos/${LIFERAY_RELEASE_REPOSITORY_OWNER}/${1}" \
-			--data "${2}" \
+			"https://api.github.com/repos/${1}/${2}" \
+			--data "${3}" \
 			--fail \
 			--header "Accept: application/vnd.github+json" \
 			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
 			--header "X-GitHub-Api-Version: 2022-11-28" \
 			--include \
 			--max-time 10 \
-			--request "${3}" \
+			--request "${4}" \
 			--retry 3 \
 			--silent)
 
