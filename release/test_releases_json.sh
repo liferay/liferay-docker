d99aaa3b6bb59267196ff82d723a1c31636127c3
Davi Santos <davi.santos@liferay.com>
2025-03-13 15:46:43 -0300

    LPD-51054 Add unit test to verify that lts versions are included in the releases.json file

--- release/test_releases_json.sh
+++ release/test_releases_json.sh
@@ -10,6 +10,7 @@ function main {
 	test_merge_json_snippets dxp
 	test_process_new_product_1
 	test_process_new_product_2
+	test_process_product
 	test_promote_product_versions dxp
 
 	tear_down
@@ -71,6 +72,10 @@ function test_process_new_product_2 {
 	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 
 	_PRODUCT_VERSION="${temp_product_version}"
+}
+
+function test_process_product {
+    assert_equals "$(jq '[.[] | select(.targetPlatformVersion == "2025.q1.1")] | length == 1' releases.json)" "true"
 }
 
 function test_promote_product_versions {
