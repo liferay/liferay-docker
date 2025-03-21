5903d9bc5ea986410357623bdc09f18d4ea5c9c4
Davi Santos <davi.santos@liferay.com>
2025-03-21 08:05:17 -0300

    LPD-51784 Add unit tests for patcher version getters

--- release/test_publishing.sh
+++ release/test_publishing.sh
@@ -7,6 +7,9 @@ source _publishing.sh
 function main {
 	setup
 
+	test_get_patcher_product_version_label
+	test_get_patcher_project_version
+	test_get_root_patcher_project_version_name
 	test_update_bundles_yml
 
 	tear_down
@@ -25,6 +28,24 @@ function tear_down {
 	unset _RELEASE_ROOT_DIR
 }
 
+function test_get_patcher_product_version_label {
+	_test_get_patcher_product_version_label "7.3.10-u20" "DXP 7.3"
+	_test_get_patcher_product_version_label "7.4.13-u100" "DXP 7.4"
+	_test_get_patcher_product_version_label "2025.q1.0" "Quarterly Releases"
+}
+
+function test_get_patcher_project_version {
+	_test_get_patcher_project_version "7.3.10-u20" "fix-pack-dxp-20-7310"
+	_test_get_patcher_project_version "7.4.13-u100" "7.4.13-u100"
+	_test_get_patcher_project_version "2025.q1.0" "2025.q1.0"
+}
+
+function test_get_root_patcher_project_version_name {
+	_test_get_root_patcher_project_version_name "7.3.10-u20" "fix-pack-base-7310"
+	_test_get_root_patcher_project_version_name "7.4.13-u100" "7.4.13-ga1"
+	_test_get_root_patcher_project_version_name "2025.q1.0" ""
+}
+
 function test_update_bundles_yml {
 	_run_update_bundles_yml "7.4.3.125-ga125"
 	_run_update_bundles_yml "7.4.13-u130"
@@ -41,6 +62,31 @@ function _run_update_bundles_yml {
 	echo -e "Running _update_bundles_yml for ${_PRODUCT_VERSION}\n"
 
 	_update_bundles_yml --test &> /dev/null
+}
+
+function _test_get_patcher_product_version_label {
+	_PRODUCT_VERSION="${1}"
+
+	echo -e "Running _test_get_patcher_product_version_label for ${_PRODUCT_VERSION}.\n"
+
+	assert_equals "$(get_patcher_product_version_label)" "${2}"
+}
+
+function _test_get_patcher_project_version {
+	_PRODUCT_VERSION="${1}"
+	_ARTIFACT_VERSION="${1}"
+
+	echo -e "Running _test_get_patcher_project_version for ${_PRODUCT_VERSION}.\n"
+
+	assert_equals "$(get_patcher_project_version)" "${2}"
+}
+
+function _test_get_root_patcher_project_version_name {
+	_PRODUCT_VERSION="${1}"
+
+	echo -e "Running _test_get_root_patcher_project_version_name for ${_PRODUCT_VERSION}.\n"
+
+	assert_equals "$(get_root_patcher_project_version_name)" "${2}"
 }
 
 main
