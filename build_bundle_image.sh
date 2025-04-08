2434f21f429da60493b975f8d8d12f2833ea82bc
Renato Rego <renato.rego@liferay.com>
2025-04-08 08:09:28 -0300

    DOCKER-279 Rename aiming consistency with existing code

--- build_bundle_image.sh
+++ build_bundle_image.sh
@@ -172,7 +172,7 @@ function download_trial_dxp_license {
 	fi
 }
 
-function get_newest_tomcat_version {
+function get_latest_tomcat_version {
 	local tomcat_version=$(get_tomcat_version "${TEMP_DIR}/liferay")
 
 	if [[ "${tomcat_version}" == "9.0"* ]]
@@ -260,7 +260,7 @@ function prepare_temp_directory {
 
 	mv "${TEMP_DIR}/liferay-"* "${TEMP_DIR}/liferay"
 
-	local tomcat_version=$(get_newest_tomcat_version)
+	local tomcat_version=$(get_latest_tomcat_version)
 	local tomcat_url="https://dlcdn.apache.org/tomcat/tomcat-${tomcat_version%%.*}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.zip"
 	local tomcat_download_dir="downloads/tomcat/apache-tomcat-${tomcat_version}"
 
