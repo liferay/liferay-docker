994d175fb7820261230b6e8218c30b0fcaea67bb
Pedro Malta <pedro.malta@liferay.com>
2025-04-24 14:20:08 -0300

    LPD-52393 Make the code more consistent with the rest of the repository

--- release/_portal_release_tester.sh
+++ release/_portal_release_tester.sh
@@ -3,28 +3,27 @@
 source _github.sh
 
 function get_test_portal_branch_name {
-    local branch_name=${1}
+	local branch_name="${1}"
 
-    if [[ "${branch_name}" =~ "7.4." ]]
-    then
-        echo "master"
-    else
-        echo "${branch_name}"
-    fi
+	if [[ "${branch_name}" =~ ^7.4.* ]]
+	then
+		echo "master"
+	else
+		echo "${branch_name}"
+	fi
 }
 
 function send_to_ci_test {
 	if [ "${SEND_BUILD_TO_TEST_CI}" = "true" ]
 	then
-		# Parameters
-		local github_branch_name="$(get_test_portal_branch_name ${LIFERAY_RELEASE_GIT_REF})"
+		local github_branch_name="$(get_test_portal_branch_name "${LIFERAY_RELEASE_GIT_REF}")"
 		local github_user_branch_name="${LIFERAY_RELEASE_GIT_REF}"
 		local github_user_name="brianchandotcom"
 		local release_url="https://releases.liferay.com/dxp/release-candidates/"
 		local repo_name="liferay-portal-ee"
-		# Trigger job
-		if(curl -X POST "http://test-1-1/job/test-portal-release/buildWithParameters" \
-			--user "${LIFERAY_RELEASE_JENKINS_USER}:${JENKINS_API_TOKEN}" \
+
+		local http_response=$(curl \
+			"http://test-1-1/job/test-portal-release/buildWithParameters" \
 			--data-urlencode "CI_TEST_SUITE=${CI_TEST_SUITE}" \
 			--data-urlencode "RUN_SCANCODE_PIPELINE=${RUN_SCANCODE_PIPELINE}" \
 			--data-urlencode "TEST_PORTAL_BRANCH_NAME=${github_branch_name}" \
@@ -38,7 +37,15 @@ function send_to_ci_test {
 			--data-urlencode "TEST_PORTAL_RELEASE_TOOLS_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-tools-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.zip" \
 			--data-urlencode "TEST_PORTAL_RELEASE_WAR_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.war" \
 			--data-urlencode "TEST_PORTAL_RELEASE_VERSION=${_PRODUCT_VERSION}" \
-			--data-urlencode "TEST_PORTAL_REPOSITORY_NAME=${repo_name}")
+			--fail \
+			--max-time 10 \
+			--request "POST" \
+			--retry 3 \
+			--silent \
+			--user "${LIFERAY_RELEASE_JENKINS_USER}:${JENKINS_API_TOKEN}" \
+			--write-out "%{http_code}")
+
+		if [ "${http_response}" == "201" ]
 		then
 			lc_log INFO "Test build triggered."
 		else
@@ -49,5 +56,4 @@ function send_to_ci_test {
 
 		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	fi
-
 }
