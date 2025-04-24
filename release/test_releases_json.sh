082d76fe6c08b0aa161ad6e5f01eeb1fecf06a1f
Davi Santos <davi.santos@liferay.com>
2025-04-24 08:11:14 -0300

    LPD-53967 Enable test mode for all tests

--- release/test_releases_json.sh
+++ release/test_releases_json.sh
@@ -19,6 +19,7 @@ function main {
 
 function set_up {
 	export LIFERAY_RELEASE_PRODUCT_NAME="dxp"
+	export LIFERAY_RELEASE_TEST_MODE="true"
 	export _PRODUCT_VERSION="2024.q1.9999"
 	export _PROMOTION_DIR="${PWD}"
 	export _RELEASE_ROOT_DIR="${PWD}"
@@ -42,6 +43,7 @@ function set_up {
 
 function tear_down {
 	unset LIFERAY_RELEASE_PRODUCT_NAME
+	unset LIFERAY_RELEASE_TEST_MODE
 	unset _PRODUCT_VERSION
 	unset _PROMOTION_DIR
 	unset _RELEASE_ROOT_DIR
