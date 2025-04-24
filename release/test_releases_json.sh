6b2d1e6202c414c40005be6d8ee987d2ac8c8297
Davi Santos <davi.santos@liferay.com>
2025-04-24 13:42:02 -0300

    LPD-53967 Refactor test_merge_json_snippets to verify the merging of all expected JSON files

--- release/test_releases_json.sh
+++ release/test_releases_json.sh
@@ -38,15 +38,20 @@ function tear_down {
 }
 
 function test_releases_json_merge_json_snippets {
-	local earliest_url="$(jq -r '.[0].url' < "$(find ./20*dxp*.json | head -n 1)")"
+	local json_files_count=$(
+		ls "${_PROMOTION_DIR}" | \
+		grep \
+			--extended-regexp \
+			"(20.*(dxp|portal).*\.json)" 2> /dev/null | \
+		wc -l)
 
-	local earliest_count="$(grep -c "\"url\": \"${earliest_url}"\" releases.json)"
+	_merge_json_snippets &> /dev/null
 
-	local latest_url="$(jq -r '.[0].url' < "$(find ./20*dxp*.json | tail -n 1)")"
+	assert_equals \
+		"${json_files_count}" \
+		"$(jq length "${_PROMOTION_DIR}/releases.json")"
 
-	local latest_count="$(grep -c "\"url\": \"${latest_url}"\" releases.json)"
-
-	assert_equals "${earliest_count}" 1 "${latest_count}" 1
+	rm ./*dxp*.json ./*portal*.json
 }
 
 function test_releases_json_process_new_product_1 {
