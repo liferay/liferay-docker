3eee10b7b2426efaf0055320e300c9875130ec14
Renato Rego <renato.rego@liferay.com>
2025-05-12 09:18:31 -0300

    DOCKER-289 Wordsmith

--- _release_common.sh
+++ _release_common.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-function get_latest_quartely_version {
-	local version_1=$(echo "${1}" | sed -e "s/-lts//")
-	local version_2=$(echo "${2}" | sed -e "s/-lts//")
+function get_latest_quarterly_version {
+	local product_version_1=$(echo "${1}" | sed -e "s/-lts//")
+	local product_version_2=$(echo "${2}" | sed -e "s/-lts//")
 
-	IFS='.' read -r version_1_year version_1_quarter version_1_sufix <<< "${version_1}"
-	IFS='.' read -r version_2_year version_2_quarter version_2_sufix <<< "${version_2}"
+	IFS='.' read -r product_version_1_year product_version_1_quarter product_version_1_suffix <<< "${product_version_1}"
+	IFS='.' read -r product_version_2_year product_version_2_quarter product_version_2_suffix <<< "${product_version_2}"
 
-	version_1_quarter=$(echo "${version_1_quarter}" | sed -e "s/q//")
-	version_2_quarter=$(echo "${version_2_quarter}" | sed -e "s/q//")
+	product_version_1_quarter=$(echo "${product_version_1_quarter}" | sed -e "s/q//")
+	product_version_2_quarter=$(echo "${product_version_2_quarter}" | sed -e "s/q//")
 
-	if [ "${version_1_year}" -gt "${version_2_year}" ]
+	if [ "${product_version_1_year}" -gt "${product_version_2_year}" ]
 	then
 		echo "${1}"
 
 		return
-	elif [ "${version_1_year}" -lt "${version_2_year}" ]
+	elif [ "${product_version_1_year}" -lt "${product_version_2_year}" ]
 	then
 		echo "${2}"
 
 		return
 	fi
 
-	if [ "${version_1_quarter}" -gt "${version_2_quarter}" ]
+	if [ "${product_version_1_quarter}" -gt "${product_version_2_quarter}" ]
 	then
 		echo "${1}"
 
 		return
-	elif [ "${version_1_quarter}" -lt "${version_2_quarter}" ]
+	elif [ "${product_version_1_quarter}" -lt "${product_version_2_quarter}" ]
 	then
 		echo "${2}"
 
 		return
 	fi
 
-	if [ "${version_1_sufix}" -gt "${version_2_sufix}" ]
+	if [ "${product_version_1_suffix}" -gt "${product_version_2_suffix}" ]
 	then
 		echo "${1}"
 
 		return
-	elif [ "${version_1_sufix}" -lt "${version_2_sufix}" ]
+	elif [ "${product_version_1_suffix}" -lt "${product_version_2_suffix}" ]
 	then
 		echo "${2}"
 
