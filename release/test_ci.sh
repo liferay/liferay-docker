e8d5514a9a07d60f77ef8d8221f425ffdc165e93
Davi Santos <davi.santos@liferay.com>
2025-05-13 11:09:28 -0300

    LPD-52393 Add unit tests for ci.sh

---
+++ release/test_ci.sh
@@ -0,0 +1,40 @@
+source ../_liferay_common.sh
+source ../_test_common.sh
+source _ci.sh
+
+function main {
+	set_up
+
+	test_ci_get_test_portal_branch_name
+	test_ci_not_trigger_ci_test_suite
+
+	tear_down
+}
+
+function set_up {
+	export TRIGGER_CI_TEST_SUITE="false"
+}
+
+function tear_down {
+	unset TRIGGER_CI_TEST_SUITE
+}
+
+function test_ci_get_test_portal_branch_name {
+	_test_ci_get_test_portal_branch_name "release-2025.q1" "release-2025.q1"
+	_test_ci_get_test_portal_branch_name "release-7.4.13.135" "master"
+	_test_ci_get_test_portal_branch_name "release-7.4.3.132-ga132" "master"
+}
+
+function test_ci_not_trigger_ci_test_suite {
+	trigger_ci_test_suite &> /dev/null
+
+	assert_equals "${?}" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+}
+
+function _test_ci_get_test_portal_branch_name {
+	echo -e "Running _test_ci_get_test_portal_branch_name for ${1}.\n"
+
+	assert_equals "$(_get_test_portal_branch_name "${1}")" "${2}"
+}
+
+main
