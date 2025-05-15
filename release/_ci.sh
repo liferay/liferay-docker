3a2890454a710aa1352a8661f0667e8d958015c1
Renato Rego <renato.rego@liferay.com>
2025-05-15 08:41:13 -0300

    LPD-52393 Sort

--- release/_ci.sh
+++ release/_ci.sh
@@ -10,17 +10,17 @@ function trigger_ci_test_suite {
 			--data-urlencode "CI_TEST_SUITE=${CI_TEST_SUITE}" \
 			--data-urlencode "RUN_SCANCODE_PIPELINE=${RUN_SCANCODE_PIPELINE}" \
 			--data-urlencode "TEST_PORTAL_BRANCH_NAME=$(_get_test_portal_branch_name "${LIFERAY_RELEASE_GIT_REF}")" \
-			--data-urlencode "TEST_PORTAL_USER_BRANCH_NAME=${LIFERAY_RELEASE_GIT_REF}" \
-			--data-urlencode "TEST_PORTAL_USER_NAME=brianchandotcom" \
 			--data-urlencode "TEST_PORTAL_BUILD_PROFILE=${LIFERAY_RELEASE_PRODUCT_NAME}" \
 			--data-urlencode "TEST_PORTAL_RELEASE_GIT_ID=${_GIT_SHA}" \
 			--data-urlencode "TEST_PORTAL_RELEASE_OSGI_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-osgi-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.zip" \
 			--data-urlencode "TEST_PORTAL_RELEASE_SQL_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-sql-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.zip" \
 			--data-urlencode "TEST_PORTAL_RELEASE_TOMCAT_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-tomcat-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.7z" \
 			--data-urlencode "TEST_PORTAL_RELEASE_TOOLS_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-tools-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.zip" \
-			--data-urlencode "TEST_PORTAL_RELEASE_WAR_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.war" \
 			--data-urlencode "TEST_PORTAL_RELEASE_VERSION=${_PRODUCT_VERSION}" \
+			--data-urlencode "TEST_PORTAL_RELEASE_WAR_URL=${release_url}${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}/liferay-dxp-${_PRODUCT_VERSION}-${_BUILD_TIMESTAMP}.war" \
 			--data-urlencode "TEST_PORTAL_REPOSITORY_NAME=liferay-portal-ee" \
+			--data-urlencode "TEST_PORTAL_USER_BRANCH_NAME=${LIFERAY_RELEASE_GIT_REF}" \
+			--data-urlencode "TEST_PORTAL_USER_NAME=brianchandotcom" \
 			--fail \
 			--max-time 10 \
 			--request "POST" \
