b97f59eac24530830f24fd7736fed022cfaf5965
Davi Santos <davi.santos@liferay.com>
2025-05-20 10:31:29 -0300

    LPD-56049 Add tests for release version check functions

--- test_release_common.sh
+++ test_release_common.sh
@@ -4,10 +4,74 @@ source ./_release_common.sh
 source ./_test_common.sh
 
 function main {
+	test_release_common_get_product_group_version
+	test_release_common_get_release_year
+	test_release_common_is_7_3_ga_release
+	test_release_common_is_7_3_release
+	test_release_common_is_7_3_u_release
+	test_release_common_is_7_4_ga_release
+	test_release_common_is_7_4_release
+	test_release_common_is_7_4_u_release
 	test_release_common_is_early_product_version_than
+	test_release_common_is_ga_release
 	test_release_common_is_quarterly_release
+	test_release_common_is_u_release
 
+	unset _PRODUCT_VERSION
 	unset ACTUAL_PRODUCT_VERSION
+}
+
+function test_release_common_get_product_group_version {
+	_test_release_common_get_product_group_version "2025.q1.0-lts" "2025.q1"
+	_test_release_common_get_product_group_version "7.4.13.nightly" "7.4"
+}
+
+function test_release_common_get_release_year {
+	_PRODUCT_VERSION="2025.q1.0-lts"
+
+	assert_equals "$(get_release_year)" "2025"
+}
+
+function test_release_common_is_7_3_ga_release {
+	_test_release_common_is_7_3_ga_release "7.3.10-ga1" "0"
+	_test_release_common_is_7_3_ga_release "7.3.7-ga8" "0"
+	_test_release_common_is_7_3_ga_release "7.4.13-u132" "1"
+	_test_release_common_is_7_3_ga_release "7.4.3.132-ga132" "1"
+}
+
+function test_release_common_is_7_3_release {
+	_test_release_common_is_7_3_release "7.3.10-u36" "0"
+	_test_release_common_is_7_3_release "7.3.7-ga8" "0"
+	_test_release_common_is_7_3_release "7.4.13-u132" "1"
+	_test_release_common_is_7_3_release "7.4.3.132-ga132" "1"
+}
+
+function test_release_common_is_7_3_u_release {
+	_test_release_common_is_7_3_u_release "7.3.10-u36" "0"
+	_test_release_common_is_7_3_u_release "7.3.7-ga8" "1"
+	_test_release_common_is_7_3_u_release "7.4.13-u132" "1"
+	_test_release_common_is_7_3_u_release "7.4.3.132-ga132" "1"
+}
+
+function test_release_common_is_7_4_ga_release {
+	_test_release_common_is_7_4_ga_release "7.3.10-u36" "1"
+	_test_release_common_is_7_4_ga_release "7.3.7-ga8" "1"
+	_test_release_common_is_7_4_ga_release "7.4.13-u132" "1"
+	_test_release_common_is_7_4_ga_release "7.4.3.132-ga132" "0"
+}
+
+function test_release_common_is_7_4_release {
+	_test_release_common_is_7_4_release "7.3.10-u36" "1"
+	_test_release_common_is_7_4_release "7.3.7-ga8" "1"
+	_test_release_common_is_7_4_release "7.4.13-u132" "0"
+	_test_release_common_is_7_4_release "7.4.3.132-ga132" "0"
+}
+
+function test_release_common_is_7_4_u_release {
+	_test_release_common_is_7_4_u_release "7.3.10-u3" "1"
+	_test_release_common_is_7_4_u_release "7.3.10-u36" "1"
+	_test_release_common_is_7_4_u_release "7.4.0-ga1" "1"
+	_test_release_common_is_7_4_u_release "7.4.13-u134" "0"
 }
 
 function test_release_common_is_early_product_version_than {
@@ -17,10 +81,81 @@ function test_release_common_is_early_product_version_than {
 	_test_release_common_is_early_product_version_than "2025.q1.1-lts" "2025.q1.0-lts" "1"
 }
 
+function test_release_common_is_ga_release {
+	_test_release_common_is_ga_release "2025.q1.0-lts" "1"
+	_test_release_common_is_ga_release "7.3.10-ga2" "0"
+	_test_release_common_is_ga_release "7.4.0-ga1" "0"
+	_test_release_common_is_ga_release "7.4.13-u134" "1"
+	_test_release_common_is_ga_release "7.4.3.132-ga132" "0"
+}
+
 function test_release_common_is_quarterly_release {
 	_test_release_common_is_quarterly_release "2025.q1.0-lts" "0"
 	_test_release_common_is_quarterly_release "7.4.3.112-ga112" "1"
 	_test_release_common_is_quarterly_release "7.4.13-u134" "1"
+}
+
+function test_release_common_is_u_release {
+	_test_release_common_is_u_release "7.3.10-u2" "0"
+	_test_release_common_is_u_release "7.4.13-u1" "0"
+	_test_release_common_is_u_release "7.4.0-ga1" "1"
+	_test_release_common_is_u_release "2025.q2.0-lts" "1"
+}
+
+function _test_release_common_get_product_group_version {
+	_PRODUCT_VERSION="${1}"
+
+	echo -e "Running _test_release_common_get_product_group_version for ${_PRODUCT_VERSION}.\n"
+
+	assert_equals "$(get_product_group_version)" "${2}"
+}
+
+function _test_release_common_is_7_3_ga_release {
+	echo -e "Running _test_release_common_is_7_3_ga_release for ${1}.\n"
+
+	is_7_3_ga_release "${1}"
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_7_3_release {
+	echo -e "Running _test_release_common_is_7_3_release for ${1}.\n"
+
+	is_7_3_release "${1}"
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_7_3_u_release {
+	echo -e "Running _test_release_common_is_7_3_u_release for ${1}.\n"
+
+	is_7_3_u_release "${1}"
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_7_4_ga_release {
+	echo -e "Running _test_release_common_is_7_4_ga_release for ${1}.\n"
+
+	is_7_4_ga_release "${1}"
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_7_4_release {
+	echo -e "Running _test_release_common_is_7_4_release for ${1}.\n"
+
+	is_7_4_release "${1}"
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_7_4_u_release {
+	echo -e "Running _test_release_common_is_7_4_u_release for ${1}.\n"
+
+	is_7_4_u_release "${1}"
+
+	assert_equals "${?}" "${2}"
 }
 
 function _test_release_common_is_early_product_version_than {
@@ -28,15 +163,31 @@ function _test_release_common_is_early_product_version_than {
 
 	echo -e "Running _test_release_common_is_early_product_version_than for ${1}.\n"
 
-	is_early_product_version_than "${2}" &> /dev/null
+	is_early_product_version_than "${2}"
 
 	assert_equals "${?}" "${3}"
+}
+
+function _test_release_common_is_ga_release {
+	echo -e "Running _test_release_common_is_ga_release for ${1}.\n"
+
+	is_ga_release "${1}"
+
+	assert_equals "${?}" "${2}"
 }
 
 function _test_release_common_is_quarterly_release {
 	echo -e "Running _test_release_common_is_quarterly_release for ${1}.\n"
 
 	is_quarterly_release "${1}" &> /dev/null
+
+	assert_equals "${?}" "${2}"
+}
+
+function _test_release_common_is_u_release {
+	echo -e "Running _test_release_common_is_u_release for ${1}.\n"
+
+	is_u_release "${1}"
 
 	assert_equals "${?}" "${2}"
 }
