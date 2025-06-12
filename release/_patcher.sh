f88ffc293d67fc1ac7a1539be7b518bc29f15ee6
Renato Rego <renato.rego@liferay.com>
2025-06-12 14:41:04 -0300

    LPD-56639 Wordsmith

--- release/_patcher.sh
+++ release/_patcher.sh
@@ -35,7 +35,7 @@ function report_jenkins_url {
 
 		ssh test-3-1 "chown -R 501:501 /mnt/mfs-hdd1-172.16.168/patcher"
 	else
-		echo "No SSH connection to test-3-1."
+		echo "Unable to connect to test-3-1."
 
 		cp -prv "${_BUILD_DIR}/patcher-status/" /mnt/patcher-shared/patcher/
 	fi
@@ -71,7 +71,7 @@ function report_patcher_status {
 
 		ssh test-3-1 "chown -R 501:501 /mnt/mfs-hdd1-172.16.168/patcher"
 	else
-		echo "No SSH connection to test-3-1."
+		echo "Unable to connect to test-3-1."
 
 		cp -prv "${_BUILD_DIR}/patcher-status/" /mnt/patcher-shared/patcher/
 	fi
