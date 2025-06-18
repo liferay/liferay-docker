c858ecc2d6519dc3dfe0e8f4879b224ff3ed910d
Renato Rego <renato.rego@liferay.com>
2025-06-18 07:44:24 -0300

    DOCKER-294 Remove blank line

--- _common.sh
+++ _common.sh
@@ -326,7 +326,6 @@ function test_docker_image {
 }
 
 function warm_up_tomcat {
-
 	#
 	# Warm up Tomcat for older versions to speed up starting Tomcat. Populating
 	# the Hypersonic files can take over 20 seconds.
