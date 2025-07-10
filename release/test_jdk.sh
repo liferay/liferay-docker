c80b144606fbff6fb825c7853def2dc013935dc8
Davi Santos <davi.santos4357@gmail.com>
2025-07-10 10:05:59 -0300

    LPD-60078 Sort

--- release/test_jdk.sh
+++ release/test_jdk.sh
@@ -24,8 +24,8 @@ function set_up {
 	export _CURRENT_JAVA_HOME="${JAVA_HOME}"
 	export _CURRENT_JAVA_OPTS="${JAVA_OPTS}"
 	export _CURRENT_PATH="${PATH}"
-	export _JDK_PARAMETERS_8="${JAVA_OPTS}"
 	export _JDK_PARAMETERS_17=$(echo "${JAVA_OPTS}" | sed "s/-XX:MaxPermSize=[^ ]*//g")
+	export _JDK_PARAMETERS_8="${JAVA_OPTS}"
 
 	export _JDK_VERSION_8="zulu8"
 
@@ -64,15 +64,13 @@ function tear_down {
 
 function test_jdk_set_jdk_version_and_parameters {
 	_test_jdk_set_jdk_version_and_parameters "2024.q2.0" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
-
 	_test_jdk_set_jdk_version_and_parameters "2024.q3.0" "/opt/java/${_JDK_VERSION_8}" "${JAVA_OPTS}"
-
 	_test_jdk_set_jdk_version_and_parameters "2025.q1.0" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
 	_test_jdk_set_jdk_version_and_parameters "7.3.10-u36" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
-	_test_jdk_set_jdk_version_and_parameters "7.4.3-ga131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
-	_test_jdk_set_jdk_version_and_parameters "7.4.3-ga132" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
 	_test_jdk_set_jdk_version_and_parameters "7.4.13-u131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
 	_test_jdk_set_jdk_version_and_parameters "7.4.13-u132" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
+	_test_jdk_set_jdk_version_and_parameters "7.4.3-ga131" "/opt/java/${_JDK_VERSION_8}" "${_JDK_PARAMETERS_8}"
+	_test_jdk_set_jdk_version_and_parameters "7.4.3-ga132" "/opt/java/${_JDK_VERSION_17}" "${_JDK_PARAMETERS_17}"
 }
 
 function _test_jdk_set_jdk_version_and_parameters {
