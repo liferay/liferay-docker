270d68bcf7a3356fdb32e9ef53957952975561b9
Davi Santos <davi.santos4357@gmail.com>
2025-07-29 08:32:00 -0300

    LPD-60450 Split into multiple lines

--- build_all_images.sh
+++ build_all_images.sh
@@ -137,7 +137,14 @@ function build_bundle_images {
 
 	if [[ "${search_output}" != "null" ]]
 	then
-		local latest_7413_version=$(yq '."7.4.13"' bundles.yml | grep '^.*:$' | sed 's/://' | sed 's/.*-u//' | sed 's/7.4.13.nightly//' | sort --numeric-sort --reverse | head --lines 1)
+		local latest_7413_version=$(\
+			yq '."7.4.13"' bundles.yml | \
+			grep '^.*:$' | \
+			sed 's/://' | \
+			sed 's/.*-u//' | \
+			sed 's/7.4.13.nightly//' | \
+			sort --numeric-sort --reverse | \
+			head --lines 1)
 
 		local versions=$(echo "${search_output}" | grep '^.*:$' | sed 's/://')
 
