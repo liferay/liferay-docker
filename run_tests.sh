b429c9c1cf0d31c2fa08ccf0ac8a486bac07daf3
Davi Santos <davi.santos4357@gmail.com>
2025-08-08 11:11:24 -0300

    LPD-62586 Modify run_tests.sh to run only relevant test based on changed files
    - Compare the current branch with upstream/master to detect changed files.
    - Run only the relevant test sets (Docker or Release) based on those changes.
    - If no changes are found, run all tests.
    - If changes are detected in both Docker and Release scripts, run all tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
 function main {
-	local tests_results=$(\
-		find . \( -name "test_build_*.sh" -o -name "test_patching_tool_version.sh" \) -type f -exec ./{} \; && \
-		\
-		cd release && \
-		\
-		find . -name "test_*.sh" -type f -exec ./{} \;)
+	local changed_files=$(git diff --name-only upstream/master)
+	local test_results=""
 
-	echo "${tests_results}"
+	if [ -z "${changed_files}" ]
+	then
+		test_results=$(_run_docker_tests && _run_release_tests)
+	else
+		if (echo "${changed_files}" | grep --extended-regexp "^[^/]+\.sh$" --quiet)
+		then
+			test_results=$(_run_docker_tests)
+		fi
 
-	if [[ "${tests_results}" == *"FAILED"* ]]
+		if (echo "${changed_files}" | grep --extended-regexp "^release/.*\.sh$|^release/test-dependencies/.*" --quiet)
+		then
+			if [ -n "${test_results}" ]
+			then
+				test_results+=$'\n'
+			fi
+
+			test_results+=$(_run_release_tests)
+		fi
+	fi
+
+	echo "${test_results}"
+
+	if [[ "${test_results}" == *"FAILED"* ]]
 	then
 		exit 1
 	fi
+}
+
+function _run_docker_tests {
+	find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f -exec {} \;
+}
+
+function _run_release_tests {
+	cd release
+	
+	find . -name "test_*.sh" -type f -exec {} \;
 }
 
 main
