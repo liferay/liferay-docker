9927f36e500aefae374c4926b5d08be3dcc09adb
Lucas Miranda <lucas.miranda@liferay.com>
2025-08-18 09:01:37 -0300

    LPD-61496 Test if container is healthy

--- test_build_all_images.sh
+++ test_build_all_images.sh
@@ -5,13 +5,38 @@ source ./_test_common.sh
 source ./build_all_images.sh --test
 
 function main {
+	set_up
+
 	if [ "${#}" -eq 1 ]
 	then
 		"${1}"
 	else
 		test_build_all_images_get_latest_available_zulu_version
 		test_build_all_images_has_slim_build_criteria
+		test_build_all_images_is_container_healthy "${_LATEST_RELEASE}"
+		test_build_all_images_is_container_healthy "7.3.10-u36"
 	fi
+
+	tear_down
+}
+
+function set_up {
+	export _LATEST_RELEASE=$(yq eval ".quarterly | keys | .[-1]" "${PWD}/bundles.yml")
+
+	LIFERAY_DOCKER_IMAGE_FILTER="${_LATEST_RELEASE}" LIFERAY_DOCKER_SLIM="true" ./build_all_images.sh &> /dev/null
+	LIFERAY_DOCKER_IMAGE_FILTER="7.3.10-u36" ./build_all_images.sh &> /dev/null
+}
+
+function tear_down {
+	docker stop "liferay-container-${_LATEST_RELEASE}" > /dev/null
+	docker stop "liferay-container-7.3.10-u36" > /dev/null
+
+	docker rm "liferay-container-${_LATEST_RELEASE}" > /dev/null
+	docker rm "liferay-container-7.3.10-u36" > /dev/null
+
+	docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}} {{.ID}}' | grep '^liferay/' | awk '{print $2}') &> /dev/null
+
+	unset _LATEST_RELEASE
 }
 
 function test_build_all_images_get_latest_available_zulu_version {
@@ -30,6 +55,36 @@ function test_build_all_images_has_slim_build_criteria {
 	_test_build_all_images_has_slim_build_criteria "7.4.13.nightly" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	_test_build_all_images_has_slim_build_criteria "7.4.3.132-ga132" "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
 	_test_build_all_images_has_slim_build_criteria "7.4.3.142-ga142" "${LIFERAY_COMMON_EXIT_CODE_OK}"
+}
+
+function test_build_all_images_is_container_healthy {
+	echo -e "Running test_build_all_images_is_container_healthy for version ${1}.\n"
+
+	assert_equals \
+		$(_run_container "${1}") \
+		"\"healthy\""
+}
+
+function _run_container {
+	local container_id=$(docker run --detach --name "liferay-container-${1}" "liferay/dxp:${1}")
+
+	for counter in {1..200}
+	do
+		local health_status=$(docker inspect --format="{{json .State.Health.Status}}" "${container_id}")
+		local license_status=$(docker logs ${container_id} 2> /dev/null | grep --count "License registered for DXP Development")
+		local portal_start=$(docker logs ${container_id} 2> /dev/null | grep --count "Starting Liferay Portal")
+
+		if [ "${health_status}" == "\"healthy\"" ] && ([ ${license_status} -gt 0 ] || [ ${portal_start} -gt 0 ])
+		then
+			echo "${health_status}"
+
+			return
+		fi
+
+		sleep 3
+	done
+
+	echo "failed"
 }
 
 function _test_build_all_images_get_latest_available_zulu_version {
