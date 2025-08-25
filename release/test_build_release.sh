cc1c89260b86f0a0b3f2686fc3b7624ad165b404
Lucas Miranda <lucas.miranda@liferay.com>
2025-08-25 16:19:24 -0300

    LPD-61491 Test bundle generation

--- release/test_build_release.sh
+++ release/test_build_release.sh
@@ -7,6 +7,7 @@ function main {
 	set_up
 
 	test_build_release_handle_automated_build
+	test_build_release_package_release
 
 	tear_down
 }
@@ -14,16 +15,23 @@ function main {
 function set_up {
 	common_set_up
 
+	LIFERAY_RELEASE_GIT_REF=2025.q1.1 ./build_release.sh --integration-test > /dev/null
+
 	export LIFERAY_RELEASE_GIT_REF="release-test"
 	export RUN_SCANCODE_PIPELINE="false"
 	export TRIGGER_CI_TEST_SUITE="false"
 	export _RELEASE_ROOT_DIR="${PWD}"
+
+	export _RELEASE_PACKAGE="${_RELEASE_ROOT_DIR}/release-data/build/release"
 }
 
 function tear_down {
+	rm --force --recursive "${_RELEASE_ROOT_DIR}/release-data"
+
 	unset LIFERAY_RELEASE_GIT_REF
 	unset RUN_SCANCODE_PIPELINE
 	unset TRIGGER_CI_TEST_SUITE
+	unset _RELEASE_PACKAGE
 	unset _RELEASE_ROOT_DIR
 }
 
@@ -37,6 +45,16 @@ function test_build_release_handle_automated_build {
 		"true" \
 		"${TRIGGER_CI_TEST_SUITE}" \
 		"true"
+}
+
+function test_build_release_package_release {
+	assert_equals \
+		"$(find "${_RELEASE_PACKAGE}" -name "liferay-dxp-tomcat-2025.q1.1-lts-*.7z" -type f | wc --lines)" \
+		"1" \
+		"$(find "${_RELEASE_PACKAGE}" -name "liferay-dxp-tomcat-2025.q1.1-lts-*.tar.gz" -type f | wc --lines)" \
+		"1" \
+		"$(find "${_RELEASE_PACKAGE}" -name "liferay-dxp-tomcat-2025.q1.1-lts-*.zip" -type f | wc --lines)" \
+		"1"
 }
 
 main
