8d1ae5ec8e3863b4f7d6df81b13a9504b0c9fbbe
Renato Rego <renato.rego@liferay.com>
2025-10-22 12:04:17 -0300

    LPD-68400 Add main function

--- release/post_build_clean.sh
+++ release/post_build_clean.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-if [ $(date +%w) != 2 ]
-then
-	exit 0
-fi
+function main {
+	if [ $(date +%w) != 2 ]
+	then
+		exit 0
+	fi
 
-docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | \
-	grep --extended-regexp ":.*(-slim).*" | \
-	awk '{print $2}' | \
-	xargs --no-run-if-empty docker rmi --force &> /dev/null
+	docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | \
+		grep --extended-regexp ":.*(-slim).*" | \
+		awk '{print $2}' | \
+		xargs --no-run-if-empty docker rmi --force &> /dev/null
 
-docker rmi $(docker images --filter "dangling=true" --no-trunc) &> /dev/null
-docker rmi --force "liferay/jdk11-jdk8:latest" &> /dev/null
-docker rmi --force "liferay/jdk11:latest" &> /dev/null
-docker rmi --force "liferay/jdk21-jdk11-jdk8:latest" &> /dev/null
-docker rmi --force "liferay/jdk21:latest" &> /dev/null
+	docker rmi $(docker images --filter "dangling=true" --no-trunc) &> /dev/null
+	docker rmi --force "liferay/jdk11-jdk8:latest" &> /dev/null
+	docker rmi --force "liferay/jdk11:latest" &> /dev/null
+	docker rmi --force "liferay/jdk21-jdk11-jdk8:latest" &> /dev/null
+	docker rmi --force "liferay/jdk21:latest" &> /dev/null
 
-for file in $(find $(find . -name "logs-20*" -type d) -name "build*image_id.txt" -type f)
-do
-	docker rmi --force $(cat "${file}" | cut --delimiter=':' --fields=2) &> /dev/null
-done
+	for file in $(find $(find . -name "logs-20*" -type d) -name "build*image_id.txt" -type f)
+	do
+		docker rmi --force $(cat "${file}" | cut --delimiter=':' --fields=2) &> /dev/null
+	done
 
-for dir in "logs-20*" "temp-*"
-do
-	find . /opt/dev/projects/github/liferay-docker -maxdepth 1 -type d -name "${dir}" -mtime +6 -exec rm --force --recursive {} \; &> /dev/null
-done
+	for dir in "logs-20*" "temp-*"
+	do
+		find . /opt/dev/projects/github/liferay-docker -maxdepth 1 -type d -name "${dir}" -mtime +6 -exec rm --force --recursive {} \; &> /dev/null
+	done
+}
+
+main
