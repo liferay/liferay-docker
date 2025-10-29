eb7aca630479a21acf4d600705ee024eeae47c6b
Brian Chan <brian.chan@liferay.com>
2025-10-29 12:54:41 +0000

    Revert "LPD-69829 Keep it lean?"
    This reverts commit 9b0fe227f13e779e10c4298d7a6bb90496e03f86.

--- release/build_nightly.sh
+++ release/build_nightly.sh
@@ -3,7 +3,12 @@
 function main {
 	while true
 	do
-		LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" LIFERAY_RELEASE_GIT_REF="master" LIFERAY_RELEASE_OUTPUT="nightly" LIFERAY_RELEASE_UPLOAD="true" ./build_release.sh
+		export LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true"
+		export LIFERAY_RELEASE_GIT_REF="master"
+		export LIFERAY_RELEASE_OUTPUT="nightly"
+		export LIFERAY_RELEASE_UPLOAD="true"
+
+		./build_release.sh
 
 		if [ "${1}" == "--no-sleep" ]
 		then
