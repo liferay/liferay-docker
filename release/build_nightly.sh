1c8f419a0b2f4b335732dbdd9fa2974fccb3272b
Brian Chan <brian.chan@liferay.com>
2025-10-29 12:54:41 +0000

    Revert "LPD-69829 Add parameter to control nightly build cadence."
    This reverts commit f56a8c86b3a7782625836f1844bc60cc41a735e8.

--- release/build_nightly.sh
+++ release/build_nightly.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
 function main {
-	while true
-	do
-		export LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true"
-		export LIFERAY_RELEASE_GIT_REF="master"
-		export LIFERAY_RELEASE_OUTPUT="nightly"
-		export LIFERAY_RELEASE_UPLOAD="true"
+	export LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true"
+	export LIFERAY_RELEASE_GIT_REF="master"
+	export LIFERAY_RELEASE_OUTPUT="nightly"
+	export LIFERAY_RELEASE_UPLOAD="true"
 
-		./build_release.sh
-
-		if [ "${1}" == "--no-sleep" ]
-		then
-			break
-		else
-			sleep 1d
-		fi
-	done
+	./build_release.sh
 }
 
-main "${1}"
+main
