02998460985289892f36610ec1c22c477a6adb44
Renato Rego <renato.rego@liferay.com>
2025-10-29 10:44:08 -0300

    LPD-69829 Invoke cleaning script

--- release/build_nightly.sh
+++ release/build_nightly.sh
@@ -3,6 +3,8 @@
 function main {
 	while true
 	do
+		./post_build_clean.sh
+
 		LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" LIFERAY_RELEASE_GIT_REF="master" LIFERAY_RELEASE_OUTPUT="nightly" LIFERAY_RELEASE_UPLOAD="true" ./build_release.sh
 
 		if [ "${1}" == "--no-sleep" ]
