f56a8c86b3a7782625836f1844bc60cc41a735e8
Renato Rego <renato.rego@liferay.com>
2025-10-29 09:23:50 -0300

    LPD-69829 Add parameter to control nightly build cadence.

--- release/build_nightly.sh
+++ release/build_nightly.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 function main {
-	export LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true"
-	export LIFERAY_RELEASE_GIT_REF="master"
-	export LIFERAY_RELEASE_OUTPUT="nightly"
-	export LIFERAY_RELEASE_UPLOAD="true"
+	while true
+	do
+		export LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true"
+		export LIFERAY_RELEASE_GIT_REF="master"
+		export LIFERAY_RELEASE_OUTPUT="nightly"
+		export LIFERAY_RELEASE_UPLOAD="true"
 
-	./build_release.sh
+		./build_release.sh
+
+		if [ "${1}" == "--no-sleep" ]
+		then
+			break
+		else
+			sleep 1d
+		fi
+	done
 }
 
-main
+main "${1}"
