1d6e1b3a988376746704a33c50b1e6376c8e40b0
Davi Santos <davi.santos@liferay.com>
2025-10-29 08:55:44 -0300

    LPD-69891 Skip filtering images for deletion, as this will run only on release-master nodes

--- release/post_build_clean.sh
+++ release/post_build_clean.sh
@@ -5,21 +5,7 @@ source ./_liferay_common.sh
 function main {
 	lc_log INFO "Cleaning build."
 
-	docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | \
-		grep --extended-regexp ":.*(-slim).*" | \
-		awk '{print $2}' | \
-		xargs --no-run-if-empty docker rmi --force &> /dev/null
-
-	docker rmi $(docker images --filter "dangling=true" --no-trunc) &> /dev/null
-	docker rmi --force "liferay/jdk11-jdk8:latest" &> /dev/null
-	docker rmi --force "liferay/jdk11:latest" &> /dev/null
-	docker rmi --force "liferay/jdk21-jdk11-jdk8:latest" &> /dev/null
-	docker rmi --force "liferay/jdk21:latest" &> /dev/null
-
-	for file in $(find $(find . -name "logs-20*" -type d) -name "build*image_id.txt" -type f)
-	do
-		docker rmi --force $(cat "${file}" | cut --delimiter=':' --fields=2) &> /dev/null
-	done
+	docker system prune --all --force &> /dev/null
 
 	for dir in "logs-20*" "temp-*"
 	do
