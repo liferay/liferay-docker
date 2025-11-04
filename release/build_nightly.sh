9f4d3bacd16bd547b986c34365bb8e1f154cb82f
Pedro Malta <pedro.malta@liferay.com>
2025-11-04 09:08:05 -0300

    LPD-70528 Add error handling for the build_nightly.sh file

--- release/build_nightly.sh
+++ release/build_nightly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ../_liferay_common.sh
 
 function main {
 	while true
@@ -6,6 +7,13 @@ function main {
 		./post_build_clean.sh
 
 		LIFERAY_COMMON_DOWNLOAD_SKIP_CACHE="true" LIFERAY_RELEASE_GIT_REF="master" LIFERAY_RELEASE_OUTPUT="nightly" LIFERAY_RELEASE_UPLOAD="true" ./build_release.sh
+
+		if [ "${?}" -ne 0 ] && [ "${1}" == "--no-sleep" ]
+		then
+			lc_log ERROR "Nightly build failed."
+
+			exit "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+		fi
 
 		if [ "${1}" == "--no-sleep" ]
 		then
