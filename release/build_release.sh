15095fe781df9d3d7c031abd312ef5e95eebd4ef
Pedro Malta <pedro.malta@liferay.com>
2025-12-22 15:31:55 -0300

    LPD-74624 Fix test case

--- release/build_release.sh
+++ release/build_release.sh
@@ -72,11 +72,14 @@ function check_usage {
 }
 
 function handle_automated_build {
-	if [ "$(get_release_output)" == "hotfix" ] ||
-	   [ "$(get_release_output)" == "nightly" ] ||
-	   [ "${BUILD_CAUSE}" != "TIMERTRIGGER" ]
+	if [ "${LIFERAY_RELEASE_TEST_MODE}" != "true" ]
 	then
-		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+		if [ "$(get_release_output)" == "hotfix" ] ||
+		   [ "$(get_release_output)" == "nightly" ] ||
+		   [ "${BUILD_CAUSE}" != "TIMERTRIGGER" ]
+		then
+			return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+		fi
 	fi
 
 	lc_log INFO "This build was triggered automatically."
