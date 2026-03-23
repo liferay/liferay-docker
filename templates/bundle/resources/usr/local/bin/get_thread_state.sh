47c412d96be8b3acd2c284ddd43d7ee7cbb49f50
Renato Rego <renato.rego@liferay.com>
2026-03-23 13:11:52 -0300

    LPD-79036 Consistency with existing code pattern

--- templates/bundle/resources/usr/local/bin/get_thread_state.sh
+++ templates/bundle/resources/usr/local/bin/get_thread_state.sh
@@ -6,7 +6,7 @@ function _compare {
 
 	while read -r hash name state
 	do
-		((total++))
+		total=$((total + 1))
 
 		found=$(awk \
 			-v h="${hash}" \
@@ -17,7 +17,7 @@ function _compare {
 
 		if [ -n "${found}" ]
 		then
-			((match++))
+			match=$((match + 1))
 		fi
 	done < "${1}"
 
