fa869cef4e3b8a7f8f501e59d960022d0d82d1a0
Pedro Malta <pedro.malta@liferay.com>
2026-05-12 09:58:27 -0300

    LPD-89568 Add script to trigger premium-support Q1 LTS builds

---
+++ release/build_premium_support_q1_lts.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+source ../_liferay_common.sh
+source ../_release_common.sh
+
+function check_usage {
+	if [ -z "${JENKINS_API_TOKEN}" ] || [ -z "${LIFERAY_RELEASE_JENKINS_USER}" ]
+	then
+		echo "Usage: ${0}"
+		echo ""
+		echo "The script reads the following environment variables:"
+		echo ""
+		echo "    JENKINS_API_TOKEN: API token of the Jenkins user that triggers build-release"
+		echo "    LIFERAY_RELEASE_JENKINS_USER: Jenkins user that triggers build-release"
+
+		exit "${LIFERAY_COMMON_EXIT_CODE_HELP}"
+	fi
+}
+
+function main {
+	if [[ "${BASH_SOURCE[0]}" != "${0}" ]]
+	then
+		return
+	fi
+
+	check_usage
+
+	local exit_code="${LIFERAY_COMMON_EXIT_CODE_OK}"
+
+	for branch in $(get_premium_support_q1_lts_release_branches)
+	do
+		if ! trigger_build_release "${branch}"
+		then
+			exit_code="${LIFERAY_COMMON_EXIT_CODE_BAD}"
+		fi
+	done
+
+	return "${exit_code}"
+}
+
+function trigger_build_release {
+	local branch="${1}"
+
+	local http_response=$(curl \
+		"https://release-master.liferay.com/job/build-release/buildWithParameters" \
+		--data-urlencode "CI_TEST_SUITE=portal-release-acceptance" \
+		--data-urlencode "LIFERAY_RELEASE_GIT_REF=${branch}" \
+		--data-urlencode "RUN_SCANCODE_PIPELINE=true" \
+		--data-urlencode "TRIGGER_CI_TEST_SUITE=true" \
+		--max-time 10 \
+		--request "POST" \
+		--retry 3 \
+		--silent \
+		--user "${LIFERAY_RELEASE_JENKINS_USER}:${JENKINS_API_TOKEN}" \
+		--write-out "%{http_code}")
+
+	if [ "${http_response}" == "201" ]
+	then
+		lc_log INFO "Triggered build-release for ${branch}."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_OK}"
+	fi
+
+	lc_log ERROR "Unable to trigger build-release for ${branch}. HTTP response: ${http_response}."
+
+	return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+}
+
+main "${@}"
