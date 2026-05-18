aca17156074c3ba60dfcd0070b30f203ebb14c88
Renato Rego <renato.rego@liferay.com>
2026-05-18 12:01:53 -0300

    LPD-85421 Run tests related to code changes only

--- run_tests.sh
+++ run_tests.sh
@@ -14,20 +14,14 @@ function main {
 	then
 		test_results=$((_run_docker_tests && _run_release_tests) 2>&1 | tee /dev/stderr)
 	else
-		if (echo "${changed_files}" | grep --extended-regexp "^[^/]+\.sh$" --quiet)
+		test_results=$(_run_docker_tests "${changed_files}" 2>&1 | tee /dev/stderr)
+
+		if [ -n "${test_results}" ]
 		then
-			test_results=$(_run_docker_tests 2>&1 | tee /dev/stderr)
+			test_results+=$'\n'
 		fi
 
-		if (echo "${changed_files}" | grep --extended-regexp "^release/.*\.sh$|^release/test-dependencies/.*" --quiet)
-		then
-			if [ -n "${test_results}" ]
-			then
-				test_results+=$'\n'
-			fi
-
-			test_results+=$(_run_release_tests 2>&1 | tee /dev/stderr)
-		fi
+		test_results+=$(_run_release_tests "${changed_files}" 2>&1 | tee /dev/stderr)
 	fi
 
 	if [[ "${test_results}" == *"FAILED"* ]]
@@ -39,13 +33,29 @@ function main {
 }
 
 function _run_docker_tests {
-	find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f | sort | xargs --max-args=1 /bin/bash
+	if [ -z "${1}" ]
+	then
+		find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f | sort | xargs --max-args=1 /bin/bash
+	else
+		for changed_file in $(echo "${1}" | grep --extended-regexp "^[^/]+\.sh$")
+		do
+			find . -name "test_$(basename ${changed_file} | sed "s/^_//")" -type f | xargs --max-args=1 /bin/bash
+		done
+	fi
 }
 
 function _run_release_tests {
 	cd release
 
-	find . -name "test_*.sh" -type f | sort | xargs --max-args=1 /bin/bash
+	if [ -z "${1}" ]
+	then
+		find . -name "test_*.sh" -type f | sort | xargs --max-args=1 /bin/bash
+	else
+		for changed_file in $(echo "${1}" | grep --extended-regexp "^release/.*\.sh$|^release/test-dependencies/.*")
+		do
+			find . -name "test_$(basename ${changed_file} | sed "s/^_//")" -type f | xargs --max-args=1 /bin/bash
+		done
+	fi
 }
 
 main
