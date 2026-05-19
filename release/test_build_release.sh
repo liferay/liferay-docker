8521e043de8548798bf01267e46656e4aaf17858
Renato Rego <renato.rego@liferay.com>
2026-05-19 14:38:21 -0300

    LPD-85421 Prevent intermittent test failure

--- release/test_build_release.sh
+++ release/test_build_release.sh
@@ -155,6 +155,10 @@ function test_build_release_main {
 }
 
 function _clean_up_release_data {
+	pgrep --full --list-name "${_RELEASE_ROOT_DIR}/release-data" \
+		| awk '{print $1}' \
+		| xargs --no-run-if-empty kill -9
+
 	rm --force --recursive "${_RELEASE_ROOT_DIR}/release-data"
 }
 
