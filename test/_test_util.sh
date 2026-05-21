95844a850c76a1e5f6792b58111f22a546587c33
Renato Rego <renato.rego@liferay.com>
2026-05-21 15:03:25 -0300

    LPD-89568 Add new test util file to reuse code across tests.

---
+++ test/_test_util.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function add_release_to_test_dependency {
+	local release_path="dxp"
+
+	if [[ "${1}" =~ *-[0-9]+$ ]]
+	then
+		release_path="${release_path}/release-candidates"
+	fi
+
+	local release=$(
+		cat <<- END
+		<li>
+			<a href="/${release_path}/${1}" class="icon icon-directory" title="${1}">
+				<span class="name">${1}</span>
+				<span class="size"></span>
+				<span class="date">01/01/2026 12:00:00 PM</span>
+			</a>
+		</li>
+		END
+	)
+
+	release="${release//$'\n'/\\n}"
+
+	sed --in-place "/<\/ul>/i \\${release}" "${2}"
+}
