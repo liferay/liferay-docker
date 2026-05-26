7043e6f8c8f56fda2e3deb29eaebaed195bf0e9b
Davi Santos <davi.santos@liferay.com>
2026-05-26 15:21:38 -0300

    LPD-91206 Add script to sync Crowdin translations to liferay-portal

---
+++ crowdin/crowdin_sync.sh
@@ -0,0 +1,320 @@
+#!/bin/bash
+
+source ../_liferay_common.sh
+source ../release/_git.sh
+
+function check_usage {
+	if [ -z "${CROWDIN_API_TOKEN}" ] ||
+	   [ -z "${CROWDIN_PROJECT_ID}" ]
+	then
+		print_help
+	fi
+
+	_CROWDIN_DIR="${PWD}"
+
+	LIFERAY_COMMON_LOG_DIR="${_CROWDIN_DIR}/logs"
+
+	_PROJECTS_DIR="/opt/dev/projects/github"
+
+	if [ ! -d "${_PROJECTS_DIR}" ]
+	then
+		_PROJECTS_DIR="${_CROWDIN_DIR}"
+	fi
+}
+
+function close_existing_pull_request {
+	lc_log INFO "Checking for existing open pull requests."
+
+	for repository_owner in brianchandotcom liferay-release
+	do
+		local existing_pull_request=$(gh pr list \
+			--jq ".[0]" \
+			--json "number" \
+			--repo "${repository_owner}/liferay-portal" \
+			--search "head:crowdin-translations" \
+			--state "open")
+
+		if [ -z "${existing_pull_request}" ]
+		then
+			lc_log INFO "No existing pull request found in ${repository_owner}/liferay-portal."
+
+			continue
+		fi
+
+		local pull_request_number=$( \
+			echo "${existing_pull_request}" | jq --raw-output ".number")
+
+		lc_log INFO "Closing existing pull request #${pull_request_number}."
+
+		gh pr close "${pull_request_number}" --repo "${repository_owner}/liferay-portal"
+	done
+}
+
+function create_pull_request {
+	lc_log INFO "Creating pull request with new translations."
+
+	if ! git remote get-url liferay-release &> /dev/null
+	then
+		git remote add liferay-release "git@github.com:liferay-release/liferay-portal.git"
+	fi
+
+	git push liferay-release "${_TEMP_BRANCH}"
+
+	if [ "${?}" -ne 0 ]
+	then
+		lc_log ERROR "Unable to push branch ${_TEMP_BRANCH} to liferay-release/liferay-portal."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+
+	gh pr create \
+		--base "master" \
+		--body "This pull request was automatically created by the Release team." \
+		--head "liferay-release:${_TEMP_BRANCH}" \
+		--repo "liferay-release/liferay-portal" \
+		--title "LPD-91206 Update Translations"
+
+	if [ "${?}" -ne 0 ]
+	then
+		lc_log ERROR "Unable to create pull request with new translations."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+}
+
+function download_translations {
+	lc_log INFO "Downloading translations from Crowdin."
+
+	crowdin download translations \
+		--branch "master" \
+		--export-only-approved \
+		--no-progress \
+		--plain \
+		--verbose
+
+	if [ "${?}" -ne 0 ]
+	then
+		lc_log ERROR "Unable to download translations from Crowdin."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+}
+
+function main {
+	if [[ "${BASH_SOURCE[0]}" != "${0}" ]]
+	then
+		return
+	fi
+
+	check_usage
+
+	lc_time_run close_existing_pull_request
+
+	if [ "${_PROJECTS_DIR}" == "${_CROWDIN_DIR}" ]
+	then
+		lc_background_run clone_repository liferay-portal
+
+		lc_wait
+	fi
+
+	lc_time_run update_portal_repository
+
+	lc_time_run set_up_branch
+
+	lc_time_run upload_sources
+
+	lc_time_run download_translations
+
+	lc_time_run merge_and_commit_translations
+
+	if [ "${_CREATE_PULL_REQUEST}" != "true" ]
+	then
+		lc_log INFO "Skipping pull request creation because there are no new translations."
+
+		exit "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	fi
+
+	lc_time_run create_pull_request
+}
+
+function merge_and_commit_translations {
+	local translation_file_regex="(Language|bundle)(_[a-zA-Z].*)?\.properties$"
+
+	local changed_files=$( \
+		git diff --name-only | grep --extended-regexp "${translation_file_regex}")
+
+	if [ -z "${changed_files}" ]
+	then
+		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	fi
+
+	lc_log INFO "Merging approved translations into translation files."
+
+	local translation_file
+
+	while IFS= read -r translation_file
+	do
+		_merge_translation_file "${translation_file}"
+	done <<< "${changed_files}"
+
+	local merged_files=$( \
+		git diff --name-only | grep --extended-regexp "${translation_file_regex}")
+
+	if [ -z "${merged_files}" ]
+	then
+		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+	fi
+
+	echo "${merged_files}" | xargs git add
+
+	git commit --message "LPD-91206 Update Translations"
+
+	_CREATE_PULL_REQUEST=true
+}
+
+function print_help {
+	echo "Usage: ${0}"
+	echo ""
+	echo "The script reads the following environment variables:"
+	echo ""
+	echo "    CROWDIN_API_TOKEN: Crowdin API token."
+	echo "    CROWDIN_PROJECT_ID: Crowdin project ID."
+	echo ""
+
+	exit "${LIFERAY_COMMON_EXIT_CODE_HELP}"
+}
+
+function set_up_branch {
+	_TEMP_BRANCH="crowdin-translations-$(date "+%Y%m%d%H%M%S")"
+
+	lc_cd "${_PROJECTS_DIR}/liferay-portal"
+
+	git checkout -b "${_TEMP_BRANCH}"
+
+	if [ "${?}" -ne 0 ]
+	then
+		lc_log ERROR "Unable to create branch ${_TEMP_BRANCH}."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+
+	cp "${_CROWDIN_DIR}/crowdin.yml" "${_PROJECTS_DIR}/liferay-portal"
+}
+
+function update_portal_repository {
+	lc_cd "${_PROJECTS_DIR}/liferay-portal"
+
+	git checkout master --force
+
+	git reset --hard && git clean -dfx
+
+	if ! git remote get-url upstream &> /dev/null
+	then
+		git remote add upstream "git@github.com:liferay/liferay-portal.git"
+	fi
+
+	git pull upstream master
+
+	git log -1
+}
+
+function upload_sources {
+	lc_log INFO "Uploading source files to Crowdin."
+
+	crowdin upload sources \
+		--branch "master" \
+		--no-progress \
+		--plain \
+		--verbose
+
+	if [ "${?}" -ne 0 ]
+	then
+		lc_log ERROR "Unable to upload source files to Crowdin."
+
+		return "${LIFERAY_COMMON_EXIT_CODE_BAD}"
+	fi
+}
+
+function _apply_crowdin_translations {
+	local crowdin_translation_file=${1}
+	local head_translation_file=${2}
+
+	awk \
+		-v crowdin_translation_file="${crowdin_translation_file}" \
+		-v head_translation_file="${head_translation_file}" '
+		function is_translation(line) {
+			if (line ~ /^[#!]/ || line !~ /=/) {
+				return 0
+			}
+
+			return 1
+		}
+
+		function parse_key(line) {
+			sub(/=.*/, "", line)
+
+			return line
+		}
+
+		FILENAME == crowdin_translation_file {
+			if (is_translation($0)) {
+				key = parse_key($0)
+
+				crowdin_translations[key] = $0
+			}
+		}
+
+		FILENAME == head_translation_file {
+			if (!is_translation($0)) {
+				print
+
+				next
+			}
+
+			key = parse_key($0)
+
+			if (key in crowdin_translations) {
+				print crowdin_translations[key]
+			} else {
+				print
+			}
+		}
+	' "${crowdin_translation_file}" "${head_translation_file}"
+}
+
+function _has_new_translations {
+	local head_translation_file=${1}
+	local merged_translation_file=${2}
+
+	! diff --brief \
+		<(grep "=" "${head_translation_file}") \
+		<(grep "=" "${merged_translation_file}") &> /dev/null
+}
+
+function _merge_translation_file {
+	local crowdin_translation_file=${1}
+
+	local head_translation_file=$(mktemp)
+
+	git show "HEAD:${crowdin_translation_file}" > "${head_translation_file}"
+
+	local merged_translation_file=$(mktemp)
+
+	_apply_crowdin_translations "${crowdin_translation_file}" "${head_translation_file}" > "${merged_translation_file}"
+
+	if [ -z "$(tail --bytes=1 "${merged_translation_file}")" ]
+	then
+		truncate --size=-1 "${merged_translation_file}"
+	fi
+
+	if _has_new_translations "${head_translation_file}" "${merged_translation_file}"
+	then
+		mv "${merged_translation_file}" "${crowdin_translation_file}"
+	else
+		cp "${head_translation_file}" "${crowdin_translation_file}"
+	fi
+
+	rm --force "${head_translation_file}" "${merged_translation_file}"
+}
+
+main "${@}"
